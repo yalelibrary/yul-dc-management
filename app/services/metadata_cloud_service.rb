3b997c3bb954681b2af052224fd6fc6d1ddbdbcc
Max Kadel <max@curationexperts.com>
2020-05-17 14:47:44 -0400

    WIP, looping through array but not outputting url

--- app/services/metadata_cloud_service.rb
+++ app/services/metadata_cloud_service.rb
@@ -1,9 +1,29 @@
 # frozen_string_literal: true
+require 'csv'
 
 class MetadataCloudService
   def mc_get
     metadata_cloud_username = ENV["MC_USER"]
     metadata_cloud_password = ENV["MC_PW"]
     HTTP.basic_auth(user: metadata_cloud_username, pass: metadata_cloud_password).get("https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/16371272?mediaType=json")
+  end
+
+  def oid_array
+    @oid_array ||= build_oid_array
+  end
+
+  def build_oid_array
+    fixture_ids_table = CSV.read(Rails.root.join("spec", "fixtures", "fixture_ids.csv"), headers: true)
+    oid_array = fixture_ids_table.by_col[0]
+  end
+
+  def metadata_cloud_url
+    @metadata_cloud_url ||= build_metadata_cloud_url
+  end
+
+  def build_metadata_cloud_url
+    oid_array.each do |oid|
+      puts "https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/#{oid}?mediaType=json"
+    end
   end
 end
--- spec/services/metadata_cloud_service_spec.rb
+++ spec/services/metadata_cloud_service_spec.rb
@@ -4,10 +4,19 @@ require "webmock"
 
 WebMock.allow_net_connect!
 
-RSpec.describe MetadataCloudService do
+RSpec.describe MetadataCloudService, vpn_only: true do
   let(:mcs) { described_class.new }
 
   it "can connect to the metadata cloud using basic auth" do
     expect(mcs.mc_get.to_str).to include "Manuscript, on parchment"
   end
+
+  it "can read from a csv file" do
+    expect(mcs.oid_array).to include "2034600"
+  end
+
+  it "can loop through the oids and build a metadata cloud default url" do
+    expect(mcs.build_metadata_cloud_url).to include "https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/16371272?mediaType=json"
+  end
+
 end
