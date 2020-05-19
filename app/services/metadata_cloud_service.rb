91d29a196105801cbd2ff928d1748f841ce367b3
Max Kadel <max@curationexperts.com>
2020-05-18 22:41:57 -0400

    well, it parses... and there's a new file... but that file is empty

--- app/services/metadata_cloud_service.rb
+++ app/services/metadata_cloud_service.rb
@@ -2,18 +2,25 @@
 require 'csv'
 
 class MetadataCloudService
-  def mc_get
+  def mc_get(oid_url)
     metadata_cloud_username = ENV["MC_USER"]
     metadata_cloud_password = ENV["MC_PW"]
-    HTTP.basic_auth(user: metadata_cloud_username, pass: metadata_cloud_password).get("https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/16371272?mediaType=json")
+    HTTP.basic_auth(user: metadata_cloud_username, pass: metadata_cloud_password).get(oid_url)
   end
 
-  def list_of_oids
-    @list_of_oids ||= build_oid_array
+  def save_mc_json_to_file(mc_response)
+    file_folder = Rails.root.join("spec", "fixtures", "new_json")
+    json_metadata = mc_response.body.to_str
+    JSON.parse(json_metadata)
+    File.open(file_folder.join("test_file" + ".json"), "w")
   end
 
-  def build_oid_array
-    fixture_ids_table = CSV.read(Rails.root.join("spec", "fixtures", "fixture_ids.csv"), headers: true)
+  def list_of_oids(oid_path)
+    @list_of_oids ||= build_oid_array(oid_path)
+  end
+
+  def build_oid_array(oid_path)
+    fixture_ids_table = CSV.read(oid_path, headers: true)
     fixture_ids_table.by_col[0]
   end
 
@@ -21,5 +28,9 @@ class MetadataCloudService
     "https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/#{oid}?mediaType=json"
   end
 
-  def refresh_data(oid_path); end
+  def refresh_data(oid_path)
+    list_of_oids(oid_path).each do |oid|
+      mc_get(build_metadata_cloud_url(oid))
+    end
+  end
 end
--- spec/services/metadata_cloud_service_spec.rb
+++ spec/services/metadata_cloud_service_spec.rb
@@ -6,25 +6,30 @@ WebMock.allow_net_connect!
 
 RSpec.describe MetadataCloudService, vpn_only: true do
   let(:mcs) { described_class.new }
+  let(:oid_path) { Rails.root.join("spec", "fixtures", "fixture_ids.csv") }
+  let(:oid_url) { "https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/16371272?mediaType=json" }
+  let(:new_fixture_path) { Rails.root.join("spec", "fixtures", "new_json", "test_file.json") }
+  let(:mc_response) { mcs.mc_get(oid_url) }
 
   it "can connect to the metadata cloud using basic auth" do
-    expect(mcs.mc_get.to_str).to include "Manuscript, on parchment"
+    expect(mcs.mc_get(oid_url).to_str).to include "Manuscript, on parchment"
   end
 
   it "can read from a csv file" do
-    expect(mcs.list_of_oids).to include "2034600"
+    expect(mcs.list_of_oids(oid_path)).to include "2034600"
   end
 
-  it "can loop through the oids and build a metadata cloud default url" do
+  it "can take an oid and build a metadata cloud default url" do
     expect(mcs.build_metadata_cloud_url("2004628")).to eq "https://metadata-api-test.library.yale.edu/metadatacloud/api/ladybird/oid/2004628?mediaType=json"
   end
 
-  it "can take a list of oids" do
-    oid_path = Rails.root.join("spec", "fixtures", "fixture_ids.csv")
-    mcs.refresh_data(oid_path)
+  it "can save a response to the local file system" do
+    mcs.save_mc_json_to_file(mc_response)
+    expect(File).to exist(new_fixture_path)
   end
 
-  it "" do
-    true
+  it "the new file that is created isn't empty.." do
+    
   end
+
 end
