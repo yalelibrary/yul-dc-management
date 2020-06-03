871cd9faa25d80a7c13efd5c4be2ba80aa83a9ad
Max Kadel <max@curationexperts.com>
2020-06-03 16:12:09 -0400

    Add basic management page so there's somewhere to land on deploy (#37)
    * Add basic management page so there's somewhere to land on deploy

---
+++ app/controllers/management_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ManagementController < ApplicationController
+  def index; end
+end
---
+++ spec/requests/management_request_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "Managements", type: :request do
+  describe "GET /" do
+    it "returns http success" do
+      get "/management/"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/management/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
