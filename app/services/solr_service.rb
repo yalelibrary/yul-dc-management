45aef225098afb5ce03b7e37be5f820ffff6a4a2
kcompsci <lakeisha.robinson@yale.edu>
2020-06-04 16:52:52 -0400

    ENV work in progress

--- app/services/solr_service.rb
+++ app/services/solr_service.rb
@@ -2,10 +2,10 @@
 
 class SolrService
   def self.connection
-    # solr_core = ENV["SOLR_CORE"] ||= "blacklight-core"
-    # solr_host = ENV["SOLR_HOST"] ||= "http://localhost:8983/solr"
-    solr_core = "blacklight-core"
-    solr_host = "http://solr:8983/solr"
+    solr_core = ENV["SOLR_CORE"] || "blacklight-core"
+    solr_host = ENV["SOLR_HOST"] || "http://localhost:8983/solr"
+    #solr_core = "blacklight-core"
+    #solr_host = "http://solr:8983/solr"
     RSolr.connect url: "#{solr_host}/#{solr_core}"
   end
 end
