b0d7e2142553e61d30f7c619fb4dd28ebee77766
kcompsci <lakeisha.robinson@yale.edu>
2020-06-04 15:50:21 -0400

    Change SOLR_URL to SOLR_HOST

--- app/services/solr_service.rb
+++ app/services/solr_service.rb
@@ -3,9 +3,9 @@
 class SolrService
   def self.connection
     # solr_core = ENV["SOLR_CORE"] ||= "blacklight-core"
-    # solr_url = ENV["SOLR_URL"] ||= "http://localhost:8983/solr"
+    # solr_host = ENV["SOLR_HOST"] ||= "http://localhost:8983/solr"
     solr_core = "blacklight-core"
-    solr_url = "http://solr:8983/solr"
-    RSolr.connect url: "#{solr_url}/#{solr_core}"
+    solr_host = "http://solr:8983/solr"
+    RSolr.connect url: "#{solr_host}/#{solr_core}"
   end
 end
