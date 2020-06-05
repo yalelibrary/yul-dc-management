e630bc234372670d20abe3e01d3c20f9f5d885b9
Max Kadel <max@curationexperts.com>
2020-06-05 14:38:16 -0400

    URI.join is having unexpected results, leaving out for now

--- app/services/solr_service.rb
+++ app/services/solr_service.rb
@@ -4,6 +4,6 @@ class SolrService
   def self.connection
     solr_core = ENV["SOLR_CORE"] ||= "blacklight-core"
     solr_base_url = ENV["SOLR_BASE_URL"] ||= "http://localhost:8983/solr"
-    RSolr.connect url: URI.join(solr_base_url,solr_core).to_s
+    RSolr.connect url: "#{solr_base_url}/#{solr_core}"
   end
 end
