cee19a988f22bac0a6205ff1a8c9083111afe508
Max Kadel <max@curationexperts.com>
2020-06-05 14:25:41 -0400

    fix replace-all error

--- app/services/solr_service.rb
+++ app/services/solr_service.rb
@@ -3,7 +3,7 @@
 class SolrService
   def self.connection
     solr_core = ENV["SOLR_CORE"] ||= "blacklight-core"
-    SOLR_BASE_URL = ENV["SOLR_BASE_URL"] ||= "http://localhost:8983/solr"
-    RSolr.connect url: "#{SOLR_BASE_URL}/#{solr_core}"
+    solr_base_url = ENV["SOLR_BASE_URL"] ||= "http://localhost:8983/solr"
+    RSolr.connect url: URI.join(solr_base_url,solr_core).to_s
   end
 end
