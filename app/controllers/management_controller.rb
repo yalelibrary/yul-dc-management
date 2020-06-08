480d36b7b4de5546651dc1d7e2f5794120a3f23d
dylan salay <dylansalay@gmail.com>
2020-06-08 11:41:05 -0700

    Add flash notice and solr task spec

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -5,5 +5,7 @@ class ManagementController < ApplicationController
 
   def run_task
     FixtureIndexingService.index_fixture_data
+    redirect_to management_index_path
+    flash[:notice] = "These files are being indexed in the background and will be ready soon."
   end
 end
--- spec/system/solr_tasks_spec.rb
+++ spec/system/solr_tasks_spec.rb
@@ -2,13 +2,21 @@
 require 'rails_helper'
 
 RSpec.describe "Solr Indexing Tasks", type: :system do
-  describe 'index' do
-    it 'redirects to the run_task page' do
+  describe 'Click solr task button' do
+    before do
       visit management_index_path
-
       click_on("Index to Solr")
+    end
+
+    it 'displays a flash message that indexing is occuring in the background' do
       expect(page.status_code).to eq(200)
-      expect(page).to have_content(/Your files have been indexed/)
+      expect(page).to have_content(/These files are being indexed in the background and will be ready soon./)
+    end
+
+    it "can index the contents of a directory to Solr" do
+      response = SolrService.connection.get 'select', params: { q: '*:*' }
+      expect(response["response"]["numFound"]).to be > 45
+      expect(response["response"]["numFound"]).to be < 101
     end
   end
 end
