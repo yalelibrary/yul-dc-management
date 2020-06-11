d0bbc17d40a40183dd18e5d31ea4d77795a40aaa
dylan salay <dylansalay@gmail.com>
2020-06-11 10:22:55 -0700

    Spec for service display

---
+++ spec/view/management_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe "Management", type: :feature do
+  describe 'index page' do
+    before do
+      ENV['REGISTRY_HOST'] = ''
+      ENV['REGISTRY_URI'] = 'yalelibraryit/dc-management'
+      ENV['MANAGEMENT_TAG'] = 'v1.2.3'
+      ENV['SOLR_TAG'] = 'v3.2.1'
+      visit management_index_path
+    end
+
+    it 'dynamically displays container services' do
+      expect(page).to have_content("Containers")
+      expect(page).to have_text("yalelibraryit/dc-management:v1.2.3")
+      expect(page).to have_content("postgres")
+      expect(page).to have_text("yalelibraryit/dc-solr:v3.2.1")
+    end
+  end
+end
