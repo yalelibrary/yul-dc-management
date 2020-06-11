dc1a3ff9594074bf6005f7013a9ad422d30c9dba
dylan salay <dylansalay@gmail.com>
2020-06-11 11:18:38 -0700

    Address PR comments

--- spec/view/management_spec.rb
+++ spec/view/management_spec.rb
@@ -4,18 +4,17 @@ require 'rails_helper'
 RSpec.describe "Management", type: :feature do
   describe 'index page' do
     before do
-      ENV['REGISTRY_HOST'] = ''
       ENV['REGISTRY_URI'] = 'yalelibraryit/dc-management'
-      ENV['MANAGEMENT_TAG'] = 'v1.2.3'
-      ENV['SOLR_TAG'] = 'v3.2.1'
+      ENV['MANAGEMENT_TAG'] = 'v1beta.gamma'
+      ENV['SOLR_TAG'] = 'v3alpha.omega'
       visit management_index_path
     end
 
     it 'dynamically displays deployed services' do
       expect(page).to have_content("Containers")
-      expect(page).to have_content("yalelibraryit/dc-management:v1.2.3")
+      expect(page).to have_content("yalelibraryit/dc-management:v1beta.gamma")
       expect(page).to have_content("postgres")
-      expect(page).to have_content("yalelibraryit/dc-solr:v3.2.1")
+      expect(page).to have_content("yalelibraryit/dc-solr:v3alpha.omega")
     end
   end
 end
