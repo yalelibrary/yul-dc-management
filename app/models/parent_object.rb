6f87b4db208fbf899e531417d52fa0b635d454f9
Max Kadel <max@curationexperts.com>
2020-06-11 09:36:39 -0400

    Describe parent object model more fully

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+# A parent object is the unit of discovery (what is represented as a single record in Blacklight).
+# It is synonymous with a parent oid in Ladybird.
 
 class ParentObject < ApplicationRecord
 end
