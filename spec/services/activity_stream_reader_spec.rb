ca9515c3f205c37c9b9bd6e08ee3d2c6e4e44861
Max Kadel <max@curationexperts.com>
2020-06-12 17:19:48 -0400

    require webmock for tests

--- spec/services/activity_stream_reader_spec.rb
+++ spec/services/activity_stream_reader_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require "rails_helper"
 require "support/time_helpers"
+require "webmock"
 
 RSpec.describe ActivityStreamReader do
   let(:asr) { described_class.new }
