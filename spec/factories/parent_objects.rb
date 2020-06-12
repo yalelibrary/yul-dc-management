b312a8e0d678cdffee06b431b432fff5bfdbbf21
Max Kadel <max@curationexperts.com>
2020-06-12 12:11:51 -0400

    Remove db:seed in CI config

--- spec/factories/parent_objects.rb
+++ spec/factories/parent_objects.rb
@@ -4,10 +4,6 @@ FactoryBot.define do
   factory :parent_object do
     oid { "MyString" }
     bib_id { "MyString" }
-    # holding_id { "MyString" }
-    # item_id { "MyString" }
-    # barcode { "MyString" }
-    # aspace_uri { "MyString" }
     last_mc_update { "2020-06-10 17:38:27" }
     factory :parent_object_with_bib_id do
       oid { "2004628" }
