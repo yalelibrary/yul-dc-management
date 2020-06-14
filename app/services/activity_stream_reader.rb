96e0018b17c03a56ffe650e1a8ec3ee389842531
Max Kadel <max@curationexperts.com>
2020-06-14 14:21:28 -0400

    Datermine whether there are new entries in the activity stream

--- app/services/activity_stream_reader.rb
+++ app/services/activity_stream_reader.rb
@@ -6,14 +6,20 @@ class ActivityStreamReader
   ##
   # Return the last time the Activity Stream was successfully read
   def last_run_time
-    last_success = ActivityStreamLog.where(status: "Success").last
-    last_success.run_time
+    @last_run_time ||= ActivityStreamLog.where(status: "Success").last.run_time.to_datetime
   end
 
-  def walk_the_stream(_last_run_time)
+  def walk_the_stream
     mcs = MetadataCloudService.new
     latest_page = mcs.mc_get("https://metadata-api-test.library.yale.edu/metadatacloud/streams/activity").body.to_s
     parsed_page = JSON.parse(latest_page)
-    parsed_page["orderedItems"].first["endTime"] # most_recent_in_stream
+    most_recent_in_stream = parsed_page["orderedItems"].first["endTime"].to_datetime
+    if most_recent_in_stream.after?(last_run_time)
+      # Make ActivityStreamEvents
+      "foo"
+    else
+      # Stop or go to next event
+      "bar"
+    end
   end
 end
--- spec/services/activity_stream_reader_spec.rb
+++ spec/services/activity_stream_reader_spec.rb
@@ -4,14 +4,14 @@ require "support/time_helpers"
 
 RSpec.describe ActivityStreamReader do
   let(:asr) { described_class.new }
-  let(:asl_success) { FactoryBot.create(:successful_activity_stream_log, run_time: 2.hours.ago) }
+  let(:asl_new_success) { FactoryBot.create(:successful_activity_stream_log, run_time: 2.hours.ago) }
   let(:asl_failed) { FactoryBot.create(:failed_activity_stream_log, run_time: 1.hour.ago) }
   let(:latest_activity_stream_page) { File.open(File.join(fixture_path, "activity_stream", "page-3.json")).read }
 
   before do
     freeze_time
-    # Apparently the mocked authorization hash matters in passing CI, so you have to provide username and password
-    # matching the mocked stub request authorization
+    # Apparently the mocked authorization hash matters in passing CI, so you have
+    # to provide username and password matching the mocked stub request authorization
     ENV["MC_USER"] = "some_username"
     ENV["MC_PW"] = "some_password"
     stub_request(:get, "https://metadata-api-test.library.yale.edu/metadatacloud/streams/activity")
@@ -36,12 +36,22 @@ RSpec.describe ActivityStreamReader do
   end
 
   it "can figure out the last time it was run successfully" do
-    asl_success
+    asl_new_success
     asl_failed
-    expect(asr.last_run_time).to eq asl_success.run_time
+    expect(asr.last_run_time).to eq asl_new_success.run_time
   end
 
-  it "can pull data from the MetadataCloud activity stream" do
-    expect(asr.walk_the_stream(2.hours.ago)).to include "2020-06-11T14:47:47"
+  context "with a last successful run older than the the fixture activity stream" do
+    let(:asl_old_success) { FactoryBot.create(:successful_activity_stream_log, run_time: DateTime.new(2020, 6, 10).in_time_zone) }
+
+    it "determines whether there is new information since the last run" do
+      asl_old_success
+      expect(asr.walk_the_stream).to eq "foo"
+    end
+
+    it "can pull data from the MetadataCloud activity stream" do
+      asl_new_success
+      expect(asr.walk_the_stream).to eq "bar"
+    end
   end
 end
