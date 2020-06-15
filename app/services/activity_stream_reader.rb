bda6396d59d6ed74f3b0791d05bd255d072fd9bf
Max Kadel <max@curationexperts.com>
2020-06-15 09:21:11 -0400

    Parse activity stream (#66)
    * Begin to implement activity stream log and reader classes
    * Begins parsing activity stream
    * Determine whether there are new entries in the activity stream

---
+++ app/services/activity_stream_reader.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class ActivityStreamReader
+  def self.update; end
+
+  ##
+  # Return the last time the Activity Stream was successfully read
+  def last_run_time
+    @last_run_time ||= ActivityStreamLog.where(status: "Success").last.run_time.to_datetime
+  end
+
+  def walk_the_stream
+    mcs = MetadataCloudService.new
+    latest_page = mcs.mc_get("https://metadata-api-test.library.yale.edu/metadatacloud/streams/activity").body.to_s
+    parsed_page = JSON.parse(latest_page)
+    most_recent_in_stream = parsed_page["orderedItems"].first["endTime"].to_datetime
+    if most_recent_in_stream.after?(last_run_time)
+      # Make ActivityStreamEvents
+      "foo"
+    else
+      # Stop or go to next event
+      "bar"
+    end
+  end
+end
