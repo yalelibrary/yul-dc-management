e5b92c5b958b4687b3494ad194c3515617fa6d1d
jzgo <jez@notch8.com>
2020-06-16 14:01:43 -0700

    Allow localhost for all environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.hosts << "localhost"
   config.hosts << ".curationexperts.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
