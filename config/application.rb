9f22f27577970639638be81c88e8050bff2045f0
Max Kadel <max@curationexperts.com>
2020-06-17 18:07:03 -0400

    remove explicit hosts for now

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,6 @@ module YulDcManagement
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.hosts << "localhost"
-    config.hosts << ".library.yale.edu"
-    config.hosts << ".curationexperts.com"
-    config.hosts << ".elb.amazonaws.com"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
