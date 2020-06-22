0de47906b25d5456d856c27d72ecf740f6770c94
Max Kadel <max@curationexperts.com>
2020-06-22 14:04:46 -0400

    Correct earlier migration error - never replace all

--- db/migrate/20200610173827_create_parent_objects.rb
+++ db/migrate/20200610173827_create_parent_objects.rb
@@ -2,9 +2,9 @@ class CreateParentObjects < ActiveRecord::Migration[6.0]
   def change
     create_table :parent_objects do |t|
       t.string :oid
-      t.string :bib
-      t.string :holding
-      t.string :item
+      t.string :bib_id
+      t.string :holding_id
+      t.string :item_id
       t.string :barcode
       t.string :aspace_uri
       t.datetime :last_mc_update
