234ce7adeec932c45d42f0e3e844938a8d508129
Max Kadel <max@curationexperts.com>
2020-06-22 09:54:18 -0400

    Correct migration (find-replace error)

--- db/migrate/20200621104105_change_id_column_name.rb
+++ db/migrate/20200621104105_change_id_column_name.rb
@@ -1,7 +1,7 @@
 class ChangeIdColumnName < ActiveRecord::Migration[6.0]
   def change
-    rename_column :parent_objects, :bib, :bib
-    rename_column :parent_objects, :holding, :holding
-    rename_column :parent_objects, :item, :item
+    rename_column :parent_objects, :bib_id, :bib
+    rename_column :parent_objects, :holding_id, :holding
+    rename_column :parent_objects, :item_id, :item
   end
 end
