d71137c63df5a1d070b7576a9148b196681d3062
Max Kadel <max@curationexperts.com>
2020-06-23 13:36:27 -0400

    Add Camerata to versions dashboard
    Connected to https://github.com/yalelibrary/YUL-DC/issues/261

--- spec/view/management_spec.rb
+++ spec/view/management_spec.rb
@@ -4,20 +4,21 @@ require 'rails_helper'
 RSpec.describe "Management", type: :feature do
   describe 'index page' do
     before do
-      ENV['MANAGEMENT_VERSION'] = 'v1beta.gamma'
-      ENV['POSTGRES_VERSION'] = 'SQLite'
+      ENV['MANAGEMENT_VERSION'] = 'v1.beta.gamma'
+      ENV['POSTGRES_VERSION'] = 'v7.delta.omicron'
       ENV['BLACKLIGHT_VERSION'] = 'v2.alpha.sigma'
       ENV['SOLR_VERSION'] = 'v3.alpha.omega'
       ENV['IIIF_IMAGE_VERSION'] = 'v4.delta.chi'
       ENV['IIIF_MANIFEST_VERSION'] = 'v5.lambda.phi'
+      ENV['CAMERATA_VERSION'] = 'v6.lambda.phi'
       visit management_index_path
     end
 
     it 'dynamically displays deployed services' do
       expect(page).to have_content("Containers")
-      expect(page).to have_content("v1beta.gamma")
+      expect(page).to have_content("v1.beta.gamma")
       expect(page).to have_content("yalelibraryit/dc-management")
-      expect(page).to have_content("SQLite")
+      expect(page).to have_content("v7.delta.omicron")
       expect(page).to have_content("yalelibraryit/dc-postgres")
       expect(page).to have_content("v2.alpha.sigma")
       expect(page).to have_content("yalelibraryit/dc-blacklight")
@@ -27,6 +28,8 @@ RSpec.describe "Management", type: :feature do
       expect(page).to have_content("yalelibraryit/dc-iiif-cantaloupe")
       expect(page).to have_content("v5.lambda.phi")
       expect(page).to have_content("yalelibraryit/dc-management")
+      expect(page).to have_content("v6.lambda.phi")
+      expect(page).to have_content("yalelibrary/yul-dc-camerata")
     end
   end
 end
