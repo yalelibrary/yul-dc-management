01662f54d1caaae5ac98046dc9109cf2ee51fd41
JP Engstrom <jpengst@gmail.com>
2020-06-24 12:35:26 -0700

    added dropdown for solr indexing (#109)
    Co-authored-by: JP Engstrom <jpengstrom@JPs-MacBook-Pro.local>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'bootstrap';
 
 //= require jquery3
 //= require popper
