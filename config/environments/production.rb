bd944fbd74e30b131a0f722b19faf4fe54b33560
Max Kadel <max@curationexperts.com>
2020-07-02 10:01:52 -0400

    Normalize db names (#126)
    * Use DB naming consistent with Blacklight application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "yul_dc_management_production"
+  # config.active_job.queue_name_prefix = "management_yul_production"
 
   config.action_mailer.perform_caching = false
 
