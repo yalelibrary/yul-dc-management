611124d6029cc3069d2b41c7f627f6282e1213e8
Max Kadel <max@curationexperts.com>
2020-07-07 09:08:55 -0400

    change name for param so metadata_source can become own model

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :metadata_samples
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'management#index'
-  get "index_to_solr/:metadata_source", to: 'management#index_to_solr', as: 'metadata_source'
+  get "index_to_solr/:ms", to: 'management#index_to_solr', as: 'ms'
 
   resources :oid_imports do
     collection { post :import }
