c7648faf32c94b159f9c3768f5d88b098c4c2bb2
mikeapp <michael.appleby@yale.edu>
2020-07-09 13:42:07 -0400

    Add an OID minter service and controller (#137)
    Add an OID minter service and controller

---
+++ app/controllers/oid_minter_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+class OidMinterController < ApplicationController
+  def generate_oids
+    number = params[:number]
+    number ||= 1
+    if an_integer?(number)
+      oids = OidMinterService.generate_oids(number.to_i)
+      respond_to do |format|
+        format.json { render json: { oids: oids } }
+        format.text { render plain: oids.join("\n") }
+        format.any  { render plain: "Please request text/plain or application/json content types.", status: :not_acceptable }
+      end
+    else
+      render plain: "Invalid request, please supply an integer value", status: :bad_request
+    end
+  end
+
+  def an_integer?(number)
+    number.to_s !~ /\D/
+  end
+end
