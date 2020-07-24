45643ea088905183ed23e7f257d8dae103f129e2
Max Kadel <max@curationexperts.com>
2020-07-24 09:32:45 -0400

    Put solr fields in alphabetical order

--- app/services/fixture_indexing_service.rb
+++ app/services/fixture_indexing_service.rb
@@ -26,69 +26,57 @@ class FixtureIndexingService
   def self.build_solr_document(id_prefix, oid, data_hash)
     {
       id: "#{id_prefix}#{oid}",
-      title_tsim: data_hash["title"],
-      # title_vern_ssim # title in the vernacular
-      # subtitle_tsim
-      # subtitle_vern_ssim # subtitle in the vernacular
-      author_ssim: data_hash["creator"],
-      author_tsim: data_hash["creator"],
-      # author_vern_ssim # author in the vernacular
-      extent_ssim: data_hash["extent"],
-      format: data_hash["format"],
-      # url_fulltext_ssim
-      url_suppl_ssim: data_hash["relatedUrl"],
-      language_ssim: data_hash["language"],
-      # published_ssim
-      # published_vern_ssim
-      # lc_callnum_ssim
-      # isbn_ssim
-      description_tesim: data_hash["description"],
       abstract_ssim: data_hash["abstract"],
       alternativeTitle_ssim: data_hash["alternativeTitle"],
       alternative_title_tsm: data_hash["alternativeTitleDisplay"],
+      author_ssim: data_hash["creator"],
+      author_tsim: data_hash["creator"],
+      box_ssim: extract_box_ssim(data_hash),
+      coordinates_ssim: data_hash["coordinate"],
+      copyrightDate_ssim: data_hash["copyrightDate"],
+      date_tsim: data_hash["date"], # Not clear what date this refers to, not in Blacklight
+      dateStructured_ssim: data_hash["dateStructured"],
+      description_tesim: data_hash["description"],
+      digital_ssim: data_hash["digital"],
+      edition_ssim: data_hash["edition"],
+      extent_ssim: data_hash["extent"],
+      extentOfDigitization_ssim: data_hash["extentOfDigitization"],
+      findingAid_ssim: data_hash["findingAid"],
+      folder_ssim: data_hash["folder"],
+      format: data_hash["format"],
       genre_ssim: data_hash["genre"],
       geo_subject_ssim: data_hash["geoSubject"],
-      resourceType_ssim: data_hash["resourceType"],
-      subjectName_ssim: data_hash["subjectName"],
-      subject_topic_tsim: data_hash["subjectTopic"],
-      extentOfDigitization_ssim: data_hash["extentOfDigitization"],
-      rights_ssim: data_hash["rights"],
-      publicationPlace_ssim: data_hash["publicationPlace"],
-      sourceCreated_ssim: data_hash["sourceCreated"],
-      publisher_ssim: data_hash["publisher"],
-      copyrightDate_ssim: data_hash["copyrightDate"],
-      source_ssim: data_hash["source"], # refers to source of metadata, e.g. Ladybird, Voyager, etc.
-      recordType_ssim: data_hash["recordType"],
-      sourceTitle_ssim: data_hash["sourceTitle"],
-      sourceDate_ssim: data_hash["sourceDate"],
-      sourceNote_ssim: data_hash["sourceNote"],
-      sourceEdition_ssim: data_hash["sourceEdition"], # Not currently in Blacklight application
-      references_ssim: data_hash["references"],
-      dateStructured_ssim: data_hash["dateStructured"],
-      # children_ssim
-      # importUrl_ssim
-      illustrative_matter_tsi: data_hash["illustrativeMatter"],
-      oid_ssim: data_hash["oid"] || oid,
       identifierMfhd_ssim: data_hash["identifierMfhd"],
       identifierShelfMark_ssim: data_hash["identifierShelfMark"],
-      box_ssim: extract_box_ssim(data_hash),
-      folder_ssim: data_hash["folder"],
-      orbisBibId_ssim: data_hash["orbisRecord"], # may change to orbisBibId
-      orbisBarcode_ssim: data_hash["orbisBarcode"] || data_hash["barcode"],
-      findingAid_ssim: data_hash["findingAid"],
-      # collectionId_ssim
-      edition_ssim: data_hash["edition"],
-      uri_ssim: data_hash["uri"],
-      partOf_ssim: data_hash["partOf"],
-      number_of_pages_ss: data_hash["numberOfPages"],
+      illustrative_matter_tsi: data_hash["illustrativeMatter"],
+      language_ssim: data_hash["language"],
       material_ssim: data_hash["material"],
-      scale_ssim: data_hash["scale"],
-      digital_ssim: data_hash["digital"],
-      coordinates_ssim: data_hash["coordinate"],
+      number_of_pages_ss: data_hash["numberOfPages"],
+      oid_ssim: data_hash["oid"] || oid,
+      orbisBarcode_ssim: data_hash["orbisBarcode"] || data_hash["barcode"],
+      orbisBibId_ssim: data_hash["orbisRecord"], # may change to orbisBibId
+      partOf_ssim: data_hash["partOf"],
       projection_ssim: data_hash["projection"],
-      date_tsim: data_hash["date"], # Not clear what date this refers to, not in Blacklight
       public_bsi: true, # TEMPORARY, makes everything public
-      visibility_ssi: extract_visibility(oid, data_hash)
+      publicationPlace_ssim: data_hash["publicationPlace"],
+      publisher_ssim: data_hash["publisher"],
+      recordType_ssim: data_hash["recordType"],
+      references_ssim: data_hash["references"],
+      resourceType_ssim: data_hash["resourceType"],
+      rights_ssim: data_hash["rights"],
+      scale_ssim: data_hash["scale"],
+      source_ssim: data_hash["source"], # refers to source of metadata, e.g. Ladybird, Voyager, etc.
+      sourceCreated_ssim: data_hash["sourceCreated"],
+      sourceDate_ssim: data_hash["sourceDate"],
+      sourceEdition_ssim: data_hash["sourceEdition"], # Not currently in Blacklight application
+      sourceNote_ssim: data_hash["sourceNote"],
+      sourceTitle_ssim: data_hash["sourceTitle"],
+      subject_topic_tsim: data_hash["subjectTopic"],
+      subjectName_ssim: data_hash["subjectName"],
+      title_tsim: data_hash["title"],
+      uri_ssim: data_hash["uri"],
+      url_suppl_ssim: data_hash["relatedUrl"],
+      visibility_ssi: extract_visibility(oid, data_hash),
     }
   end
 
