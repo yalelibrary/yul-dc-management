a2b8a8d31b0764807b7c10b85d818453f724de91
Max Kadel <max@curationexperts.com>
2020-07-24 12:01:08 -0400

    Address test warning and deprecation

--- spec/models/parent_object_spec.rb
+++ spec/models/parent_object_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe ParentObject, type: :model do
     let(:unexpected_metadata_source) { FactoryBot.create(:metadata_source, id: 4, metadata_cloud_name: "foo", display_name: "Foo", file_prefix: "F-") }
     let(:parent_object) { FactoryBot.create(:parent_object, oid: "2004628", authoritative_metadata_source: unexpected_metadata_source) }
     it "raises an error when trying to get the authoritative_json for an unexpected metadata source" do
-      expect { parent_object.authoritative_json }.to raise_error
+      expect { parent_object.authoritative_json }.to raise_error(StandardError)
     end
   end
   context "a newly created ParentObject with just the oid and default authoritative_metadata_source (Ladybird for now)" do
