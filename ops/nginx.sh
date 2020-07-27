52ed990afbab421a8c2500f7f121a2fa91a1f0b9
McClain Looney <m@loonsoft.com>
2020-07-27 15:05:50 -0500

    remove tailer job

--- ops/nginx.sh
+++ ops/nginx.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
-if [[ ! -e /var/log/nginx/error.log ]]; then
-        # The Nginx log forwarder might be sleeping and waiting
-        # until the error log becomes available. We restart it in
-        # 1 second so that it picks up the new log file quickly.
-        (sleep 1 && sv restart /etc/service/nginx-log-forwarder)
-fi
-
 if [ -z $PASSENGER_APP_ENV ]
 then
     export PASSENGER_APP_ENV=development
