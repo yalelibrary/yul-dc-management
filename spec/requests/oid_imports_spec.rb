16b3c301baadc72eb0ec5d0688cdbae1afd25f9d
dylan salay <dylansalay@gmail.com>
2020-07-28 13:15:32 -0700

    Oid Import spec

--- spec/requests/oid_imports_spec.rb
+++ spec/requests/oid_imports_spec.rb
@@ -2,16 +2,31 @@
 require 'rails_helper'
 
 RSpec.describe "OID Imports", type: :request do
-  let(:invalid_attributes) do
-    {
-      text: Rails.root + "spec/fixtures/short_fixture_ids.csv"
-    }
-  end
-
   describe "GET /new" do
     it "renders a successful response" do
       get new_oid_import_url
       expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with invalid parameters" do
+      let(:file) { fixture_file_upload('short_fixture_ids.csv', 'text/csv') }
+      before do
+        allow_any_instance_of(OidImport).to receive(:save).and_return(false) # rubocop:disable RSpec/AnyInstance
+      end
+
+      it "does not create a new OidImport" do
+        expect do
+          post oid_imports_url, params: {
+            oid_import: {
+              csv: file
+            }
+          }
+        end.to change(OidImport, :count).by(0)
+        expect(response).to be_successful
+        expect(response).not_to redirect_to(oid_imports_path)
+      end
     end
   end
 end
