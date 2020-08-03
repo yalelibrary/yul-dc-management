bb88afe0e3a107a66a47aa84c1aeefa226cfbc2c
Max Kadel <max@curationexperts.com>
2020-08-03 16:28:03 -0400

    Will not error out if there is not goobi namespace in xml (#191)
    - Pending more information on a more final Goobi xml, give more error info on file upload and don't error out, even if this won't be final form of xml

--- app/models/goobi_xml_import.rb
+++ app/models/goobi_xml_import.rb
@@ -12,6 +12,7 @@ class GoobiXmlImport < ApplicationRecord
 
   def validate_upload
     mets_doc = MetsDocument.new(@file)
+    return errors.add(:file, 'must contain a xmlns:goobi namespace') unless mets_doc.includes_goobi?
     return errors.add(:file, 'must contain an oid') if mets_doc.oid.nil? || mets_doc.oid.empty?
     return errors.add(:file, 'must be a valid Goobi METs file') unless mets_doc.valid_mets?
     return errors.add(:file, 'all image files must be available to the application') unless mets_doc.all_images_present?
