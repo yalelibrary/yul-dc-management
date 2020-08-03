2ff2614d35cd389476edffe88ca7e068eefa8c68
McClain Looney <mlooney@users.noreply.github.com>
2020-08-02 23:40:57 -0500

    Semantic Logging (#166)
    * Add support for semantic logging
    
        * rails_semantic_logger gem added & configured
        * logs to stdout, file appenders removed
        * semantic conversion suppressed in dev
        * amazing_print added to dev mode (for colorized logs)
    
    * have nginx & passenger logs go to stdout
    
    * remove tailer job
    
    Co-authored-by: Max Kadel <max@curationexperts.com>
    Co-authored-by: Mark Bussey <mark@curationexperts.com>
    Co-authored-by: Rob Kaufman <rob@notch8.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.rails_semantic_logger.semantic = false # turn off semantic logging conversion in dev
+  config.colorize_logging = true # turn on fancy colorized logs in dev
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
