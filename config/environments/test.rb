b17c9df76ad8aafba74a2b10f46145de27cac2d9
Max Kadel <max@curationexperts.com>
2020-08-05 08:13:38 -0400

    Only log errors for test environment (#194)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   ENV["SOLR_CORE"] = "blacklight-test"
 
+  config.log_level = :error
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
