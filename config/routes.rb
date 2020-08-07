b8f75c116c7a7b997060a14159c8617b591a608a
Rob Kaufman <rob@notch8.com>
2020-08-07 00:42:38 -0700

    Remove the fixture indexing to solr, only support database items being indexed.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   resources :metadata_samples
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'management#index'
-  get "index_to_solr/:metadata_source", to: 'management#index_to_solr', as: 'metadata_source'
 
   resources :oid_imports do
     collection { post :import }
