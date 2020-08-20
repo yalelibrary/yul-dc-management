730ebe13c1d92a740d8585976aa4918b1a24318c
Max Kadel <max@curationexperts.com>
2020-08-20 08:38:29 -0400

    Create manifests from METS closer to final form (#222)

--- spec/system/mets_xml_imports_spec.rb
+++ spec/system/mets_xml_imports_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 
 RSpec.describe "METS Xml Imports", type: :system, prep_metadata_sources: true do
   before do
-    stub_metadata_cloud("2012315")
+    stub_metadata_cloud("16172421")
+  end
+
+  around do |example|
+    original_path = ENV["ACCESS_MASTER_MOUNT"]
+    ENV["ACCESS_MASTER_MOUNT"] = File.join("spec", "fixtures", "goobi", "metadata")
+    example.run
+    ENV["ACCESS_MASTER_MOUNT"] = original_path
   end
 
   context "when uploading a METS xml" do
@@ -11,17 +18,18 @@ RSpec.describe "METS Xml Imports", type: :system, prep_metadata_sources: true do
       visit new_mets_xml_import_path
       expect(MetsXmlImport.count).to eq 0
       expect(ParentObject.count).to eq 0
-      page.attach_file("mets_xml_import_file", fixture_path + "/goobi/metadata/2012315/meta.xml")
+      page.attach_file("mets_xml_import_file", fixture_path + "/goobi/metadata/16172421/meta.xml")
       click_button("Import")
       expect(MetsXmlImport.count).to eq 1
       expect(ParentObject.count).to eq 1
+      expect(ParentObject.last.oid).to eq MetsXmlImport.last.oid
       expect(MetsXmlImport.last.mets_xml).not_to eq nil
       expect(MetsXmlImport.last.mets_xml).not_to be_empty
     end
 
     it "gives a success message when uploading a valid Goobi METs file" do
       visit new_mets_xml_import_path
-      page.attach_file("mets_xml_import_file", fixture_path + "/goobi/metadata/2012315/meta.xml")
+      page.attach_file("mets_xml_import_file", fixture_path + "/goobi/metadata/16172421/meta.xml")
       click_button("Import")
       expect(page.body).to include "METS xml import was successfully created."
     end
@@ -30,7 +38,7 @@ RSpec.describe "METS Xml Imports", type: :system, prep_metadata_sources: true do
   context "when uploading non-valid Goobi xml" do
     it "gives a failure message when upload fails" do
       visit new_mets_xml_import_path
-      page.attach_file("mets_xml_import_file", fixture_path + "/goobi/metadata/2012315/meta_no_image_files.xml")
+      page.attach_file("mets_xml_import_file", fixture_path + "/goobi/metadata/16172421/no_image_files.xml")
       click_button("Import")
       expect(page.body).not_to include "METS xml import was successfully created."
       expect(page.body).to include "File must be a valid METs file"
