d2f1301fb07940de2cc5be2d35cbc8c0218345ef
Max Kadel <max@curationexperts.com>
2020-08-24 14:47:19 -0400

    WIP - tests red

--- lib/yale_convert.rb
+++ lib/yale_convert.rb
@@ -3,14 +3,26 @@ require 'aws-sdk-s3'
 require 'digest'
 require 'English'
 
+# 0. Locate access_master
+# 1. Copy access_master to local workspace
+# 2. Create ptiff from local file using tiff_to_pyramid.bash
+# 3. Save ptiff to S3
+# 4. Remove local copy 
+
 class YaleConvert
   S3 = Aws::S3::Client.new(region: ENV['AWS_DEFAULT_REGION'])
 
+
+  def self.make_tempfile(input)
+    tfile = Tempfile.new(binmode: true)
+
+  end
+
   def self.convert(cksum, bucket, input)
-    temp_in = Tempfile.new(binmode: true)
-    S3.get_object(bucket: bucket, key: input) do |chunk|
-      temp_in.write chunk
-    end
+    temp_in = make_tempfile(input)
+    # S3.get_object(bucket: bucket, key: input) do |chunk|
+    #   temp_in.write chunk
+    # end
 
     check = Digest::SHA256.file(temp_in)
     raise "Checksum failed. Should be: #{check}" unless check == cksum
@@ -22,7 +34,7 @@ class YaleConvert
 
     out_key = "ptiffs/#{input.split('/').last}"
 
-    File.open(temp_out, 'r') do |f|
+    File.open(temp_out, 'rb') do |f|
       S3.put_object(bucket: bucket, key: out_key, body: f)
     end
     "s3://#{bucket}/#{out_key}"
--- spec/lib/yale_convert_spec.rb
+++ spec/lib/yale_convert_spec.rb
@@ -2,6 +2,7 @@
 require 'aws-sdk-s3'
 require 'rails_helper'
 require 'yale_convert'
+
 RSpec.describe YaleConvert do
   before do
     stub_request(:get, "https://yale-image-samples.s3.amazonaws.com/originals/1002533.tif")
@@ -10,17 +11,29 @@ RSpec.describe YaleConvert do
       .to_return(status: 200)
   end
 
+  around do |example|
+    original_temp_image_workspace = ENV['TEMP_IMAGE_WORKSPACE']
+    ENV['TEMP_IMAGE_WORKSPACE'] = 'spec/fixtures/temp_images'
+    example.run
+    ENV['TEMP_IMAGE_WORKSPACE'] = original_temp_image_workspace
+  end
+
+  it "takes a path to a file and makes a copy of it in TEMP_IMAGE_WORKSPACE" do
+    described_class.make_tempfile('spec/fixtures/images/sample.tiff')
+    expect(File.exist?("spec/fixtures/temp_images/sample.tiff")).to eq true
+  end
+
   it "checks for file checksum and fails if it doesn't match" do
     expect do
-      described_class.convert('6', 'yale-image-samples', 'originals/1002533.tif')
+      described_class.convert('6', 'yale-image-samples', 'spec/fixtures/images/sample.tiff')
     end.to(
       raise_error(RuntimeError, /\AChecksum failed\. Should be: .*\z/)
     )
   end
 
   it "downloads the image from s3 converts the image and uploads the new one to s3" do
-    expect(described_class.convert('1e56c34a73f3c936fe97836064a71209868063a954a1b653ac86ff007e04df93',
-                                   'yale-image-samples', 'originals/1002533.tif')).to eq "s3://yale-image-samples/ptiffs/1002533.tif"
+    expect(described_class.convert('5ffeed57f61bbc0e58d7d75313b552c2fcaaa6151ef745358e38d3a9212192d2',
+                                   'yale-image-samples', 'spec/fixtures/images/sample.tiff')).to eq "s3://yale-image-samples/ptiffs/1002533.tif"
   end
 
   it "bails if the shell script fails" do
