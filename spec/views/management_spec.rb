e1f887235870dfff222db40840bb14639b2f18f5
Rachel M. Kadel <rachelkg@gmail.com>
2020-08-25 13:40:44 -0400

    Add css id for management version

--- spec/views/management_spec.rb
+++ spec/views/management_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 
 RSpec.describe "Management", type: :feature do
   describe 'index page' do
-    before do
+    around do |example|
+      original_management_version = ENV['MANAGEMENT_VERSION']
+      original_postgres_version = ENV['POSTGRES_VERSION']
+      original_blacklight_version = ENV['BLACKLIGHT_VERSION']
+      original_solr_version = ENV['SOLR_VERSION']
+      original_iiif_image_version = ENV['IIIF_IMAGE_VERSION']
+      original_iiif_manifest_version = ENV['IIIF_MANIFEST_VERSION']
+      original_camerata_version = ENV['CAMERATA_VERSION']
       ENV['MANAGEMENT_VERSION'] = 'v1.beta.gamma'
       ENV['POSTGRES_VERSION'] = 'v7.delta.omicron'
       ENV['BLACKLIGHT_VERSION'] = 'v2.alpha.sigma'
@@ -11,6 +18,16 @@ RSpec.describe "Management", type: :feature do
       ENV['IIIF_IMAGE_VERSION'] = 'v4.delta.chi'
       ENV['IIIF_MANIFEST_VERSION'] = 'v5.lambda.phi'
       ENV['CAMERATA_VERSION'] = 'v6.lambda.phi'
+      example.run
+      ENV['MANAGEMENT_VERSION'] = original_management_version
+      ENV['POSTGRES_VERSION'] = original_postgres_version
+      ENV['BLACKLIGHT_VERSION'] = original_blacklight_version
+      ENV['SOLR_VERSION'] = original_solr_version
+      ENV['IIIF_IMAGE_VERSION'] = original_iiif_image_version
+      ENV['IIIF_MANIFEST_VERSION'] = original_iiif_manifest_version
+      ENV['CAMERATA_VERSION'] = original_camerata_version
+    end
+    before do
       visit root_path
     end
 
@@ -30,6 +47,9 @@ RSpec.describe "Management", type: :feature do
       expect(page).to have_content("yalelibraryit/dc-management")
       expect(page).to have_content("v6.lambda.phi")
       expect(page).to have_content("yalelibrary/yul-dc-camerata")
+    end
+    it 'Has a css id for the management version' do
+      expect(page).to have_selector("#management_version")
     end
   end
 end
