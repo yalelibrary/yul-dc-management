0a17130889ab32c2c4a490db0c3667a335c8ce5f
Bess Sadler <bess@curationexperts.com>
2020-08-27 10:19:52 -0400

    Refactor tests to match

--- app/models/iiif_presentation.rb
+++ app/models/iiif_presentation.rb
@@ -64,7 +64,7 @@ class IiifPresentation
 
   def seed
     {
-      '@id' => "#{ENV['IIIF_MANIFESTS_BASE_URL']}/#{oid}.json",
+      '@id' => "#{ENV['IIIF_MANIFESTS_BASE_URL']}/#{@oid}.json",
       'label' => @parent_object.authoritative_json["title"].first
     }
   end
