4978dee2c1027e6fd27c83f413f450d0afeeb1fe
Max Kadel <max@curationexperts.com>
2020-08-27 18:18:27 -0400

    WIP - target test red for the right reasons, but sadly they're all red :'(

--- app/models/child_object.rb
+++ app/models/child_object.rb
@@ -3,4 +3,13 @@
 class ChildObject < ApplicationRecord
   belongs_to :parent_object, foreign_key: 'parent_object_oid', class_name: "ParentObject"
   self.primary_key = 'oid'
+
+  def has_ptiff?
+    # TODO: put a real method here
+    false
+  end
+
+  def access_master_path
+    PyramidalTiffFactory.access_master_path(child_oid)
+  end
 end
