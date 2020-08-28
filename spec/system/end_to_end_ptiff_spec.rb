ddda8bd2d66aae4ad0e38834d12ed4c48437b9a0
Max Kadel <max@curationexperts.com>
2020-08-28 13:49:56 -0400

    Stub request to S3 for test

--- spec/system/end_to_end_ptiff_spec.rb
+++ spec/system/end_to_end_ptiff_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 
 RSpec.describe "Creation of PTIFFs for all ChildObjects", type: :system, prep_metadata_sources: true do
   around do |example|
+    original_image_bucket = ENV["S3_SOURCE_BUCKET_NAME"]
+    ENV["S3_SOURCE_BUCKET_NAME"] = "yale-test-image-samples"
     perform_enqueued_jobs do
       example.run
     end
+    ENV["S3_SOURCE_BUCKET_NAME"] = original_image_bucket
   end
 
   context "Child of a legacy ParentObject" do
@@ -13,6 +16,8 @@ RSpec.describe "Creation of PTIFFs for all ChildObjects", type: :system, prep_me
     let(:child_object_oid) { 1_052_760 }
     before do
       stub_metadata_cloud("2012036")
+      stub_request(:head, "https://yale-test-image-samples.s3.amazonaws.com/ptiffs/1052760.tif")
+        .to_return(status: 200)
     end
 
     it "make all the child_objects and all their ptiffs" do
