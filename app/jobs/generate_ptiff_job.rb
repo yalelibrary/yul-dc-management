38cf7bbea3f2418f99543c13ca328bad8c38a6a0
Max Kadel <max@curationexperts.com>
2020-08-31 09:23:22 -0400

    PR cleanup - don't try to do background job yet

--- app/jobs/generate_ptiff_job.rb
+++ app/jobs/generate_ptiff_job.rb
@@ -4,6 +4,6 @@ class GeneratePtiffJob < ApplicationJob
   queue_as :default
 
   def perform(child_object)
-    PyramidalTiffFactory.generate_ptiff_from(child_object)
+    # PyramidalTiffFactory.generate_ptiff_from(child_object)
   end
 end
