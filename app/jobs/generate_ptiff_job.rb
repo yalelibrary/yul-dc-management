4c8293d5fcdc28ae49d42ee5fe5da3cd52ef3697
Max Kadel <max@curationexperts.com>
2020-08-31 10:26:10 -0400

    Respond to review comments

--- app/jobs/generate_ptiff_job.rb
+++ app/jobs/generate_ptiff_job.rb
@@ -4,6 +4,12 @@ class GeneratePtiffJob < ApplicationJob
   queue_as :default
 
   def perform(child_object)
+    # Currently commented out because error handling is not yet robust enough
+    # to fully background this work. Uncomment when the background job continues to the
+    # next child_object & gives error reporting without halting the process when:
+    # - It cannot find the original_access_master
+    # - The ptiff conversion fails for any reason
+    # - The remote ptiff already exists (info, not error)
     # PyramidalTiffFactory.generate_ptiff_from(child_object)
   end
 end
