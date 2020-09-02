780511a34a8b9c7db9c2ad0c56543ae3cb351794
Martin Lovell <martin.lovell@yale.edu>
2020-09-02 10:13:12 -0400

    Refactor and add tests, do not set w/h from metadata

--- app/jobs/generate_ptiff_job.rb
+++ app/jobs/generate_ptiff_job.rb
@@ -4,10 +4,6 @@ class GeneratePtiffJob < ApplicationJob
   queue_as :default
 
   def perform(child_object)
-    conversion_information = PyramidalTiffFactory.generate_ptiff_from(child_object)
-    return unless conversion_information
-    child_object.width = conversion_information[:width]
-    child_object.height = conversion_information[:height]
-    child_object.save
+    child_object.convert_to_ptiff
   end
 end
--- spec/models/child_object_spec.rb
+++ spec/models/child_object_spec.rb
@@ -17,7 +17,37 @@ RSpec.describe ChildObject, type: :model, prep_metadata_sources: true do
     stub_metadata_cloud("2004628")
     stub_request(:head, "https://yale-test-image-samples.s3.amazonaws.com/ptiffs/456789.tif")
       .to_return(status: 200)
+    allow(PyramidalTiffFactory).to receive(:generate_ptiff_from).and_return(width: 2591, height: 4056)
     parent_object
+  end
+
+  it "does not have a height and width before conversion" do
+    expect(child_object.height).to be_nil
+    expect(child_object.width).to be_nil
+  end
+
+  context "a child object with an image that has been converted" do
+    around do |example|
+      perform_enqueued_jobs do
+        example.run
+      end
+    end
+    it "has valid height and width after jobs complete" do
+      parent_object.reload
+      first_child_object = parent_object.child_objects.first
+      expect(first_child_object.height).not_to be_nil
+      expect(first_child_object.width).not_to be_nil
+      expect(first_child_object.height).to be 4056
+      expect(first_child_object.width).to be 2591
+    end
+  end
+
+  it "has a valid height and width after conversion" do
+    expect(child_object.height).to be_nil
+    expect(child_object.width).to be_nil
+    child_object.convert_to_ptiff
+    expect(child_object.height).to be 4056
+    expect(child_object.width).to be 2591
   end
 
   it "can access the parent object" do
