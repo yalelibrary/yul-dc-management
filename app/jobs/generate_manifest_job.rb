4db8011d981f7826e9f1c76cdde7d26aca9da7fe
Max Kadel <max@curationexperts.com>
2020-09-11 16:31:33 -0400

    Remove this change - future PR

--- app/jobs/generate_manifest_job.rb
+++ app/jobs/generate_manifest_job.rb
@@ -6,6 +6,5 @@ class GenerateManifestJob < ApplicationJob
   def perform(parent_object)
     # generate iiif manifest and save it to s3
     parent_object.iiif_presentation.save
-    # parent_object.solr_index
   end
 end
