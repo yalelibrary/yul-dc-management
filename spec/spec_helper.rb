33fa860fcd20da85a08351c0dfd9c315f2096bc5
Rob Kaufman <rob@notch8.com>
2020-09-11 23:41:10 -0700

    Fix filtering on VPN for case where VPN=false and not nil

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -77,7 +77,7 @@ RSpec.configure do |config|
   #   # aliases for `it`, `describe`, and `context` that include `:focus`
   #   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
-  config.filter_run_excluding vpn_only: true if ENV['VPN'].nil?
+  config.filter_run_excluding vpn_only: true if ENV['VPN'] != 'true'
   #
   #   # Allows RSpec to persist some state between runs in order to support
   #   # the `--only-failures` and `--next-failure` CLI options. We recommend
