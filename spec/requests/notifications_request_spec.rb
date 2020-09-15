033941495155b3b9f74c2323bc01e19748bd0ac2
dylan salay <dylansalay@gmail.com>
2020-09-15 12:05:15 -0700

    failing spec quick fix

--- spec/requests/notifications_request_spec.rb
+++ spec/requests/notifications_request_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 RSpec.describe "Notifications", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/notifications/index"
-      expect(response).to have_http_status(:success)
+      get '/notifications'
+      expect(response).to redirect_to(user_session_path)
     end
   end
 end
