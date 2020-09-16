74d1c5127026d5dafc822614199c02814398c4a9
Max Kadel <max@curationexperts.com>
2020-09-16 09:20:27 -0400

    Index external image host to solr

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -150,8 +150,7 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
 
   def representative_thumbnail
     oid = child_objects.where(order: 1)&.first&.oid
-    image_host = ENV['THUMBNAIL_BASE_URL'] || ENV['IIIF_IMAGE_BASE_URL']
-    "#{image_host}/2/#{oid}/full/!200,200/0/default.jpg"
+    "#{ENV['IIIF_IMAGE_BASE_URL']}/2/#{oid}/full/!200,200/0/default.jpg"
   end
 
   def child_captions
