9f1862167399924bb8ffd3e05a99fa463d72874a
Max Kadel <max@curationexperts.com>
2020-09-22 11:22:46 -0400

    Add CAS authentication to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has the expected fields" do
+    u = described_class.new
+    u.email = "river@yale.edu"
+    u.provider = "cas"
+    u.uid = "River"
+    u.save!
+    expect(u.errors).to be_empty
+    expect(u.provider).to eq "cas"
+    expect(u.uid).to eq "River"
+  end
 end
