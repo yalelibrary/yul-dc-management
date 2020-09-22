4ca4a325af001f3a39ce23bd15ffdfe5c2bf4901
Max Kadel <max@curationexperts.com>
2020-09-22 09:24:21 -0400

    Add a couple specs to parent and child objects
    - verify what we think should be happening for common examples

--- spec/models/child_object_spec.rb
+++ spec/models/child_object_spec.rb
@@ -13,42 +13,59 @@ RSpec.describe ChildObject, type: :model, prep_metadata_sources: true do
     ENV["S3_SOURCE_BUCKET_NAME"] = original_image_bucket
   end
 
-  before do
-    stub_metadata_cloud("2004628")
-    stub_request(:head, "https://yale-test-image-samples.s3.amazonaws.com/ptiffs/89/45/67/89/456789.tif")
-      .to_return(status: 200)
-    allow(child_object.pyramidal_tiff).to receive(:valid?).and_return(true)
-    allow(child_object.pyramidal_tiff).to receive(:conversion_information).and_return(width: 2591, height: 4056)
-    parent_object
+  describe "a child object that already has a remote ptiff" do
+    let(:child_object) { described_class.create(oid: "456789", parent_object: parent_object, width: 200, height: 200) }
+    before do
+      stub_metadata_cloud("2004628")
+      stub_request(:head, "https://yale-test-image-samples.s3.amazonaws.com/ptiffs/89/45/67/89/456789.tif")
+        .to_return(status: 200)
+      stub_request(:head, "https://yale-test-image-samples.s3.amazonaws.com/originals/89/45/67/89/456789.tif")
+        .to_return(status: 200)
+      parent_object
+    end
+    it "does not try to generate the ptiff if it already has height & width and remote ptiff already exists" do
+      expect(child_object.pyramidal_tiff.valid?).to eq false
+    end
   end
 
-  it "does not have a height and width before conversion" do
-    expect(child_object.height).to be_nil
-    expect(child_object.width).to be_nil
-  end
+  describe "a child object that has successfully generated a ptiff" do
+    before do
+      stub_metadata_cloud("2004628")
+      stub_request(:head, "https://yale-test-image-samples.s3.amazonaws.com/ptiffs/89/45/67/89/456789.tif")
+        .to_return(status: 200)
+      allow(child_object.pyramidal_tiff).to receive(:valid?).and_return(true)
+      allow(child_object.pyramidal_tiff).to receive(:conversion_information).and_return(width: 2591, height: 4056)
+      parent_object
+    end
 
-  it "has a valid thumbnail url" do
-    first_child_object = parent_object.child_objects.first
-    expect(first_child_object.thumbnail_url).to eq "#{(ENV['IIIF_IMAGE_BASE_URL'])}/2/456789/full/200,/0/default.jpg"
-  end
+    it "does not have a height and width before conversion" do
+      expect(child_object.height).to be_nil
+      expect(child_object.width).to be_nil
+    end
 
-  it "has a valid height and width after conversion" do
-    expect(child_object.height).to be_nil
-    expect(child_object.width).to be_nil
-    child_object.convert_to_ptiff
-    expect(child_object.height).to be 4056
-    expect(child_object.width).to be 2591
-  end
+    it "has a valid thumbnail url" do
+      first_child_object = parent_object.child_objects.first
+      expect(first_child_object.thumbnail_url).to eq "#{(ENV['IIIF_IMAGE_BASE_URL'])}/2/456789/full/200,/0/default.jpg"
+    end
 
-  it "can access the parent object" do
-    expect(child_object.parent_object).to be_instance_of ParentObject
-  end
+    it "has a valid height and width after conversion" do
+      expect(child_object.height).to be_nil
+      expect(child_object.width).to be_nil
+      child_object.convert_to_ptiff
+      expect(child_object.height).to be 4056
+      expect(child_object.width).to be 2591
+    end
 
-  it "can return a the remote access master path" do
-    expect(child_object.remote_access_master_path).to eq "originals/89/45/67/89/456789.tif"
-  end
+    it "can access the parent object" do
+      expect(child_object.parent_object).to be_instance_of ParentObject
+    end
 
-  it "can return a the remote ptiff path" do
-    expect(child_object.remote_ptiff_path).to eq "ptiffs/89/45/67/89/456789.tif"
+    it "can return a the remote access master path" do
+      expect(child_object.remote_access_master_path).to eq "originals/89/45/67/89/456789.tif"
+    end
+
+    it "can return a the remote ptiff path" do
+      expect(child_object.remote_ptiff_path).to eq "ptiffs/89/45/67/89/456789.tif"
+    end
   end
 end
