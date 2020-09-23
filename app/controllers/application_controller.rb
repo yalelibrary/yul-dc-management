204e5ee84accb95a7c852b90e7be4e055cce397e
Max Kadel <max@curationexperts.com>
2020-09-23 14:17:25 -0400

    WIP: Fix infinite redirect
    - Failing tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to user_cas_omniauth_authorize_path
+    end
+  end
 end
