5b4a7a7658bf6cca67b1201fafac378eb28eefe6
Bess Sadler <bess@curationexperts.com>
2020-09-24 11:28:08 -0400

    Add a publicly accessible landing page
    So that we can health-check the management application during the deploy process, we need at least one page that is publicly accessible outside of CAS authentication. This opens management#index up outside of CAS, but only displays the actual dashboard if the user is authenticated. Otherwise, it tells the user they need to sign in.

--- spec/requests/management_request_spec.rb
+++ spec/requests/management_request_spec.rb
@@ -10,12 +10,14 @@ RSpec.describe "Management root path", type: :request do
         login_as(user)
         get "/"
         expect(response).to have_http_status(:success)
+        expect(response.body).to match(/Management Dashboard/)
       end
     end
     context "as an unauthenticated user" do
       it "returns http redirect" do
         get "/"
-        expect(response).to have_http_status(:redirect)
+        expect(response).to have_http_status(:success)
+        expect(response.body).to match(/You must sign in/)
       end
     end
   end
