1108bb7d3203c7f721fcc81e8ea125effcea08ae
Max Kadel <max@curationexperts.com>
2020-09-29 10:33:51 -0400

    Revert "Parent object datatable (#293)" (#304)
    This reverts commit 6d9bcd7a1841ad09ff85c7b34002aa86ab3dae93.

--- config/initializers/ajax_datatables_rails.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-AjaxDatatablesRails.configure do |config|
-  # available options for db_adapter are: :pg, :mysql, :mysql2, :sqlite, :sqlite3
-  config.db_adapter = :pg
-end
