cea20c463bb0b99e90f00fd96359bb780dcc9280
Rob Kaufman <rob@notch8.com>
2020-09-29 17:23:07 -0700

    Revert "Revert "Parent object datatable (#293)" (#304)"
    This reverts commit 1108bb7d3203c7f721fcc81e8ea125effcea08ae.

---
+++ app/datatables/parent_object_datatable.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class ParentObjectDatatable < AjaxDatatablesRails::ActiveRecord
+  def view_columns
+    # Declare strings in this format: ModelName.column_name
+    # or in aliased_join_table.column_name format
+    @view_columns ||= {
+      oid: { source: "ParentObject.oid", cond: :like },
+      authoritative_source: { source: "MetadataSource.display_name", cond: :like, searchable: true },
+      bib: { source: "ParentObject.bib", cond: :like, searchable: true },
+      holding: { source: "ParentObject.holding", cond: :like, searchable: true },
+      item: { source: "ParentObject.item", cond: :like, searchable: true },
+      barcode: { source: "ParentObject.barcode", cond: :like, searchable: true },
+      aspace_uri: { source: "ParentObject.aspace_uri", cond: :like },
+      last_ladybird_update: { source: "ParentObject.last_ladybird_update", cond: :like },
+      last_voyager_update: { source: "ParentObject.last_voyager_update", cond: :like },
+      last_aspace_update: { source: "ParentObject.last_aspace_update", cond: :like },
+      last_id_update: { source: "ParentObject.last_id_update", cond: :like },
+      visibility: { source: "ParentObject.visibility", cond: :like, searchable: true }
+    }
+  end
+
+  def data
+    records.map do |parent_object|
+      {
+        oid: parent_object.oid,
+        authoritative_source: parent_object.source_name,
+        bib: parent_object.bib,
+        holding: parent_object.holding,
+        item: parent_object.item,
+        barcode: parent_object.barcode,
+        aspace_uri: parent_object.aspace_uri,
+        last_ladybird_update: parent_object.last_ladybird_update,
+        last_voyager_update: parent_object.last_voyager_update,
+        last_aspace_update: parent_object.last_aspace_update,
+        last_id_update: parent_object.last_id_update,
+        visibility: parent_object.visibility,
+        DT_RowId: parent_object.oid
+      }
+    end
+  end
+
+  def get_raw_records # rubocop:disable Naming/AccessorMethodName
+    ParentObject.joins(:authoritative_metadata_source)
+  end
+end
