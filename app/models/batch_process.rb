b32658e02876a2147161412544217716eb3fd979
JP Engstrom <jpengstrom@vpn172022117159.its.yale.internal>
2020-10-01 12:01:54 -0700

    wip specs

--- app/models/batch_process.rb
+++ app/models/batch_process.rb
@@ -1,12 +1,31 @@
 class BatchProcess < ApplicationRecord
   attr_reader :file
   after_create :refresh_metadata_cloud
-  validate :check_file_type
+  validate :validate_import
   has_many :users
 
-  def check_file_type
+
+  def validate_import
     return if file.blank?
-    errors.add(:file, 'must be a csv') if CSV.read(file).blank?
+    if csv.present?
+      errors.add(:file, 'must be a csv') if CSV.read(file).blank?
+    elsif mets_xml.present?
+      return errors.add(:file, 'must contain an oid') if mets_doc.oid.nil? || mets_doc.oid.empty?
+      return errors.add(:file, 'must be a valid METs file') unless mets_doc.valid_mets?
+      return errors.add(:file, 'all image files must be available to the application') unless mets_doc.all_images_present?
+    else
+      return errors.add(:file, 'not a valid file type. Must be a CSV or XML.')
+    end
+  end
+
+  def kind
+    if csv.present?
+      return 'CSV'
+    elsif  mets_xml.present?
+      return 'METS'
+    else 
+      return 'Unknown'
+    end
   end
 
   def file=(value)
@@ -14,13 +33,30 @@ class BatchProcess < ApplicationRecord
     self[:csv] = value.read
   end
 
+  # stay
   def parsed_csv
     @parsed_csv ||= CSV.parse(csv, headers: true) if csv.present?
   end
 
-  def refresh_metadata_cloud
+  def mets_doc
+    @mets_doc ||= MetsDocument.new(mets_xml) if mets_xml.present?
+  end
+
+  def refresh_metadata_cloud_csv
     oids = parsed_csv.entries.map { |r| r['oid'] }
     metadata_sources = parsed_csv.entries.map { |r| r['Source'] }
     MetadataCloudService.create_parent_objects_from_oids(oids, metadata_sources)
+  end
+
+  def refresh_metadata_cloud_mets
+    MetadataCloudService.create_parent_objects_from_oids([mets_doc.oid], ['ladybird']) # TODO: make 'ladybird' a metadata source attribute on this object
+  end
+
+  def refresh_metadata_cloud
+    if csv.present?
+      refresh_metadata_cloud_csv
+    elsif mets_xml.present?
+      refresh_metadata_cloud_mets
+    end
   end
 end
--- spec/models/batch_process_spec.rb
+++ spec/models/batch_process_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe BatchProcess, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe BatchProcess, type: :model, prep_metadata_sources: true do
+  subject(:batch_process) { described_class.new }
+
+  before do
+    stub_metadata_cloud("2034600")
+    stub_metadata_cloud("2046567")
+    stub_metadata_cloud("16414889")
+    stub_metadata_cloud("14716192")
+    stub_metadata_cloud("16854285")
+  end
+
+  describe "csv file import" do
+    it "requires no attributes" do
+      expect(BatchProcess.new).to be_valid
+    end
+
+    it "accepts a csv file as a virtual attribute and read the csv into the csv property" do
+      batch_process.file = File.new(fixture_path + '/short_fixture_ids.csv')
+      expect(batch_process.csv).to be_present
+      expect(batch_process).to be_valid
+    end
+
+    it "does not accept non csv files" do
+      batch_process.file = File.new(Rails.root.join('public', 'favicon.ico'))
+      expect(batch_process).not_to be_valid
+      expect(batch_process.csv).to be_blank
+    end
+
+  end
+
+
 end
