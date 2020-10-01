600e07da82ad78e6ca92ef3db3f9bf12fff03b88
dylan salay <dylansalay@gmail.com>
2020-10-01 10:35:28 -0700

    Fix batch process request specs

--- app/controllers/batch_processes_controller.rb
+++ app/controllers/batch_processes_controller.rb
@@ -9,7 +9,7 @@ class BatchProcessesController < ApplicationController
   end
 
   def create
-    @batch_process = BatchProcess.new(batch_process_params.merge(created_by_id: current_user.uid))
+    @batch_process = BatchProcess.new(batch_process_params.merge(created_by: current_user.uid))
     respond_to do |format|
       if @batch_process.save
         format.html { redirect_to batch_processes_path, notice: "Your records have been retrieved from the MetadataCloud. PTIFF generation, manifest generation and indexing happen in the background." }
@@ -24,6 +24,4 @@ class BatchProcessesController < ApplicationController
     def batch_process_params
       params.require(:batch_process).permit(:file)
     end
-
-  # Use callbacks to share common setup or constraints between actions.
 end
--- spec/requests/batch_processes_request_spec.rb
+++ spec/requests/batch_processes_request_spec.rb
@@ -1,26 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "BatchProcesses", type: :request do
-
-  xdescribe "GET /index" do
-    it "returns http success" do
-      get "/batch_processes/index"
-      expect(response).to have_http_status(:success)
-    end
+  let(:user) { FactoryBot.create(:user) }
+  before do
+    login_as user
   end
 
-  xdescribe "GET /new" do
+  describe "GET /new" do
     it "returns http success" do
       get "/batch_processes/new"
       expect(response).to have_http_status(:success)
     end
   end
 
-  xdescribe "GET /create" do
-    it "returns http success" do
-      get "/batch_processes/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    context "with invalid parameters" do
+      let(:file) { fixture_file_upload('short_fixture_ids.csv', 'text/csv') }
+      before do
+        allow_any_instance_of(BatchProcess).to receive(:save).and_return(false) # rubocop:disable RSpec/AnyInstance
+      end
+
+      it "does not create a new BatchProcess" do
+        expect do
+          post batch_processes_url, params: {
+            batch_process: {
+              csv: file
+            }
+          }
+        end.to change(BatchProcess, :count).by(0)
+        expect(response).to be_successful
+        expect(response).not_to redirect_to(batch_processes_path)
+      end
     end
   end
-
 end
