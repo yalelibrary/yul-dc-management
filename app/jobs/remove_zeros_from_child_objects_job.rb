22f98f6548ff58f98e2b8165cd7d1a8db39b154a
Rob Kaufman <rob@notch8.com>
2020-10-20 12:56:28 -0700

    Update child objects in background to not block deployment (#356)
    * Make child cleanup a background job so it does not block deployment
    
    * Remove unused docker-compose file.

---
+++ app/jobs/remove_zeros_from_child_objects_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# TODO: this is a onetime thing for a data migration
+# it can be removed after 12/1/2020
+class RemoveZerosFromChildObjectsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    ChildObject.where("height < ? OR width < ?", 1, 1).find_each do |child|
+      child.height = nil
+      child.width = nil
+      child.ptiff_conversion_at = nil
+      child.parent_object.generate_manifest = true
+      child.parent_object.solr_delete
+      GeneratePtiffJob.perform_later(child)
+    end
+  end
+end
