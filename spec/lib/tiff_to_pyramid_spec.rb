539d09fc9553bcbca4a5d1f10ecda19d337b9ebc
McClain Looney <mlooney@users.noreply.github.com>
2020-10-20 14:33:04 -0500

    Fix unknown flag in vipsthumbnail error (#353)
    * Fix unknown flag in vipsthumbnail error

---
+++ spec/lib/tiff_to_pyramid_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require "rails_helper"
+require "support/time_helpers"
+require "English"
+
+RSpec.describe Class do
+  context "image conversion" do
+    it "do not fail with this weird image" do
+      Dir.mktmpdir do |d|
+        expect(`app/lib/tiff_to_pyramid.bash #{d} spec/fixtures/images/bad_flag.tiff #{d}/output.tiff`).to match(/Pyramid width: 1490\nPyramid height: 1525/)
+        expect($CHILD_STATUS).to eq 0
+      end
+    end
+  end
+end
