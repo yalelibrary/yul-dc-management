739f19b62621ca994b99c6248d632ad166729c96
McClain Looney <m@loonsoft.com>
2020-10-20 12:17:58 -0500

    Make rubocop happy again

--- spec/lib/tiff_to_pyramid_spec.rb
+++ spec/lib/tiff_to_pyramid_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 require "rails_helper"
 require "support/time_helpers"
+require "English"
 
-RSpec.describe "PtiffScript" do
-
+RSpec.describe Class do
   context "image conversion" do
-    it "should not fail with this weird image" do
+    it "do not fail with this weird image" do
       Dir.mktmpdir do |d|
         expect(`app/lib/tiff_to_pyramid.bash #{d} spec/fixtures/images/bad_flag.tiff #{d}/output.tiff`).to match /Pyramid width: 1490\nPyramid height: 1525/
-        expect($?).to eq 0
+        expect($CHILD_STATUS).to eq 0
       end
     end
   end
