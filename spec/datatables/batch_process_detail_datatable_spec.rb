5ee268a744e4975f564265f264b32a900c806b2e
Alisha Evans <alisha@notch8.com>
2020-10-29 10:13:59 -0700

    start the detail datatable spec

---
+++ spec/datatables/batch_process_detail_datatable_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BatchProcessDetailDatatable, type: :datatable, prep_metadata_sources: true do
+  columns = ['id', 'parent_oid', 'time']
+end
