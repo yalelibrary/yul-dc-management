ad65b91f36d9774fcbf374fce20afb016acb3cb8
Max Kadel <max@curationexperts.com>
2020-10-29 17:12:09 -0400

    Limit to legit failing test

--- spec/system/batch_process_detail_spec.rb
+++ spec/system/batch_process_detail_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "Batch Process detail page", type: :system, prep_metadata_sources: true do
+RSpec.describe "Batch Process detail page", type: :system, prep_metadata_sources: true, js: true do
   let(:user) { FactoryBot.create(:user, uid: "johnsmith2530") }
   before do
     stub_metadata_cloud("2004628")
@@ -24,14 +24,12 @@ RSpec.describe "Batch Process detail page", type: :system, prep_metadata_sources
       )
     end
     it "can see the details of the import" do
-      expect(page.body).to include batch_process.id.to_s
-      expect(page.body).to include "johnsmith2530"
-      expect(page.body).to have_link("small_short_fixture_ids.csv", href: "/batch_processes/#{batch_process.id}/download")
-      expect(page.body).to have_link('16057779', href: "/batch_processes/#{batch_process.id}/parent_objects/16057779")
-      within all("td.child_count")[3] do
-        expect(page.body).to include "4"
-      end
-      expect(page.body).to include "2020-10-08 14:17:01"
+      expect(page).to have_content(batch_process.id.to_s)
+      expect(page).to have_content("johnsmith2530")
+      expect(page).to have_link("small_short_fixture_ids.csv", href: "/batch_processes/#{batch_process.id}/download")
+      expect(page).to have_link('16057779', href: "/batch_processes/#{batch_process.id}/parent_objects/16057779")
+      expect(page).to have_content("4")
+      expect(page).to have_content("2020-10-08 14:17:01")
     end
     context "deleting a parent object" do
       before do
@@ -43,13 +41,9 @@ RSpec.describe "Batch Process detail page", type: :system, prep_metadata_sources
       end
 
       it "can still see the details of the import" do
-        expect(page.body).to include batch_process.id.to_s
-        within all("td.parent_oid")[3] do
-          expect(page.body).to include('16057779')
-        end
-        within all("td.child_count")[3] do
-          expect(page.body).to include('(deleted)')
-        end
+        expect(page).to have_content(batch_process.id.to_s)
+        expect(page).to have_content('16057779')
+        expect(page).to have_content('(deleted)')
       end
     end
   end
@@ -68,9 +62,7 @@ RSpec.describe "Batch Process detail page", type: :system, prep_metadata_sources
       expect(page.body).to include batch_process.id.to_s
       expect(page.body).to include "johnsmith2530"
       expect(page.body).to have_link("meta.xml", href: "/batch_processes/#{batch_process.id}/download")
-      within "td.parent_oid" do
-        expect(page.body).to have_link('16172421', href: "/batch_processes/#{batch_process.id}/parent_objects/16172421")
-      end
+      expect(page.body).to have_link('16172421', href: "/batch_processes/#{batch_process.id}/parent_objects/16172421")
       expect(page.body).to include "2020-10-08 16:17:01"
     end
   end
