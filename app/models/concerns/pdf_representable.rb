b27d94eb045cdf81916a00f1219bcc30e84ec1c3
Martin Lovell <martin.lovell@yale.edu>
2020-11-09 12:47:07 -0500

    Capture output using Open3

--- app/models/concerns/pdf_representable.rb
+++ app/models/concerns/pdf_representable.rb
@@ -9,8 +9,9 @@ module PdfRepresentable
     temp_json_file.write(pdf_generator_json)
     temp_json_file.close
     temp_pdf_file = "#{temp_json_file.path}.pdf"
-    output = `java -jar jpegs2pdf-1.0.jar "#{temp_json_file.path}" "#{temp_pdf_file}" 2>&1`
-    success = $CHILD_STATUS.success?
+    cmd = "java -jar jpegs2pdf-1.0.jar #{temp_json_file.path} #{temp_pdf_file}"
+    _, stderr, status = Open3.capture3(cmd)
+    success = status.exitstatus.zero?
     temp_json_file.delete
     if success
       raise "Java app did not create PDF file for #{oid}" unless File.exist? temp_pdf_file
@@ -18,7 +19,7 @@ module PdfRepresentable
       File.delete temp_pdf_file
     else
       File.delete temp_pdf_file if File.exist?(temp_pdf_file)
-      raise "PDF Java app returned non zero response code for #{oid}: #{output}"
+      raise "PDF Java app returned non zero response code for #{oid}: #{stderr}"
     end
   end
 
