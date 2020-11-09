93f34dca6f442c382b129b05973549ada6c1d9b7
Martin Lovell <martin.lovell@yale.edu>
2020-11-09 12:39:29 -0500

    Update pdf job to take batch_connection

--- app/jobs/generate_pdf_job.rb
+++ app/jobs/generate_pdf_job.rb
@@ -7,7 +7,7 @@ class GeneratePdfJob < ApplicationJob
     50
   end
 
-  def perform(parent_object, current_batch_process = parent_object.current_batch_process, current_batch_connection = child_object.parent_object.current_batch_connection)
+  def perform(parent_object, current_batch_process = parent_object.current_batch_process, current_batch_connection = parent_object.current_batch_connection)
     parent_object.current_batch_process = current_batch_process
     parent_object.current_batch_connection = current_batch_connection
     parent_object.generate_pdf
