ac3111e150ab705e099b7c87fa4268a7637fb245
Alisha Evans <alisha@notch8.com>
2020-11-10 10:29:44 -0800

    parent data table spec passing

--- spec/datatables/batch_process_parent_datatable_spec.rb
+++ spec/datatables/batch_process_parent_datatable_spec.rb
@@ -3,9 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe BatchProcessParentDatatable, type: :datatable, prep_metadata_sources: true do
+  let(:user) { FactoryBot.create(:user, uid: 'johnsmith2530') }
   columns = ['child_oid', 'time', 'status']
 
   before do
+    stub_metadata_cloud('16057779')
+    stub_ptiffs_and_manifests
     login_as user
   end
 
@@ -20,7 +23,6 @@ RSpec.describe BatchProcessParentDatatable, type: :datatable, prep_metadata_sour
     let(:csv_upload) { Rack::Test::UploadedFile.new(Rails.root.join(fixture_path, 'short_fixture_ids.csv')) }
 
     it 'renders a complete data table' do
-      user = FactoryBot.create(:user)
       parent_object = FactoryBot.create(:parent_object, oid: 16_057_779)
       child_object = FactoryBot.create(:child_object, oid: 456_789, parent_object: parent_object)
       batch_process = FactoryBot.create(:batch_process, user: user)
@@ -34,7 +36,7 @@ RSpec.describe BatchProcessParentDatatable, type: :datatable, prep_metadata_sour
       expect(output).to include(
         DT_RowId: child_object.oid,
         child_oid: "<a href='/batch_processes/#{batch_process.id}/parent_objects/#{parent_object.oid}/child_objects/#{child_object.oid}'>#{child_object.oid}</a>",
-        status: 'In progress - no failures',
+        status: 'Pending',
         time: child_object.created_at
       )
     end
