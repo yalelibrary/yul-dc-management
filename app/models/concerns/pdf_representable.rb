64ee0df0bff0b7993ccb31d77cc7a07d89fad785
Martin Lovell <martin.lovell@yale.edu>
2020-11-16 12:20:04 -0500

    Fix issue with null exitstatus

--- app/models/concerns/pdf_representable.rb
+++ app/models/concerns/pdf_representable.rb
@@ -10,8 +10,8 @@ module PdfRepresentable
     temp_json_file.close
     temp_pdf_file = "#{temp_json_file.path}.pdf"
     cmd = "java -jar jpegs2pdf-1.0.jar #{temp_json_file.path} #{temp_pdf_file}"
-    _, stderr, status = Open3.capture3(cmd)
-    success = status.exitstatus.zero?
+    stdout, stderr, status = Open3.capture3(cmd)
+    success = status.success?
     temp_json_file.delete
     if success
       raise "Java app did not create PDF file for #{oid}" unless File.exist? temp_pdf_file
@@ -19,7 +19,7 @@ module PdfRepresentable
       File.delete temp_pdf_file
     else
       File.delete temp_pdf_file if File.exist?(temp_pdf_file)
-      raise "PDF Java app returned non zero response code for #{oid}: #{stderr}"
+      raise "PDF Java app returned non zero response code for #{oid}: #{stderr} #{stdout}"
     end
   end
 
