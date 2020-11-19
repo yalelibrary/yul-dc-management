a044042ef1092c6fd22c9266b4a7fa4d6b1b82b9
Max Kadel <max@curationexperts.com>
2020-11-19 16:44:37 -0500

    Make sure batch process & child object notes are connected
    Co-Authored-By: Rachel Kadel <rachel.kadel@curationexperts.com>

--- app/jobs/setup_metadata_job.rb
+++ app/jobs/setup_metadata_job.rb
@@ -11,9 +11,9 @@ class SetupMetadataJob < ApplicationJob
     parent_object.create_child_records(current_batch_process, current_batch_connection)
     parent_object.save!
     parent_object.processing_event("Child object records have been created", "child-records-created", current_batch_process, current_batch_connection)
-    parent_object.child_objects.each do |c|
-      GeneratePtiffJob.perform_later(c, current_batch_process, current_batch_connection)
-      c.processing_event("Ptiff Queued", "ptiff-queued", current_batch_process, current_batch_connection)
+    parent_object.child_objects.each do |child|
+      GeneratePtiffJob.perform_later(child, current_batch_process, current_batch_connection)
+      child.processing_event("Ptiff Queued", "ptiff-queued", current_batch_process, current_batch_connection)
     end
   rescue => e
     parent_object.processing_event("Setup job failed to save: #{e.message}", "failed", current_batch_process, current_batch_connection)
