7bb129b23a302b7c22a7ff6d7eb6bd39220dd1d1
Max Kadel <max@curationexperts.com>
2020-12-04 11:20:03 -0500

    Correct previous migration so that it doesn't create any incorrect data

--- db/migrate/20201204160824_timestamp_default_fix.rb
+++
@@ -1,6 +0,0 @@
-class TimestampDefaultFix < ActiveRecord::Migration[6.0]
-  def change
-    change_column_default(:child_objects, :created_at, from: "2020-12-03 20:53:18", to: -> { 'CURRENT_TIMESTAMP' })
-    change_column_default(:child_objects, :updated_at, from: "2020-12-03 20:53:19", to: -> { 'CURRENT_TIMESTAMP' })
-  end
-end
