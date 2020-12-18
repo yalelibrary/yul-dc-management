f366e4b851af92d99dc25fa168f3e9142c5893ad
Max Kadel <max@curationexperts.com>
2020-12-18 10:38:21 -0500

    Add attribution to IIIF Manifest

--- app/models/iiif_presentation.rb
+++ app/models/iiif_presentation.rb
@@ -40,7 +40,8 @@ class IiifPresentation
     return @manifest if @manifest
     @manifest = IIIF::Presentation::Manifest.new(seed)
     @manifest["rendering"] = rendering
-    @manifest['metadata'] = metadata
+    @manifest["metadata"] = metadata
+    @manifest["attribution"] = "Yale University Libraries Digital Collections"
     @manifest.sequences << sequence
     add_canvases_to_sequence(@manifest.sequences.first)
     @manifest
