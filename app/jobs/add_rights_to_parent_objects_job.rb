270b87ef3d15c367037ae4d32d8cc0586c856dcb
Max Kadel <max@curationexperts.com>
2021-01-06 14:44:38 -0500

    Use default queue
    Co-Authored-By: Rachel Kadel <rachel.kadel@curationexperts.com>

--- app/jobs/add_rights_to_parent_objects_job.rb
+++ app/jobs/add_rights_to_parent_objects_job.rb
@@ -3,7 +3,7 @@
 # TODO: this is a onetime thing for a data migration
 # it can be removed after 3/1/2021
 class AddRightsToParentObjectsJob < ApplicationJob
-  queue_as :rights
+  queue_as :default
 
   def perform(*_args)
     ParentObject.where(rights_statement: nil).find_each do |parent|
