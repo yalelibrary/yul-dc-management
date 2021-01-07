c5176fcce53bce248b3e6fadb7c04870671ce18a
Max Kadel <max@curationexperts.com>
2021-01-07 14:53:24 -0500

    Make one-off job high priority

--- app/jobs/add_rights_to_parent_objects_job.rb
+++ app/jobs/add_rights_to_parent_objects_job.rb
@@ -5,6 +5,10 @@
 class AddRightsToParentObjectsJob < ApplicationJob
   queue_as :default
 
+  def default_priority
+    -100
+  end
+
   def perform(*_args)
     ParentObject.where(rights_statement: nil).find_each do |parent|
       parent.rights_statement = parent.ladybird_json["rights"]&.first
