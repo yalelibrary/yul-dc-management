4949fd1a0a19303eb1c59f4525cac94c279726d9
Martin Lovell <martin.lovell@yale.edu>
2021-01-11 15:23:06 -0500

    Respond to JSON request with duplicate oid error

--- app/controllers/parent_objects_controller.rb
+++ app/controllers/parent_objects_controller.rb
@@ -28,7 +28,10 @@ class ParentObjectsController < ApplicationController
   # POST /parent_objects.json
   def create
     if ParentObject.exists?(oid: parent_object_params[:oid])
-      redirect_to new_parent_object_path, flash: { alert: "The oid already exists: [#{parent_object_params[:oid]}]" }
+      respond_to do |format|
+        format.html { redirect_to new_parent_object_path, flash: { alert: "The oid already exists: [#{parent_object_params[:oid]}]" } }
+        format.json { render json: {error:"The oid already exists: [#{parent_object_params[:oid]}]"}, status: :unprocessable_entity }
+      end
       return
     end
     @parent_object = ParentObject.new(parent_object_params)
