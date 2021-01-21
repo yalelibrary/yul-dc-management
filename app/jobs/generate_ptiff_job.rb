be5edaf7dc73f8c7bf02d001ddd76016a24431ca
Max Kadel <max@curationexperts.com>
2021-01-21 09:48:16 -0500

    Turn off copying to access master pair-tree per Mike Appleby

--- app/jobs/generate_ptiff_job.rb
+++ app/jobs/generate_ptiff_job.rb
@@ -10,7 +10,7 @@ class GeneratePtiffJob < ApplicationJob
   def perform(child_object, current_batch_process, current_batch_connection = child_object.parent_object.current_batch_connection)
     child_object.parent_object.current_batch_process = current_batch_process
     child_object.parent_object.current_batch_connection = current_batch_connection
-    child_object.copy_to_access_master_pairtree if child_object.parent_object.from_mets == true
+    # child_object.copy_to_access_master_pairtree if child_object.parent_object.from_mets == true
     child_object.convert_to_ptiff!
     # Only generate manifest if all children are ready
     GenerateManifestJob.perform_later(child_object.parent_object, current_batch_process, current_batch_connection) if child_object.parent_object.needs_a_manifest?
