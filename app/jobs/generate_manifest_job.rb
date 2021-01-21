970a436ac110d8d449e73cd8f673ee26c2dc7a7d
Rachel M. Kadel <rachel.kadel@curationxperts.com>
2021-01-21 17:03:27 -0500

    Safe navigation
    Co-Authored-By: Max Kadel <max@curationexperts.com>

--- app/jobs/generate_manifest_job.rb
+++ app/jobs/generate_manifest_job.rb
@@ -32,7 +32,7 @@ class GenerateManifestJob < ApplicationJob
 
   def index_to_solr(parent_object, current_batch_process = parent_object.current_batch_process, current_batch_connection = parent_object.current_batch_connection)
     result = parent_object.solr_index
-    if (result["responseHeader"]["status"]).zero?
+    if (result&.[]("responseHeader")&.[]("status"))&.zero?
       parent_object.processing_event("Solr index updated", "solr-indexed", current_batch_process, current_batch_connection)
     else
       parent_object.processing_event("Solr index after manifest generation failed", "failed", current_batch_process, current_batch_connection)
