6528a4f4a3a45e3e3c204c440c55c50961fc03e6
Martin Lovell <martin.lovell@yale.edu>
2021-01-27 11:28:36 -0500

    update test

--- spec/models/iiif_presentation_spec.rb
+++ spec/models/iiif_presentation_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe IiifPresentation, prep_metadata_sources: true do
       expect(iiif_presentation.manifest["metadata"].last["label"]).to eq "Source"
       expect(iiif_presentation.manifest["metadata"].last["value"].first).to include 'ladybird'
       expect(iiif_presentation.manifest["metadata"].select { |k| true if k["label"] == "Orbis Bib ID" }).not_to be_empty
-      expect(iiif_presentation.manifest["metadata"][12]["value"].first).to eq 'Box 12, Folder 117'
+      expect(iiif_presentation.manifest["metadata"].select { |k| true if k["label"] == "Container / Volume Information" }.first["value"].first).to eq 'Box 12, Folder 117'
     end
 
     it "has a rendering in the sequence" do
