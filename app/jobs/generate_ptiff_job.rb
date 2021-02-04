60a921b38f2ebd2b445a7496c58a5e00849a176b
Max Kadel <max@curationexperts.com>
2021-02-04 12:31:15 -0500

    re-enable copying to pair-tree for Goobi imports

--- app/jobs/generate_ptiff_job.rb
+++ app/jobs/generate_ptiff_job.rb
@@ -13,7 +13,7 @@ class GeneratePtiffJob < ApplicationJob
     # TODO: Uncomment the line below to re-implement Goobi ingest and copy to the access master pair-tree.
     # There is a test in  spec/models/batch_process_spec.rb that should fail, take out the "pending" and logger
     # mocks there to see if it passes
-    # child_object.copy_to_access_master_pairtree if child_object.parent_object.from_mets
+    child_object.copy_to_access_master_pairtree if child_object.parent_object.from_mets
     child_object.convert_to_ptiff!
     # Only generate manifest if all children are ready
     GenerateManifestJob.perform_later(child_object.parent_object, current_batch_process, current_batch_connection) if child_object.parent_object.needs_a_manifest?
