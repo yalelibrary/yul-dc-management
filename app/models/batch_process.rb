428ac9ffb556e2c716b2e51f93eabd2eb4d4c83f
Max Kadel <max@curationexperts.com>
2021-02-06 18:42:49 -0500

    Dang it, rubocop

--- app/models/batch_process.rb
+++ app/models/batch_process.rb
@@ -30,7 +30,7 @@ class BatchProcess < ApplicationRecord # rubocop:disable Metrics/ClassLength
     @file = value
     self[:file_name] = file.original_filename
     if File.extname(file) == '.csv'
-      # Remove BOM if present 
+      # Remove BOM if present
       self[:csv] = CSV.open(value, 'rb:bom|utf-8', headers: true, return_headers: true).read
     elsif File.extname(file) == '.xml'
       self[:mets_xml] = value.read
