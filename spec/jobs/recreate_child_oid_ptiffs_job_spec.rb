170615db9af774b8e9d862b1551e9696f1fba07f
Martin Lovell <martin.lovell@yale.edu>
2021-02-08 16:27:41 -0500

    Added new batch type to force recreate Ptiff for a list of child oids

---
+++ spec/jobs/recreate_child_oid_ptiffs_job_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RecreateChildOidPtiffsJob, type: :job do
+  def queue_adapter_for_test
+    ActiveJob::QueueAdapters::DelayedJobAdapter.new
+  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:batch_process) { FactoryBot.create(:batch_process, user: user, batch_action: 'recreate child oid ptiffs') }
+  let(:metadata_source) { FactoryBot.create(:metadata_source) }
+  let(:parent_object) { FactoryBot.create(:parent_object, oid: 2_004_628, authoritative_metadata_source: metadata_source) }
+  let(:child_object) { FactoryBot.create(:child_object, oid: 456_789, parent_object: parent_object) }
+  let(:recreate_child_oid_ptiffs_job) { RecreateChildOidPtiffsJob.new }
+
+  around do |example|
+    original_image_bucket = ENV["S3_SOURCE_BUCKET_NAME"]
+    ENV["S3_SOURCE_BUCKET_NAME"] = "not-a-real-bucket"
+    example.run
+    ENV["S3_SOURCE_BUCKET_NAME"] = original_image_bucket
+  end
+
+  before do
+    stub_request(:head, "https://not-a-real-bucket.s3.amazonaws.com/originals/89/45/67/89/456789.tif")
+        .to_return(status: 200, body: "", headers: {})
+    stub_request(:head, "https://not-a-real-bucket.s3.amazonaws.com/ptiffs/89/45/67/89/456789.tif")
+        .to_return(status: 200, body: "", headers: {})
+    allow(batch_process).to receive(:oids).and_return(['456789'])
+    child_object
+  end
+
+  describe 'recreate ptiff job' do
+    it "has correct priority" do
+      expect(recreate_child_oid_ptiffs_job.default_priority).to eq(9)
+    end
+    it "has correct queue" do
+      expect(recreate_child_oid_ptiffs_job.queue_name).to eq('default')
+    end
+    it "can will create appropriate number of ptiff jobs when run" do
+      expect do
+        recreate_child_oid_ptiffs_job.perform(batch_process)
+      end.to change { Delayed::Job.where(queue: 'ptiff').count }.by(1)
+    end
+  end
+end
