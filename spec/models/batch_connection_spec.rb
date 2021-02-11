5e5e4edbbbdc5eaac9b157ce693dba502956bbe5
dylan salay <dylansalay@gmail.com>
2021-02-11 14:33:40 -0800

    spec fixes

--- spec/models/batch_connection_spec.rb
+++ spec/models/batch_connection_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe BatchConnection, type: :model, prep_metadata_sources: true do
     batch_process.file = csv_upload
     batch_process.save!
     batch_process.run_callbacks :create
-    expect(batch_process.batch_connections.count).to eq 5
+    expect(batch_process.batch_connections.count).to eq 218
     po = ParentObject.find(2_034_600)
     bp = BatchProcess.find(batch_process.id)
     expect(po.batch_connections).not_to eq nil
