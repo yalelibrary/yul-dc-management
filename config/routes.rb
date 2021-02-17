cc4630e3dc977359474bf6f45fa96f89567e2664
dylan salay <dylansalay@gmail.com>
2021-02-17 08:27:46 -0800

    Generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/index'
   resources :batch_processes do
     collection { post :import }
     member do
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    login_as user
+  end
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/users/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
