82c7ffc42b974f0d81570b6b2ec99bc30e0d8c03
dylan salay <dylansalay@gmail.com>
2021-02-22 15:17:58 -0800

    Add system spec to deactivate user

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -3,22 +3,24 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :system, js: true do
   let(:user) { FactoryBot.create(:user) }
-  let(:user2) { FactoryBot.create(:user, deactivated: true) }
+  let(:user2) { FactoryBot.create(:user) }
+
   before do
     login_as user
-    visit users_path
   end
 
   describe 'datatable' do
     it 'defaults to only display Active users' do
       user2.reload
+      visit users_path
       expect(page).to have_content(user.uid)
-      expect(page).to have_no_content(user2.uid)
+      expect(page).to have_content(user2.uid)
     end
   end
 
   describe 'are editable' do
     it 'and require an email to be present' do
+      visit users_path
       click_on('Edit')
       fill_in('Email', with: '')
       click_on('Update User')
@@ -27,6 +29,16 @@ RSpec.describe 'Users', type: :system, js: true do
 
       expect(message).to eq 'Please fill out this field.'
       expect(current_path).to eq edit_user_path(user.id)
+    end
+
+    it 'and can be deactivated' do
+      visit edit_user_path(user2.id)
+      page.check('Deactivated')
+      click_on('Update User')
+      visit users_path
+
+      expect(page).to have_content(user.uid)
+      expect(page).to have_no_content(user2.uid)
     end
   end
 end
