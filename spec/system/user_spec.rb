c8d0f377a48dea72e0ba7de10f9015163d2e081a
Alisha Evans <alisha@notch8.com>
2021-02-22 12:38:00 -0800

    editing a user validates that an email is present

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "ChildObjects", type: :system, js: true do
+RSpec.describe 'Users', type: :system, js: true do
   let(:user) { FactoryBot.create(:user) }
   let(:user2) { FactoryBot.create(:user, deactivated: true) }
   before do
@@ -9,11 +9,24 @@ RSpec.describe "ChildObjects", type: :system, js: true do
     visit users_path
   end
 
-  describe "users datatable" do
-    it "defaults to only display Active users" do
+  describe 'datatable' do
+    it 'defaults to only display Active users' do
       user2.reload
       expect(page).to have_content(user.uid)
       expect(page).to have_no_content(user2.uid)
+    end
+  end
+
+  describe 'are editable' do
+    it 'and require an email to be present' do
+      click_on('Edit')
+      fill_in('Email', with: '')
+      click_on('Update User')
+
+      message = page.find('#user_email').native.attribute('validationMessage')
+
+      expect(message).to eq 'Please fill out this field.'
+      expect(current_path).to eq edit_user_path(user.id)
     end
   end
 end
