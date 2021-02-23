d1fbfc83157ee38c078bbecb4f327d4098cbd3fe
dylan salay <dylansalay@gmail.com>
2021-02-22 16:51:59 -0800

    Add request specs for user

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -9,10 +9,72 @@ RSpec.describe "Users", type: :request do
     login_as user
   end
 
+  let(:valid_attributes) do
+    {
+      uid: 'fg1248',
+      email: 'fg1248@example.com',
+      deactivated: false
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      email: ''
+    }
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get users_path
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /edit" do
+    it "render a successful response" do
+      user = User.create! valid_attributes
+      get edit_user_url(user)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) do
+        {
+          email: 'br1589@example.com'
+        }
+      end
+
+      it "updates the requested user" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: new_attributes }
+        user.reload
+        expect(user.email).to eq('br1589@example.com')
+      end
+
+      it "redirects to the user" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: new_attributes }
+        user.reload
+        expect(response).to redirect_to(user_url(user))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders a successful response (i.e. to display the 'edit' template)" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: invalid_attributes }
+        expect(response).to be_successful
+      end
     end
   end
 end
