bb0eeadec28418c9b12135889e2aeb346c1bba6d
dylan salay <dylansalay@gmail.com>
2021-03-03 12:38:44 -0800

    generate roles controller and create action

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class RolesController < ApplicationController
+  before_action :set_user
+  before_action :set_item
+
+  def create
+    if @item && @user.has_role?(params[:role], @item)
+      redirect_back(fallback_location: root_path, flash: { alert: 'User already had this role' })
+    elsif @item
+      # each user only gets one role per item, remove all others first.
+      @user.roles.where(resource: @item).destroy_all
+      @user.add_role(params[:role], @item)
+      redirect_back(fallback_location: root_path, notice: "Role Added to User")
+    else
+      @user.add_role(params[:role])
+      redirect_back(fallback_location: root_path, notice: "Role Added to User")
+    end
+  end
+
+  def set_user
+    @user = User.find_by(uid: params[:uid])
+    return true if @user
+
+    redirect_back(fallback_location: root_path, flash: { alert: 'User UID not found' })
+    false
+  end
+
+  def set_item
+    @item = params[:item_class]&.constantize&.find(params[:item_id]) if params[:item_id]
+  end
+end
