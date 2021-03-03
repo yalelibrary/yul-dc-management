6d306d0f11caf97cee4313557b28dc8beed65404
dylan salay <dylansalay@gmail.com>
2021-03-03 09:20:19 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   resources :users, only: [:index, :edit, :update, :show, :new, :create]
   resources :child_objects
   resources :admin_sets do
-    put :set_user_role, on: :member
+    member do
+      put :set_user_role
+    end 
   end
 
   resources :parent_objects do
