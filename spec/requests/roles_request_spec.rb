711822b1d1fcf069e4529bc41536dc79b3d68ea0
Alisha Evans <alisha@notch8.com>
2021-03-03 17:33:17 -0800

    wip: role request spec

--- spec/requests/roles_request_spec.rb
+++ spec/requests/roles_request_spec.rb
@@ -2,11 +2,53 @@
 
 require 'rails_helper'
 
-RSpec.describe "Roles", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/roles/create"
-      expect(response).to have_http_status(:success)
-    end
+RSpec.describe 'Roles', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    login_as user
   end
+
+  let(:valid_parameters) do
+    {
+      uid: 'fcr7',
+      role: 'viewer',
+      item_class: 'AdminSet',
+      item_id: '2'
+    }
+  end
+
+  let(:valid_attributes) do
+    {
+      id: 1,
+      name: 'viewer',
+      resource_type: 'AdminSet',
+      resource_id: '1'
+    }
+  end
+
+  # let(:invalid_attributes) do
+  #   {
+  #     name: 'viewer',
+  #     resource_type: nil,
+  #     resource_id: '2'
+  #   }
+  # end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new role' do
+        post roles_path, params: { role: valid_parameters }
+        byebug
+        # expect do
+        #   # post roles_path, params: valid_parameters
+        #   post roles_path, params: { role: valid_attributes }
+        # end.to change(Role, :count).by(1)
+      end
+    end
+
+    # context 'with invalid parameters' do
+    #   it 'does not create a new Role' do
+    #   end
+    # end
 end
