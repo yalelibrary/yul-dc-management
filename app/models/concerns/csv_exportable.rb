a98c58d80e99274bb25919d99328f781a1a97757
dylan salay <dylansalay@gmail.com>
2021-03-24 16:59:52 -0700

    have errors sort by parent_object oid

--- app/models/concerns/csv_exportable.rb
+++ app/models/concerns/csv_exportable.rb
@@ -37,11 +37,11 @@ module CsvExportable
       end
     end
 
-    arr.sort_by { |co| [co.try(:order) || co[2], co.try(:oid) || co[0]] }
+    arr.sort_by { |co| [co.try(:order) || co[2], co.try(:oid) || co[1]] }
   end
 
   def parent_not_found(index, oid, arr, had_events)
-    row = [nil, oid, 0, "Parent Not Found in database", "", ""]
+    row = [nil, oid.to_i, 0, "Parent Not Found in database", "", ""]
     batch_processing_event("Skipping row [#{index + 2}] due to parent not found: #{oid}", 'Skipped Row') unless had_events
     arr << row
   end
@@ -50,7 +50,7 @@ module CsvExportable
     if ability.can?(:read, parent_object)
       true
     else
-      row = [nil, parent_object.oid, 0, "Access denied for parent object", "", ""]
+      row = [nil, parent_object.oid.to_i, 0, "Access denied for parent object", "", ""]
       batch_processing_event("Skipping row [#{index + 2}] due to parent permissions: #{parent_object.oid}", 'Skipped Row') unless had_events
       arr << row
       false
