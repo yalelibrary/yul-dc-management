cf56055714dbc37820f40b187943bbd6417828b4
dylan salay <dylansalay@gmail.com>
2021-03-24 16:31:27 -0700

    refactor child_object sort

--- app/models/concerns/csv_exportable.rb
+++ app/models/concerns/csv_exportable.rb
@@ -36,12 +36,13 @@ module CsvExportable
         parent_not_found(index, oid, arr, had_events)
       end
     end
-    arr.sort_by { |co| [co.try(:order) ? co.order : co[2], co.try(:oid) ? co.oid : co[0]] }
+
+    arr.sort_by { |co| [co.try(:order) || co[2], co.try(:oid) || co[0]] }
   end
 
   def parent_not_found(index, oid, arr, had_events)
     row = [nil, oid, 0, "Parent Not Found in database", "", ""]
-    batch_processing_event("Skipping row [#{index}] due to parent not found: #{oid}", 'Skipped Row') unless had_events
+    batch_processing_event("Skipping row [#{index + 2}] due to parent not found: #{oid}", 'Skipped Row') unless had_events
     arr << row
   end
 
@@ -50,7 +51,7 @@ module CsvExportable
       true
     else
       row = [nil, parent_object.oid, 0, "Access denied for parent object", "", ""]
-      batch_processing_event("Skipping row [#{index}] due to parent permissions: #{parent_object.oid}", 'Skipped Row') unless had_events
+      batch_processing_event("Skipping row [#{index + 2}] due to parent permissions: #{parent_object.oid}", 'Skipped Row') unless had_events
       arr << row
       false
     end
