5af3c48e8134e2ff51a679c8a1d9e2efb94781eb
dylan salay <dylansalay@gmail.com>
2021-03-24 16:11:44 -0700

    working csv child_oid sort

--- app/models/concerns/csv_exportable.rb
+++ app/models/concerns/csv_exportable.rb
@@ -8,38 +8,50 @@ module CsvExportable
   end
 
   def output_csv
-    had_events = batch_ingest_events_count.positive?
     return nil unless batch_action == "export child oids"
     CSV.generate do |csv|
       csv << headers
-      oids.each_with_index do |oid, index|
-        begin
-          po = ParentObject.find(oid.to_i)
-          next unless check_can_view(current_ability, index, po, csv, had_events)
-          po.child_objects.each do |co|
-            row = [co.oid, po.oid, co.order, po.authoritative_json["title"]&.first, co.label, co.caption, co.viewing_hint]
-            csv << row
-          end
-        rescue ActiveRecord::RecordNotFound
-          parent_not_found(index, oid, csv, had_events)
+      sorted_child_objects.each do |co|
+        if co.is_a?(Array)
+          csv << co
+        else
+          row = [co.oid, co.parent_object.oid, co.order, co.parent_object.authoritative_json["title"]&.first, co.label, co.caption, co.viewing_hint]
+          csv << row
         end
       end
     end
   end
 
-  def parent_not_found(index, oid, csv, had_events)
-    row = ["----", oid, "", "Parent Not Found in database", "", ""]
-    batch_processing_event("Skipping row [#{index}] due to parent not found: #{oid}", 'Skipped Row') unless had_events
-    csv << row
+  def sorted_child_objects
+    had_events = batch_ingest_events_count.positive?
+    arr = []
+    oids.each_with_index do |oid, index|
+      begin
+        po = ParentObject.find(oid.to_i)
+        next unless check_can_view(current_ability, index, po, arr, had_events)
+        po.child_objects.each do |co|
+          arr << co
+        end
+      rescue ActiveRecord::RecordNotFound
+        parent_not_found(index, oid, arr, had_events)
+      end
+    end
+    arr.sort_by { |co| [co.try(:order) ? co.order : co[2], co.try(:oid) ? co.oid : co[0]] }
   end
 
-  def check_can_view(ability, index, parent_object, csv, had_events)
+  def parent_not_found(index, oid, arr, had_events)
+    row = [nil, oid, 0, "Parent Not Found in database", "", ""]
+    batch_processing_event("Skipping row [#{index}] due to parent not found: #{oid}", 'Skipped Row') unless had_events
+    arr << row
+  end
+
+  def check_can_view(ability, index, parent_object, arr, had_events)
     if ability.can?(:read, parent_object)
       true
     else
-      row = ["----", parent_object.oid, "", "Access denied for parent object", "", ""]
+      row = [nil, parent_object.oid, 0, "Access denied for parent object", "", ""]
       batch_processing_event("Skipping row [#{index}] due to parent permissions: #{parent_object.oid}", 'Skipped Row') unless had_events
-      csv << row
+      arr << row
       false
     end
   end
