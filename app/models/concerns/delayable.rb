de365a15ae1027c5d9444b145d65b5d05509bdb9
Alisha Evans <alisha@notch8.com>
2021-03-31 12:42:34 -0700

    rubocop fixes

--- app/models/concerns/delayable.rb
+++ app/models/concerns/delayable.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 module Delayable
   extend ActiveSupport::Concern
 
   def delayed_jobs
-    Delayed::Job.where("handler LIKE ?", "%#{self.class}/#{self.oid}%")
+    Delayed::Job.where("handler LIKE ?", "%#{self.class}/#{oid}%")
   end
 
   def setup_metadata_jobs
-    Delayed::Job.where("handler LIKE ? AND handler LIKE ?", "job_class: SetupMetadataJob", "%#{self.class}/#{self.oid}%")
+    Delayed::Job.where("handler LIKE ? AND handler LIKE ?", "job_class: SetupMetadataJob", "%#{self.class}/#{oid}%")
   end
 
   def delayed_jobs_deletion
-    self.delayed_jobs.destroy_all
+    delayed_jobs.destroy_all
   end
 end
