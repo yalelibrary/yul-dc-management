9c9e9659ea125d36f61e8c65dd5663834b416003
Alisha Evans <alisha@notch8.com>
2021-04-06 10:01:43 -0700

    testing for the validity of parent_object.delayed_jobs works co-author: dylan salay <dylan@notch8.com> co-author: rob kaufman <rob@notch8.com>

--- spec/models/concerns/delayable_spec.rb
+++ spec/models/concerns/delayable_spec.rb
@@ -5,35 +5,15 @@ RSpec.describe Delayable, prep_metadata_sources: true, prep_admin_sets: true do
   let(:user) { FactoryBot.create(:user) }
   let(:batch_process) { BatchProcess.create(user_id: user.id) }
   let(:parent_object) { FactoryBot.build(:parent_object, oid: '16685691') }
-  let(:csv_upload) { Rack::Test::UploadedFile.new(Rails.root.join(fixture_path, "short_fixture_ids.csv")) }
+  let!(:job) { Delayed::Job.create(handler: parent_object.to_gid)}
 
-  around do |example|
-    perform_enqueued_jobs do
-      example.run
-    end
-  end
-
-  
-  describe "delayed_jobs" do
-
-    before do
-      stub_metadata_cloud("16685691")
-      stub_ptiffs_and_manifests
+  describe 'delayed_jobs' do
+    it 'returns delayed jobs associated with the parent object' do
+      expect(parent_object.delayed_jobs).to include(job)
     end
 
-    it "returns delayed jobs associated with the parent object" do
-      batch_process.setup_for_background_jobs(parent_object, 'ladybird')
-      # batch_connection = batch_process.batch_connections.build(connectable: parent_object)
-      # batch_connection.save
-      # parent_object.current_batch_process = batch_process
-      # parent_object.current_batch_connection = batch_connection
-      # byebug
-      parent_object.save
-      # parent_object.setup_metadata_job
-      # SetupMetadataJob.perform(parent_object, batch_process, batch_connection)
-      byebug
-      # expect(parent_)
+    it 'can distinguish between SetupMetadaJobs and other job types'
 
-    end
+    it 'will destroy all jobs from a given parent object when the parent object is destroyed'
   end
 end
