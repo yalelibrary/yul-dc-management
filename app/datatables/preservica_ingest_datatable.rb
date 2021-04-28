3126a95aa137476bca74042b3b02042f7349c87a
dylan salay <dylansalay@gmail.com>
2021-04-28 09:44:39 -0700

    Get data showing in datatable

--- app/datatables/preservica_ingest_datatable.rb
+++ app/datatables/preservica_ingest_datatable.rb
@@ -1,27 +1,35 @@
+# frozen_string_literal: true
+
 class PreservicaIngestDatatable < AjaxDatatablesRails::ActiveRecord
+  extend Forwardable
+
+  def_delegators :@view, :link_to, :show_parent_preservica_ingest_path, :parent_object_path
 
   def view_columns
     # Declare strings in this format: ModelName.column_name
     # or in aliased_join_table.column_name format
     @view_columns ||= {
-      # id: { source: "User.id", cond: :eq },
-      # name: { source: "User.name", cond: :like }
+      parent_oid: { source: "PreservicaIngest.parent_oid", orderable: true },
+      child_oid: { source: "PreservicaIngest.child_oid", orderable: true },
+      preservica_id: { source: "PreservicaIngest.preservica_id", orderable: true },
+      batch_process_id: { source: "PreservicaIngest.batch_process_id", orderable: true },
+      timestamp: { source: "PreservicaIngest.ingest_time", orderable: true }
     }
   end
 
   def data
-    records.map do |record|
+    records.map do |preservica_ingest|
       {
-        # example:
-        # id: record.id,
-        # name: record.name
+        parent_oid: preservica_ingest.parent_oid,
+        child_oid: preservica_ingest.child_oid,
+        preservica_id: preservica_ingest.preservica_id,
+        batch_process_id: preservica_ingest.batch_process_id,
+        timestamp: preservica_ingest.ingest_time
       }
     end
   end
 
-  def get_raw_records
-    # insert query here
-    # User.all
+  def get_raw_records # rubocop:disable Naming/AccessorMethodName
+    PreservicaIngest.all
   end
-
 end
