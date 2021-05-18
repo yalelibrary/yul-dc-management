ed4ad4902ce1a28f22e8d67e7ca6071dabd06a9c
Dylan Salay <50561476+dylansalay@users.noreply.github.com>
2021-05-18 10:54:46 -0700

    generate child solr documents (#616)
    * generate child solr documents
    
    * Index child solr documents with parents
    
    Co-authored-by: Steve Fox <steve@curationexperts.com>
    Co-authored-by: Dylan Salay <dylan@notch8.com>
    
    * breakout method for rubocop
    
    * fix and cleanup specs
    
    Co-authored-by: Martin Lovell <martin.lovell@yale.edu>
    Co-authored-by: Steve Fox <steve@curationexperts.com>
    Co-authored-by: Dylan Salay <dylan@notch8.com>

--- app/models/concerns/solr_indexable.rb
+++ app/models/concerns/solr_indexable.rb
@@ -9,10 +9,11 @@ module SolrIndexable
   end
 
   def solr_index
-    indexable = to_solr_full_text
+    indexable, child_solr_documents = to_solr_full_text
     return unless indexable.present?
     solr = SolrService.connection
     solr.add([indexable])
+    solr.add(child_solr_documents) unless child_solr_documents.nil?
     solr.commit
   end
 
@@ -158,21 +159,31 @@ module SolrIndexable
 
   def to_solr_full_text(json_to_index = nil)
     solr_document = to_solr(json_to_index)
-    solr_full_text_field = solr_full_text
-    solr_document[:fulltext_tsim] = solr_full_text_field unless solr_document.nil? || solr_full_text_field.nil?
-    solr_document
+    child_solr_documents = child_object_solr_documents
+    solr_document[:fulltext_tsim] = child_solr_documents.map { |child_solr_document| child_solr_document[:child_fulltext_tesim] } unless solr_document.nil? || child_solr_documents.nil?
+    [solr_document, child_solr_documents]
   end
 
-  def solr_full_text
+  def child_object_solr_documents
     if full_text?
       full_text_array = child_objects.map do |child_object|
         child_object_full_text = S3Service.download_full_text(child_object.remote_ocr_path)
         raise "Missing full text for child object: #{child_object.oid}, for parent object: #{oid}" if child_object_full_text.nil?
-        child_object_full_text
+        child_object_to_solr(child_object, child_object_full_text)
       end
       return full_text_array
     end
     nil
+  end
+
+  def child_object_to_solr(child_object, child_object_full_text)
+    parent_object = child_object.parent_object
+    {
+      id: child_object.oid,
+      parent_ssi: parent_object.oid,
+      child_fulltext_tesim: child_object_full_text,
+      parent_visibility_ssi: parent_object.visibility
+    }
   end
 
   def expand_date_structured(date_structured)
