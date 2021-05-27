d4bb7dc5b1feae6f2d3f6e76c413a30805d34523
Frederick Rodriguez <41123693+FCRodriguez7@users.noreply.github.com>
2021-05-27 11:21:23 -0400

    1318 remove solr field (#633)
    * Removed parent visibility solr field
    
    Martin Lovell <martin.lovell@yale.edu>
    
    * Styled for rubocop
    
    Martin Lovell <Martin.Lovell@yale.edu>
    
    Co-authored-by: Frederick Rodriguez <>

--- app/models/concerns/solr_indexable.rb
+++ app/models/concerns/solr_indexable.rb
@@ -181,8 +181,7 @@ module SolrIndexable
     {
       id: child_object.oid,
       parent_ssi: parent_object.oid,
-      child_fulltext_tesim: child_object_full_text,
-      parent_visibility_ssi: parent_object.visibility
+      child_fulltext_tesim: child_object_full_text
     }
   end
 
