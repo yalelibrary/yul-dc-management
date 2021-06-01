00dfedb8782ff65165a4061579a56e0cb4913b83
Alisha Evans <alisha@notch8.com>
2021-06-01 14:46:55 -0700

    convert child object table into a datatable

---
+++ app/datatables/child_object_datatable.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class ChildObjectDatatable < AjaxDatatablesRails::ActiveRecord
+  extend Forwardable
+
+  def_delegators :@view, :link_to, :child_object_path
+
+  def initialize(params, opts = {})
+    @view = opts[:view_context]
+    @current_ability = opts[:current_ability]
+    super
+  end
+
+  def view_columns
+    # default values: 'searchable: true', 'orderable: true', 'cond: :like'
+    # add those values here only for override purposes
+    @view_columns ||= {
+      oid: { source: 'ChildObject.oid' },
+      label: { source: 'ChildObject.label' },
+      caption: { source: 'ChildObject.caption' },
+      width: { source: 'ChildObject.width' },
+      height: { source: 'ChildObject.height' },
+      order: { source: 'ChildObject.order' },
+      parent_object: { source: 'ChildObject.parent_object_oid' }
+    }
+  end
+
+  def data
+    records.map do |child_object|
+      {
+        oid: link_to(child_object.oid, child_object_path(child_object)),
+        label: child_object.label,
+        caption: child_object.caption,
+        width: child_object.width,
+        height: child_object.height,
+        order: child_object.order,
+        parent_object: child_object.parent_object_oid,
+      }
+    end
+  end
+
+  def get_raw_records
+    ChildObject.accessible_by(@current_ability, :read)
+  end
+end
