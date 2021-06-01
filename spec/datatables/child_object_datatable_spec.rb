5f757046c1332da79994ad8bcd133299c38016cb
Alisha Evans <alisha@notch8.com>
2021-06-01 16:28:45 -0700

    added second spec

--- spec/datatables/child_object_datatable_spec.rb
+++ spec/datatables/child_object_datatable_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe ChildObjectDatatable, type: :datatable, prep_metadata_sources: tr
     expect(ChildObjectDatatable.new(datatable_sample_params(columns), view_context: child_object_datatable_view_mock, current_ability: Ability.new(user)).data).to eq([])
   end
 
+  it 'renders a complete data table' do
+    stub_metadata_cloud('2004628')
+    admin_set = AdminSet.find_by_key('brbl')
+    parent_object = FactoryBot.create(:parent_object, admin_set: admin_set)
+    child_object = FactoryBot.create(:child_object, parent_object: parent_object)
+
+    output = ChildObjectDatatable.new(datatable_sample_params(columns), view_context: child_object_datatable_view_mock, current_ability: Ability.new(user)).data
+
+    expect(output.size).to eq(1)
+    expect(output).to include(
+      oid: "<a href=\"/child_objects/1\">1</a>",
+      label: nil,
+      caption: 'MyString',
+      width: 1,
+      height: 1,
+      order: 1,
+      parent_object: 2004628,
+      actions: "<a href=\"/management/child_objects/10736292/edit\">Edit</a> | <a data-confirm=\"Are you sure?\" rel=\"nofollow\" data-method=\"delete\" href=\"/management/child_objects/10736292\">Destroy</a>",
+      DT_RowId: 10736292
+    )
+  end
 end
