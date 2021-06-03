fc91d220646853c433487e08b1d9af8932c6c84b
Frederick Rodriguez <frederick.rodriguez@yale.edu>
2021-06-03 16:47:20 -0400

    Fixed typo

--- spec/system/footer_spec.rb
+++ spec/system/footer_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe 'footer', type: :system do
 
   it 'has css' do
     expect(page).to have_css '.branch-name'
-    expect(page).to have_css '.yale_management_header_footer'f
+    expect(page).to have_css '.yale_management_header_footer'
   end
 end
