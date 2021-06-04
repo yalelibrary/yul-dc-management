f16ccb0a32bc634125eb1454a78d494c124b035a
Frederick Rodriguez <frederick.rodriguez@yale.edu>
2021-06-04 16:37:20 -0400

    Redesign of mgmt ui nav menu

--- spec/system/slide_bar_spec.rb
+++ spec/system/slide_bar_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe 'slidebar', type: :system, js: true do
+RSpec.describe 'the management application has sidebar', type: :system, js: true do
   before do
     visit '/'
   end
@@ -9,14 +9,24 @@ RSpec.describe 'slidebar', type: :system, js: true do
   it 'has css' do
     expect(page).to have_css '.list-group-item'
     expect(page).to have_css '.list-group-item-action'
+    expect(page).to have_css '.top-menu-item'
+    expect(page).to have_css '.flex-column'
+    expect(page).to have_css '.list-group'
+    expect(page).to have_css '.sidebar_yale'
+    expect(page).to have_css '.border-right'
+    expect(page).to have_css '.sign-button'
   end
 
   it 'has a list of choices when the user is not authenticated' do
-    expect(page).to have_content("Dashboard")
-    expect(page).to have_content("Parent Objects")
-    expect(page).to have_content("Child Objects")
-    expect(page).to have_content("Batch Process")
-    expect(page).to have_content("Preservation")
-    expect(page).to have_content("Notifications")
+    expect(page).to have_content("DASHBOARD")
+    expect(page).to have_content("PARENT OBJECTS")
+    expect(page).to have_content("CHILD OBJECTS")
+    expect(page).to have_content("BATCH PROCESS")
+    expect(page).to have_content("PRESERVATION")
+    expect(page).to have_content("NOTIFICATIONS")
+  end
+
+  it "item with hovering is visible to the user" do
+    find('.sidebar_yale').hover
   end
 end
