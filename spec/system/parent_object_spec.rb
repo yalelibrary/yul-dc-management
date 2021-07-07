7a2688120cf8b4506706303bf275a5bba1cae58e
Martin Lovell <martin.lovell@yale.edu>
2021-07-07 15:07:37 -0400

    update test

--- spec/system/parent_object_spec.rb
+++ spec/system/parent_object_spec.rb
@@ -500,7 +500,7 @@ RSpec.describe "ParentObjects", type: :system, prep_metadata_sources: true, prep
       end
       it "does not allow creation of new parent with wrong admin set" do
         click_on("Create Parent object")
-        expect(page.body).to eq 'Access denied'
+        expect(page).to have_content('Access denied')
       end
     end
 
