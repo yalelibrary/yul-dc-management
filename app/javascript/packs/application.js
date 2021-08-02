f9cbfaaecb1c31cdb7c8cb99ccf2d9d2ea87758c
Frederick Rodriguez <frederick.rodriguez@yale.edu>
2021-08-02 10:18:55 -0400

    Modified a merge file
    Co-authored-by: Maggie Zhao <maggie.zhao@yale.edu>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-const JSZip = require('jszip')
 require('datatables.net-bs4')(window, $)
 require('datatables.net-buttons-bs4')(window, $)
 require('datatables.net-buttons/js/buttons.colVis.js')(window, $)
@@ -34,8 +33,6 @@ import "@fortawesome/fontawesome-free/js/all.js";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-window.JSZip = JSZip;
-
 let dataTable;
 $( document ).on('turbolinks:load', function() {
   let initialColumnSearchValues = [];
@@ -47,96 +44,6 @@ $( document ).on('turbolinks:load', function() {
     try { columnInfo = columnInfo && JSON.parse(columnInfo); } catch (e) { columnInfo = null; }
     columns = columns.map(function (col) {if (columnInfo && columnInfo[col.data] && columnInfo[col.data].hidden) col.visible = false; return col } )
     let hasSearch = columns.some(function(col){return col.searchable;});
-    const onColumnsUpdate = function(dataTable) {
-      let colVisibilityMap = createSearchRow(dataTable);
-      localStorage.setItem(dataTableStorageKey, JSON.stringify(colVisibilityMap));
-    }
-    const createSearchRow = function(dataTable) {
-      $('#search-row').remove();
-      let searchRow = $("<tr role='row' id='search-row'></tr>");
-      let index = 0;
-      let colVisibilityMap = {}
-      dataTable.api().columns().every(function () {
-        let column = this;
-        let searchInit = initialColumnSearchValues[index];
-        let colDef = columns[index++];
-        colVisibilityMap[colDef.data] = {hidden:!column.visible()};
-        if (!column.visible()) return;
-        if (colDef.searchable) {
-          let th = $("<th class='datatable-search-row'/>");
-          let input = null;
-          if (colDef.options) {
-            input = $("<select><option>All</option>" + colDef.options.map(function (option) {
-              if (typeof option==="string"){
-                option={value:option, label:option}
-              }
-              if (option.selected) {
-                column.search(option.value)
-              }
-              return "<option value='"+ option.value +"' "+ (option.selected ? "selected": '') + ">" + option.label + "</option>"
-            }) + "</select>");
-          } else {
-            input = $("<input type='text' size='12' placeholder='" + $(column.header()).text() + "' />");
-          }
-          (input).on('keyup change clear', function () {
-            let v = this.value;
-            if (v === "All" && colDef.options) v = "";
-            if (column.search() !== v) {
-              column.search(v);
-              scheduleDraw();
-            }
-          });
-          searchRow.append(th.append(input));
-          input.val(searchInit);
-        } else {
-          searchRow.append("<th />");
-        }
-      });
-      $(dataTable.api().table().header()).append(searchRow);
-      // store the information about which columns are visible for this page
-      return colVisibilityMap;
-    }
-
-    dataTable = $('.is-datatable').dataTable({
-      "deferLoading":true,
-      "processing": true,
-      "serverSide": true,
-      "stateSave": true,
-      "ajax": {
-        "url": $('.is-datatable').data('source')
-      },
-      "pagingType": "full_numbers",
-      "bAutoWidth": false, // AutoWidth has issues with hiding and showing columns as startup
-      "columns": columns,
-      "order": columnOrder(columns),
-      "lengthMenu": [[50, 100, 500, -1], [50, 100, 500, "All"]],
-      "sDom":hasSearch?'Blrtip':'<"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>rtip',
-      buttons: [
-        {
-          text: "Clear Filters",
-          className: "clear-filters-button",
-          action: () => {
-            dataTable.api().columns().search('').visible( true, true ).order('asc' ).state.clear().draw() ;
-            $(".datatable-search-row input").val("");
-          }
-        },
-        {
-          extend: 'colvis',
-          text: "\u25EB"
-        },
-        {
-          extend: 'csvHtml5',
-          text: "CSV",
-          exportOptions: {
-            columns: ':visible'
-          }
-        },
-        {
-          extend: 'excelHtml5',
-          text: "Excel",
-          exportOptions: {
-            columns: ':visible'
-          }
     const onColumnsUpdate = function(dataTable) {
       let colVisibilityMap = createSearchRow(dataTable);
       localStorage.setItem(dataTableStorageKey, JSON.stringify(colVisibilityMap));
@@ -279,17 +186,3 @@ const columnOrder = (columns) => {
   })
   return columnOrder;
 }
-
-$( document ).on('turbolinks:load', function() {
-  let show_hide_template_link = function(){
-    let batch_action = $('#batch_process_batch_action').val();
-    $(".download_batch_process_template").attr("href", "batch_processes/download_template?batch_action=" + $('#batch_process_batch_action').val());
-    if (batch_action) {
-      $(".download_batch_process_template").fadeIn();
-    } else {
-      $(".download_batch_process_template").fadeOut();
-    }
-  }
-  $('#batch_process_batch_action').on('change', show_hide_template_link)
-  show_hide_template_link();
-})
