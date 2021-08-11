3c8d61ea3f1ed4813d1e6fdc14823bcc360a1059
Martin Lovell <martin.lovell@yale.edu>
2021-08-11 14:24:03 -0400

    rubocop

--- app/jobs/update_all_metadata_job.rb
+++ app/jobs/update_all_metadata_job.rb
@@ -10,12 +10,11 @@ class UpdateAllMetadataJob < ApplicationJob
   def perform(start_position = 0)
     parent_objects = ParentObject.order(:oid).offset(start_position).limit(UpdateAllMetadataJob.job_limit)
     last_job = parent_objects.count < UpdateAllMetadataJob.job_limit
-    if parent_objects.count.positive?
-      parent_objects.each do |po|
-        po.metadata_update = true
-        po.setup_metadata_job
-      end
-      UpdateAllMetadataJob.perform_later(start_position + parent_objects.count) unless last_job
+    return unless parent_objects.count.positive?
+    parent_objects.each do |po|
+      po.metadata_update = true
+      po.setup_metadata_job
     end
+    UpdateAllMetadataJob.perform_later(start_position + parent_objects.count) unless last_job
   end
 end
