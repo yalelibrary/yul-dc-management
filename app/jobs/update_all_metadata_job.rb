bdef4e606288be311cec5e2def8c1bcdd0a96155
martinlovell <32851993+martinlovell@users.noreply.github.com>
2021-08-13 12:47:07 -0400

    Allow reindex all to take parameters for adminsets and metadata sources (#748)
    * Allow reindex all to take parameters for adminsets and metadata sources
    
    * Add button and dialog from admin sets to refresh metadata
    
    * Add button and dialog from admin sets to refresh metadata
    
    * add confirm to form submit
    
    * Update view test with devise helper
    
    * Added test for job with where
    
    * Added system test for update metadata dialog
    
    * add select all link
    
    * ui adjustments
    
    * factor out partial for metadata update dialog

--- app/jobs/update_all_metadata_job.rb
+++ app/jobs/update_all_metadata_job.rb
@@ -7,14 +7,14 @@ class UpdateAllMetadataJob < ApplicationJob
     5000
   end
 
-  def perform(start_position = 0)
-    parent_objects = ParentObject.order(:oid).offset(start_position).limit(UpdateAllMetadataJob.job_limit)
+  def perform(start_position = 0, where = '')
+    parent_objects = ParentObject.where(where).order(:oid).offset(start_position).limit(UpdateAllMetadataJob.job_limit)
     last_job = parent_objects.count < UpdateAllMetadataJob.job_limit
     return unless parent_objects.count.positive? # stop if nothing is found
     parent_objects.each do |po|
       po.metadata_update = true
       po.setup_metadata_job
     end
-    UpdateAllMetadataJob.perform_later(start_position + parent_objects.count) unless last_job
+    UpdateAllMetadataJob.perform_later(start_position + parent_objects.count, where) unless last_job
   end
 end
