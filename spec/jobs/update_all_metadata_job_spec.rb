5fe81f8f1be924543897f9e4b3236644c41498cf
Martin Lovell <martin.lovell@yale.edu>
2021-08-13 08:01:29 -0400

    Added test for job with where

--- spec/jobs/update_all_metadata_job_spec.rb
+++ spec/jobs/update_all_metadata_job_spec.rb
@@ -20,21 +20,21 @@ RSpec.describe UpdateAllMetadataJob, type: :job, prep_metadata_sources: true, so
   end
 
   context 'with more than limit parent objects' do
-    before do
-      limit = UpdateAllMetadataJob.job_limit
-      total_records = 8000 #  some number > UpdateAllMetadataJob.job_limit < UpdateAllMetadataJob.job_limit * 2
+    let(:parent_object_where) { double }
+    let(:total_records) { 8000 }
+    let(:limit) { UpdateAllMetadataJob.job_limit }
+    let(:expected_call_count) { (total_records.to_f / limit).ceil }
 
-      # create mocks for everything the job uses
+    before do
+      # create mocks
       parent = double
       expect(parent).to receive(:metadata_update=).exactly(total_records).times
       expect(parent).to receive(:setup_metadata_job).exactly(total_records).times
 
-      parent_object_where = double
       parent_object_order = double
       parent_object_order_offset1 = double
       parent_object_order_offset2 = double
-      expect(ParentObject).to receive(:where).with('').and_return(parent_object_where).exactly((total_records.to_f / limit).ceil).times
-      expect(parent_object_where).to receive(:order).and_return(parent_object_order).exactly((total_records.to_f / limit).ceil).times
+      expect(parent_object_where).to receive(:order).and_return(parent_object_order).exactly(expected_call_count).times
       expect(parent_object_order).to receive(:offset).with(0).and_return parent_object_order_offset1
       expect(parent_object_order).to receive(:offset).with(limit).and_return parent_object_order_offset2
       expect(parent_object_order_offset1).to receive(:limit).with(limit).and_return [*1..limit].map { |_ix| parent }
@@ -47,8 +47,14 @@ RSpec.describe UpdateAllMetadataJob, type: :job, prep_metadata_sources: true, so
       end
     end
 
-    it 'goes through all parents in batches' do
+    it 'processes all parents in batches' do
+      expect(ParentObject).to receive(:where).with('').and_return(parent_object_where).exactly(expected_call_count).times
       UpdateAllMetadataJob.perform_later
+    end
+
+    it 'processes parents with where' do
+      expect(ParentObject).to receive(:where).with(admin_set_id: [1, 2, 3], authoritative_metadata_source_id: [2, 3]).and_return(parent_object_where).exactly(expected_call_count).times
+      UpdateAllMetadataJob.perform_later(0, admin_set_id: [1, 2, 3], authoritative_metadata_source_id: [2, 3])
     end
   end
 end
