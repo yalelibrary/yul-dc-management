cadc6278f7a3f99fd03bebaed5cdc63d0162d009
K8Sewell <kait@notch8.com>
2021-08-24 11:37:44 -0700

    Manually triggers setup metadata job and removes after update hook:

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -21,7 +21,6 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   self.primary_key = 'oid'
   after_save :setup_metadata_job
   after_update :solr_index_job # we index from the fetch job on create
-  after_update :setup_metadata_job
   after_destroy :solr_delete
   after_destroy :note_deletion
   after_destroy :delayed_jobs_deletion
