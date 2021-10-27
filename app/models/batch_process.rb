483487b0566c832f2ba575f5e985e3493c677049
Frederick <frederick910@gmail.com>
2021-10-27 14:29:13 -0400

    Deleted duplicate code in file
    Co-authored-by: Martin Lovell <martin.lovell@yale.edu>

--- app/models/batch_process.rb
+++ app/models/batch_process.rb
@@ -179,38 +179,6 @@ class BatchProcess < ApplicationRecord # rubocop:disable Metrics/ClassLength
     object.current_batch_connection.save! if object.class == ChildObject
   end
 
-  # DELETE PARENT OBJECTS: ------------------------------------------------------------------------ #
-
-  # DELETES PARENT OBJECTS FROM INGESTED CSV
-  def delete_objects
-    parsed_csv.each_with_index do |row, index|
-      oid = row['oid']
-      action = row['action']
-      metadata_source = row['source']
-      batch_processing_event("Skipping row [#{index + 2}] with parent oid: #{oid}, action value for oid must be 'delete' to complete deletion.", 'Invalid Vocab') if action != "delete"
-      next unless action == 'delete'
-      parent_object = deletable_parent_object(oid, index)
-      next unless parent_object
-      setup_for_background_jobs(parent_object, metadata_source)
-      parent_object.destroy
-      parent_object.processing_event("Parent #{parent_object.oid} has been deleted", 'deleted')
-    end
-  end
-
-  # CHECKS TO SEE IF USER HAS ABILITY TO DELETE OBJECTS:
-  def deletable_parent_object(oid, index)
-    parent_object = ParentObject.find_by(oid: oid)
-    if parent_object.blank?
-      batch_processing_event("Skipping row [#{index + 2}] with parent oid: #{oid} because it was not found in local database", 'Skipped Row')
-      return false
-    elsif !current_ability.can?(:destroy, parent_object)
-      batch_processing_event("Skipping row [#{index + 2}] with parent oid: #{oid}, user does not have permission to delete.", 'Permission Denied')
-      return false
-    else
-      parent_object
-    end
-  end
-
   # RECREATE CHILD OID PTIFFS: -------------------------------------------------------------------- #
 
   # RECREATES CHILD OID PTIFFS FROM INGESTED CSV
