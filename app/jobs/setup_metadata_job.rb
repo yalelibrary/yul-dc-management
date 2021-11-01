518fc01387b2bf9de5391f5515425f7d21482dea
Martin Lovell <martin.lovell@yale.edu>
2021-11-01 14:52:47 -0400

    Only create PTIFF jobs if conversion is necessary

--- app/jobs/setup_metadata_job.rb
+++ app/jobs/setup_metadata_job.rb
@@ -35,11 +35,18 @@ class SetupMetadataJob < ApplicationJob
     parent_object.create_child_records if parent_object.from_upstream_for_the_first_time?
     parent_object.save!
     parent_object.processing_event("Child object records have been created", "child-records-created")
+    ptiff_jobs_queued = false
     parent_object.child_objects.each do |child|
       parent_object.current_batch_process&.setup_for_background_jobs(child, nil)
-      GeneratePtiffJob.perform_later(child, current_batch_process)
-      child.processing_event("Ptiff Queued", "ptiff-queued")
+      if child.pyramidal_tiff.height_and_width? && child.pyramidal_tiff.file_on_s3
+        child_object.processing_event("PTIFF exists on S3, not converting: #{child.oid}", 'ptiff-ready-skipped')
+      else
+        GeneratePtiffJob.perform_later(child, current_batch_process)
+        child.processing_event("Ptiff Queued", "ptiff-queued")
+        ptiff_jobs_queued = true
+      end
     end
+    GenerateManifestJob.perform_later(parent_object, parent_object.current_batch_process, parent_object.current_batch_connection) if parent_object.needs_a_manifest? && !ptiff_jobs_queued
   rescue => child_create_error
     parent_object.processing_event(child_create_error.message, "failed")
   end
