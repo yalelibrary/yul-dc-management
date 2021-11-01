2eaa3b39982c1371295de811b976703f3be49b84
martinlovell <32851993+martinlovell@users.noreply.github.com>
2021-11-01 16:13:29 -0400

    Remove delayed job query from ERB (#831)
    * Remove delayed job query from ERB
    
    * Use empty?

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -59,7 +59,7 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   end
 
   def self.cannot_reindex
-    return true if Delayable.solr_reindex_jobs.count.positive?
+    return true unless Delayable.solr_reindex_jobs.empty?
   end
 
   # Returns true if last_ladybird_update has changed from nil to some value, indicating initial ladybird fetch
