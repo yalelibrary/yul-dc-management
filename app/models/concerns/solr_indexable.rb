ffc0dd726ba4f07f425b6b6ab88888ef9de80f68
Martin Lovell <martin.lovell@yale.edu>
2021-11-04 14:32:39 -0400

    Add new field for sortable series with archival sort prefix

--- app/models/concerns/solr_indexable.rb
+++ app/models/concerns/solr_indexable.rb
@@ -123,6 +123,7 @@ module SolrIndexable
       rights_tesim: rights_statement,
       scale_tesim: json_to_index["scale"],
       series_ssi: json_to_index["series"],
+      series_sort_ssi: "#{json_to_index['archivalSort']}|#{json_to_index['series']}",
       source_ssim: json_to_index["source"], # refers to source of metadata, e.g. Ladybird, Voyager, etc.
       sourceCreated_tesim: json_to_index["sourceCreated"],
       sourceDate_tesim: json_to_index["sourceDate"],
