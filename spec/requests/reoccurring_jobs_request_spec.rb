b33d43d3fddc993490c270a9318927248b0745c3
K8Sewell <kait@notch8.com>
2021-11-18 16:10:32 -0800

    Adds tests for reoccuring job controller

---
+++ spec/requests/reoccurring_jobs_request_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "Reoccurring Jobs", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'with logged in user' do
+    before do
+      login_as user
+    end
+
+    describe "GET /index" do
+      it "returns http success" do
+        get reoccurring_jobs_url
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "POST /create" do
+      it "correctly responds with invalid attributes" do
+        post reoccurring_jobs_url
+        expect(response).to be_successful
+      end
+    end
+  end
+end
