b7fd26759cee61a2260774ed6984aad0539de050
JP Engstrom <jpengstrom@vpn17225180135.its.yale.internal>
2021-11-18 16:21:29 -0800

    added preservica ingest request spec

---
+++ spec/requests/preservica_ingest_request_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "Preservica Ingest", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'with logged in user' do
+    before do
+      login_as user
+    end
+
+    describe "GET /index" do
+      it "returns http success" do
+        get preservica_ingests_url
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+  end
+end
