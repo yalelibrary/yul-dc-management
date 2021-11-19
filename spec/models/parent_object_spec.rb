e85310002e79c5efc30b9c246005c61763a6cce4
Martin Lovell <martin.lovell@yale.edu>
2021-11-19 17:23:45 -0500

    Update test

--- spec/models/parent_object_spec.rb
+++ spec/models/parent_object_spec.rb
@@ -613,6 +613,10 @@ RSpec.describe ParentObject, type: :model, prep_metadata_sources: true, prep_adm
         ENV['VPN'] = original_vpn
       end
       it "posts digital object changes when source changes" do
+        stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1")
+            .to_return(status: 200, body: { dummy: "data" }.to_json)
+        stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/aspace/repositories/11/archival_objects/515305")
+            .to_return(status: 200, body: { dummy: "data" }.to_json)
         stub_request(:post, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/digital_object_updates")
             .to_return(status: 200, body: { data: "fake data" }.to_json)
         expect(parent_object.ladybird_cloud_url).to eq "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1"
