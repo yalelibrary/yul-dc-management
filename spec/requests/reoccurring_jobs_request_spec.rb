42a6acbb24d879fa1986147e2e866aa9196d432d
JP Engstrom <jpengstrom@vpn172022156176.its.yale.internal>
2021-11-23 10:43:20 -0800

    added VPN info to request spec

--- spec/requests/reoccurring_jobs_request_spec.rb
+++ spec/requests/reoccurring_jobs_request_spec.rb
@@ -5,6 +5,16 @@ require 'rails_helper'
 RSpec.describe "Reoccurring Jobs", type: :request do
   let(:user) { FactoryBot.create(:user) }
 
+  around do |example|
+    original_vpn = ENV['VPN']
+    original_metadata_cloud_host = ENV['METADATA_CLOUD_HOST']
+    ENV['METADATA_CLOUD_HOST'] = "metadata-api.library.yale.edu"
+    ENV['VPN'] = 'true'
+    example.run
+    ENV['VPN'] = original_vpn
+    ENV['METADATA_CLOUD_HOST'] = original_metadata_cloud_host
+  end
+
   describe 'with logged in user' do
     before do
       login_as user
