4e4a443ef9c9ddecfe0b8e3473a6b9ab6e56649f
K8Sewell <kait@notch8.com>
2021-11-23 15:55:42 -0800

    Updates pending tests

--- spec/models/preservica_ingest_spec.rb
+++ spec/models/preservica_ingest_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe PreservicaIngest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "exists" do
+    expect(described_class).to be_a(Class)
+  end
 end
