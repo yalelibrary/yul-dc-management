961c42cee27a068f348882b4f71fdb16fe037b9c
K8Sewell <kait@notch8.com>
2021-11-24 09:30:27 -0800

    Adds to channel spec

--- spec/channels/application_cable/connection_spec.rb
+++ spec/channels/application_cable/connection_spec.rb
@@ -4,5 +4,7 @@ require "rails_helper"
 RSpec.describe ApplicationCable::Connection do
   it "exists" do
     expect(described_class).to be_a(Class)
+    expect(described_class).not_to be_a(Hash)
+    expect(described_class).not_to be_a(Array)
   end
 end
