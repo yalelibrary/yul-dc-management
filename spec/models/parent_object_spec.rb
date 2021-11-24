407e4363f3d6fdf6c029e1e691238e0301ed44de
Martin Lovell <martin.lovell@yale.edu>
2021-11-24 14:00:24 -0500

    reorganize tests

--- spec/models/parent_object_spec.rb
+++ spec/models/parent_object_spec.rb
@@ -615,30 +615,29 @@ RSpec.describe ParentObject, type: :model, prep_metadata_sources: true, prep_adm
         ENV['VPN'] = original_vpn
         ENV['FEATURE_FLAGS'] = original_flags
       end
-      it "posts digital object changes when source changes" do
+      before do
         stub_full_text_not_found('2005512')
-        stub_request(:post, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/digital_object_updates")
-            .to_return(status: 200, body: { data: "fake data" }.to_json)
         stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1")
             .to_return(status: 200, body: { dummy: "data" }.to_json)
         stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/aspace/repositories/11/archival_objects/515305")
             .to_return(status: 200, body: { dummy: "data" }.to_json)
         allow(S3Service).to receive(:upload_if_changed).and_return(true)
+      end
+      it "posts digital object changes when source changes" do
+        stub_request(:post, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/digital_object_updates")
+            .to_return(status: 200, body: { data: "fake data" }.to_json)
         expect(parent_object.ladybird_cloud_url).to eq "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1"
         parent_object.aspace_uri = '/repositories/11/archival_objects/515305'
         parent_object.authoritative_metadata_source_id = aspace
         parent_object.child_object_count = 1
         parent_object.visibility = "Public"
+        expect(parent_object).to receive(:mc_post).once
         parent_object.save!
       end
       it "posts digital object changes when source changes, and continues if post fails" do
+        stub_request(:post, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/digital_object_updates")
+            .to_return(status: 200, body: { data: "fake data" }.to_json)
         stub_full_text_not_found('2005512')
-        stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1")
-            .to_return(status: 200, body: { dummy: "data" }.to_json)
-        stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/aspace/repositories/11/archival_objects/515305")
-            .to_return(status: 200, body: { dummy: "data" }.to_json)
-        allow(S3Service).to receive(:upload_if_changed).and_return(true)
-        expect(parent_object.ladybird_cloud_url).to eq "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1"
         allow(parent_object).to receive(:mc_post).and_raise("boom!")
         parent_object.aspace_uri = '/repositories/11/archival_objects/515305'
         parent_object.authoritative_metadata_source_id = aspace
@@ -650,12 +649,6 @@ RSpec.describe ParentObject, type: :model, prep_metadata_sources: true, prep_adm
         stub_full_text_not_found('2005512')
         stub_request(:post, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/digital_object_updates")
             .to_return(status: 404, body: { data: "fake data" }.to_json)
-        stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1")
-            .to_return(status: 200, body: { dummy: "data" }.to_json)
-        stub_request(:get, "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/aspace/repositories/11/archival_objects/515305")
-            .to_return(status: 200, body: { dummy: "data" }.to_json)
-        allow(S3Service).to receive(:upload_if_changed).and_return(true)
-        expect(parent_object.ladybird_cloud_url).to eq "https://#{MetadataSource.metadata_cloud_host}/metadatacloud/api/1.0.1/ladybird/oid/2005512?include-children=1"
         parent_object.aspace_uri = '/repositories/11/archival_objects/515305'
         parent_object.authoritative_metadata_source_id = aspace
         parent_object.child_object_count = 1
