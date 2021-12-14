4e3fb6ecbb57c4039c93b4716e3f7caadc8350be
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2021-12-14 13:42:12 -0800

    Adds redirect to parent (#859)
    * Adds migration to add redirect to parent
    
    * added redirect_to to schema
    
    * adds redirect to parent object
    
    * refactored parent datatable
    
    * Updates controller action
    
    * Separates out parent datatable config
    
    * Pulls additional table
    
    * Adds minify process
    
    * Restores app.js
    
    * Refines minify process
    
    * Adds confirmation dialog for redirect
    
    * Makes redirect to on form visible only on condition
    
    * added datatable for redirected_to parents
    
    * saving
    
    * refactor update method
    
    * specs started
    
    * Adds datatable spec
    
    * Adds request spec
    
    * Remove not relevant code
    
    * Remove timing split
    
    * Rubocop
    
    * Adds copy of results
    
    * Restores config
    
    * Removes tmp test storage
    
    * Changes parallel run count
    
    * Fix failing specs
    
    * added guard clause for rubocop
    
    Co-authored-by: JP Engstrom <jpengstrom@MacBook-Pro.local>

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -31,9 +31,12 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   after_destroy :pdf_deletion
   after_destroy :digital_object_delete
   paginates_per 50
+  # rubocop:disable Metrics/LineLength
+  validates :redirect_to, format: { with: /\A((http|https):\/\/)?(collections-test.|collections-uat.|collections.)?library.yale.edu\/catalog\//, message: " in incorrect format. Please enter DCS url https://collections.library.yale.edu/catalog/123", allow_blank: true }
+  # rubocop:enable Metrics/LineLength
 
   def self.visibilities
-    ['Private', 'Public', 'Yale Community Only']
+    ['Private', 'Public', 'Redirect', 'Yale Community Only']
   end
 
   # Options from iiif presentation api 2.1 - see https://iiif.io/api/presentation/2.1/#viewingdirection
