f5beb2e9ec4bcc9ae9d97a17d2f2b63daf3872e5
JP Engstrom <jpengstrom@macbook-pro.lan>
2021-12-21 09:43:52 -0800

    job specs

--- spec/jobs/export_admin_set_csv_job_spec.rb
+++ spec/jobs/export_admin_set_csv_job_spec.rb
@@ -9,11 +9,14 @@ RSpec.describe ExportAdminSetCsvJob, type: :job do
   let(:user) { FactoryBot.create(:user) }
   let(:batch_process) { FactoryBot.create(:batch_process, user: user) }
   let(:export_admin_set_csv_job) { ExportAdminSetCsvJob.new }
+  let(:brbl) { AdminSet.find_by_key("brbl") }
+  let(:other_admin_set) { FactoryBot.create(:admin_set) }
+  let(:parent_object) { FactoryBot.create(:parent_object, oid: 2_034_600, admin_set: brbl) }
 
   it 'increments the job queue by one' do
     ActiveJob::Base.queue_adapter = :delayed_job
     expect do
-      described_class.perform_later(batch_process)
+      described_class.perform_later(batch_process, brbl)
     end.to change { Delayed::Job.count }.by(1)
   end
 
@@ -25,16 +28,16 @@ RSpec.describe ExportAdminSetCsvJob, type: :job do
     expect(export_admin_set_csv_job.queue_name).to eq('default')
   end
 
-  # it 'calls parent_output_csv when performed' do
-  #   expect(batch_process).to receive(:parent_output_csv).once
-  #   described_class.new.perform(batch_process)
-  # end
+  it 'calls parent_output_csv when performed' do
+    expect(batch_process).to receive(:export_admin_set_csv).once
+    described_class.new.perform(batch_process, brbl)
+  end
 
-  # it 'reports error when parent_output_csv fails' do
-  #   allow(batch_process).to receive(:parent_output_csv).and_raise('boom!')
-  #   inst = described_class.new
-  #   expect do
-  #     inst.perform(batch_process)
-  #   end.to raise_error('boom!')
-  # end
+  it 'reports error when parent_output_csv fails' do
+    allow(batch_process).to receive(:export_admin_set_csv).and_raise('boom!')
+    inst = described_class.new
+    expect do
+      inst.perform(batch_process, brbl)
+    end.to raise_error('boom!')
+  end
 end
