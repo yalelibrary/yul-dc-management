c6954edd6777dcccf5407bdfd37b5d72aacb59db
JP Engstrom <jpengstrom@macbook-pro.lan>
2022-01-13 15:59:24 -0800

    rubocop

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -35,7 +35,7 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   # rubocop:disable Metrics/LineLength
   validates :redirect_to, format: { with: /\A((http|https):\/\/)?(collections-test.|collections-uat.|collections.)?library.yale.edu\/catalog\//, message: " in incorrect format. Please enter DCS url https://collections.library.yale.edu/catalog/123", allow_blank: true }
   # rubocop:enable Metrics/LineLength
-  validates :preservica_uri, presence: true, format: { with: %r{\A/}, message: " in incorrect format. URI must start with a /" }, if: proc { self.digital_object_source.present? }
+  validates :preservica_uri, presence: true, format: { with: %r{\A/}, message: " in incorrect format. URI must start with a /" }, if: proc { digital_object_source.present? }
 
   def check_for_redirect
     minify if redirect_to.present?
