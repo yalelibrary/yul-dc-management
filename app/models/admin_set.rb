845ac0a735da4c4f46d66627cea82626c3bd0438
K8Sewell <kait@notch8.com>
2022-01-20 16:55:10 -0800

    Validates preservica credentials

--- app/models/admin_set.rb
+++ app/models/admin_set.rb
@@ -23,4 +23,33 @@ class AdminSet < ApplicationRecord
   def remove_editor(user)
     user.remove_role(:editor, self)
   end
+
+  def preservica_credentials_verified
+    if valid_format?(ENV['PRESERVICA_CREDENTIALS'])
+      # return true
+      true
+    else
+      # if not displays flash message to user
+      'Preservica credentials not configured for this Admin Set'
+    end
+  end
+
+  def valid_format?(json)
+    # valid JSON,
+    parsed_json = JSON.parse(json)
+
+    # exists,
+    if ENV['PRESERVICA_CREDENTIALS'].present? &&
+       # contains admin set key,
+       parsed_json.include?(key) &&
+       # and key references username and password with not blank values
+       parsed_json[key.to_s]['username'].present? &&
+       parsed_json[key.to_s]['password'].present?
+      return true
+    else
+      return false
+    end
+  rescue JSON::ParserError
+    false
+  end
 end
--- spec/models/admin_set_spec.rb
+++ spec/models/admin_set_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe AdminSet, type: :model do
-  let(:admin_set) { FactoryBot.create(:admin_set, key: "key", label: "label", homepage: "http://test.com") }
+  let(:admin_set) { FactoryBot.create(:admin_set, key: "brbl", label: "label", homepage: "http://test.com") }
+  let(:admin_set_two) { FactoryBot.create(:admin_set, key: "key", label: "label", homepage: "http://test.com") }
   let(:user) { FactoryBot.create(:user) }
 
   it "returns proper values" do
@@ -61,6 +62,29 @@ RSpec.describe AdminSet, type: :model do
       admin_set.add_editor(user)
       expect(user.roles.count).to eq(1)
       expect(user.roles.first.name).to eq("editor")
+    end
+  end
+
+  describe "preservica credentials" do
+    around do |example|
+      original_preservica_cred = ENV['PRESERVICA_CREDENTIALS']
+      ENV['PRESERVICA_CREDENTIALS'] = '{
+        "brbl": {
+          "username": "foo",
+          "password": "bar"
+         },
+         "mssa": {
+          "username": "baz",
+          "password": "boop"
+         }
+      }'
+      example.run
+      ENV['PRESERVICA_CREDENTIALS'] = original_preservica_cred
+    end
+
+    it "verifies if credentials exist" do
+      expect(admin_set.preservica_credentials_verified).to eq true
+      expect(admin_set_two.preservica_credentials_verified).to eq "Preservica credentials not configured for this Admin Set"
     end
   end
 end
