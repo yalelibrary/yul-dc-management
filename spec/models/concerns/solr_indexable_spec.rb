dfb702d7a18e2a57456274bb8cc010bfff9a45dc
K8Sewell <kait@notch8.com>
2022-01-26 13:46:12 -0800

    breaks out contexts

--- spec/models/concerns/solr_indexable_spec.rb
+++ spec/models/concerns/solr_indexable_spec.rb
@@ -5,51 +5,66 @@ require 'rails_helper'
 RSpec.describe SolrIndexable, type: :model do
   let(:solr_indexable) { ParentObject.new(admin_set: FactoryBot.create(:admin_set)) }
 
-  before do
-    allow(solr_indexable).to receive(:manifest_completed?).and_return(true)
+  describe "valid solr document" do
+
+    before do
+      allow(solr_indexable).to receive(:manifest_completed?).and_return(true)
+    end
+
+    it "indexes language and languageCode" do
+      solr_document = solr_indexable.to_solr('languageCode' => ['eng'], 'language' => ['English'])
+      expect(solr_document[:languageCode_ssim]).to eq(['eng'])
+      expect(solr_document[:language_ssim]).to eq(['English'])
+    end
+
+    it "indexes the project identifier" do
+      solr_document = solr_indexable.to_solr('project_identifier' => ['project id'])
+      expect(solr_document[:project_identifier_tesi]).to eq(['project id'])
+    end
+
+    it "indexes the digitization note" do
+      solr_document = solr_indexable.to_solr('digitization_note' => ['digitization note'])
+      expect(solr_document[:digitization_note_tesi]).to eq(['digitization note'])
+    end
+
+    it "indexes the ancestor titles" do
+      solr_document = solr_indexable.to_solr('ancestorTitles' => ['ancestor title'])
+      expect(solr_document[:ancestorTitles_tesim]).to eq(['ancestor title'])
+    end
+
+    it "indexes the subject headings" do
+      solr_document = solr_indexable.to_solr('subjectHeading' => ['Test > Test2', 'Two > Three > Four'])
+      expect(solr_document[:subjectHeading_ssim]).to eq(['Test > Test2', 'Two > Three > Four'])
+      expect(solr_document[:subjectHeadingFacet_ssim]).to eq(['Test', 'Test > Test2', 'Two', 'Two > Three', 'Two > Three > Four'])
+    end
+
+    it "indexes the collect creators" do
+      solr_document = solr_indexable.to_solr('ancestorCreator' => ['ancestor creator'])
+      expect(solr_document[:collectionCreators_ssim]).to eq(['ancestor creator'])
+    end
+    # rubocop:disable Lint/ParenthesesAsGroupedExpression
+    it "indexes the creators only" do
+      solr_document = solr_indexable.to_solr ({ 'ancestorCreator' => ['ancestor creator'], 'creator' => ['creator', 'creators', 'creatorx'] })
+      expect(solr_document[:creator_ssim]).to eq(['creator', 'creators', 'creatorx', 'ancestor creator'])
+    end
+
+    it "indexes the all the creators for the collect" do
+      solr_document = solr_indexable.to_solr ({ 'ancestorCreator' => ['ancestor creator', 'ancestor creator king'], 'creator' => ['creator', 'creators', 'creatorx', 'ancestor creator king'] })
+      expect(solr_document[:creator_ssim]).to eq(['creator', 'creators', 'creatorx', 'ancestor creator king', 'ancestor creator'])
+      expect(solr_document[:collectionCreators_ssim]).to eq(['ancestor creator'])
+    end
+    # rubocop:enable Lint/ParenthesesAsGroupedExpression
   end
 
-  it "indexes language and languageCode" do
-    solr_document = solr_indexable.to_solr('languageCode' => ['eng'], 'language' => ['English'])
-    expect(solr_document[:languageCode_ssim]).to eq(['eng'])
-    expect(solr_document[:language_ssim]).to eq(['English'])
-  end
 
-  it "indexes the project identifier" do
-    solr_document = solr_indexable.to_solr('project_identifier' => ['project id'])
-    expect(solr_document[:project_identifier_tesi]).to eq(['project id'])
-  end
+  describe "incomplete solr document" do
+    before do
+      allow(solr_indexable).to receive(:manifest_completed?).and_return(false)
+    end
 
-  it "indexes the digitization note" do
-    solr_document = solr_indexable.to_solr('digitization_note' => ['digitization note'])
-    expect(solr_document[:digitization_note_tesi]).to eq(['digitization note'])
+    it "deletes the incomplete record from solr" do
+      byebug
+    
+    end
   end
-
-  it "indexes the ancestor titles" do
-    solr_document = solr_indexable.to_solr('ancestorTitles' => ['ancestor title'])
-    expect(solr_document[:ancestorTitles_tesim]).to eq(['ancestor title'])
-  end
-
-  it "indexes the subject headings" do
-    solr_document = solr_indexable.to_solr('subjectHeading' => ['Test > Test2', 'Two > Three > Four'])
-    expect(solr_document[:subjectHeading_ssim]).to eq(['Test > Test2', 'Two > Three > Four'])
-    expect(solr_document[:subjectHeadingFacet_ssim]).to eq(['Test', 'Test > Test2', 'Two', 'Two > Three', 'Two > Three > Four'])
-  end
-
-  it "indexes the collect creators" do
-    solr_document = solr_indexable.to_solr('ancestorCreator' => ['ancestor creator'])
-    expect(solr_document[:collectionCreators_ssim]).to eq(['ancestor creator'])
-  end
-  # rubocop:disable Lint/ParenthesesAsGroupedExpression
-  it "indexes the creators only" do
-    solr_document = solr_indexable.to_solr ({ 'ancestorCreator' => ['ancestor creator'], 'creator' => ['creator', 'creators', 'creatorx'] })
-    expect(solr_document[:creator_ssim]).to eq(['creator', 'creators', 'creatorx', 'ancestor creator'])
-  end
-
-  it "indexes the all the creators for the collect" do
-    solr_document = solr_indexable.to_solr ({ 'ancestorCreator' => ['ancestor creator', 'ancestor creator king'], 'creator' => ['creator', 'creators', 'creatorx', 'ancestor creator king'] })
-    expect(solr_document[:creator_ssim]).to eq(['creator', 'creators', 'creatorx', 'ancestor creator king', 'ancestor creator'])
-    expect(solr_document[:collectionCreators_ssim]).to eq(['ancestor creator'])
-  end
-  # rubocop:enable Lint/ParenthesesAsGroupedExpression
 end
