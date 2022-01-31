464cf48914c4685a1a4c0812a9c5e0b19e78a243
JP Engstrom <jpengstrom@macbook-pro.lan>
2022-01-31 09:55:11 -0800

    refactor

--- app/models/concerns/solr_indexable.rb
+++ app/models/concerns/solr_indexable.rb
@@ -13,34 +13,27 @@ module SolrIndexable
       if self&.redirect_to.present?
         indexable = to_solr
       else
-        indexable, _child_solr_documents = to_solr_full_text
+        indexable, child_solr_documents = to_solr_full_text
       end
       return unless indexable.present?
       solr = SolrService.connection
-      check_for_incomplete
-      result = solr.commit
-      if (result&.[]("responseHeader")&.[]("status"))&.zero?
-        processing_event("Solr index updated", "solr-indexed")
+      case indexable
+      when indexable[:incomplete].present?
+        # deletes parent
+        solr.delete_by_id(oid.to_s)
+        # deletes children
+        solr.delete_by_query("parent_ssi:#{oid}")
       else
-        processing_event("Solr index after manifest generation failed", "failed")
+        solr.add([indexable])
+        solr.add(child_solr_documents) unless child_solr_documents.nil?
       end
+      result = solr.commit
+      (result&.[]("responseHeader")&.[]("status"))&.zero? ? processing_event("Solr index updated", "solr-indexed") : processing_event("Solr index after manifest generation failed", "failed")
     rescue => e
       processing_event("Solr indexing failed due to #{e.message}", "failed")
       raise # this reraises the error after we document it
     end
     result
-  end
-
-  def check_for_incomplete
-    if indexable[:incomplete].present?
-      # deletes parent
-      solr.delete_by_id(oid.to_s)
-      # deletes children
-      solr.delete_by_query("parent_ssi:#{oid}")
-    else
-      solr.add([indexable])
-      solr.add(child_solr_documents) unless child_solr_documents.nil?
-    end
   end
 
   def solr_delete
