0a39dd2bcd15098da1ce3dcba37a8f635d8c9015
Martin Lovell <martin.lovell@yale.edu>
2022-03-29 10:48:36 -0400

    update id in test, remove sleeps

--- spec/models/batch_process_spec.rb
+++ spec/models/batch_process_spec.rb
@@ -369,6 +369,7 @@ RSpec.describe BatchProcess, type: :model, prep_metadata_sources: true, prep_adm
         it "can create a parent_object with preservica values" do
           # rubocop:disable RSpec/AnyInstance
           allow_any_instance_of(SetupMetadataJob).to receive(:perform).and_return(true)
+          allow_any_instance_of(PreservicaImageService).to receive(:image_list).and_return(nil)
           # rubocop:enable RSpec/AnyInstance
           expect do
             batch_process.file = preservica_parent
@@ -379,7 +380,7 @@ RSpec.describe BatchProcess, type: :model, prep_metadata_sources: true, prep_adm
           expect(generated_po.aspace_uri).to eq "/repositories/11/archival_objects/515305"
           expect(generated_po.bib).to eq "36"
           expect(generated_po.digital_object_source).to eq "Preservica"
-          expect(generated_po.preservica_uri).to eq "/structural-objects/b4dbf905-0cff-45f1-90e2-a62b609d6a28"
+          expect(generated_po.preservica_uri).to eq "/structural-objects/7fe35e8c-c21a-444a-a2e2-e3c926b519c5"
           expect(generated_po.barcode).to eq "barcode"
           expect(generated_po.holding).to eq "holding"
           expect(generated_po.visibility).to eq "Public"
--- spec/models/preservica/preservica_import_pattern_two_spec.rb
+++ spec/models/preservica/preservica_import_pattern_two_spec.rb
@@ -116,8 +116,6 @@ RSpec.describe Preservica::PreservicaObject, type: :model, prep_metadata_sources
     expect(co_first.preservica_generation_uri).to eq "https://preservica-dev-v6.library.yale.edu/api/entity/content-objects/2e328d84-e429-4d46-a865-9ee11157b500/generations/1"
     expect(co_first.preservica_bitstream_uri).to eq "/home/app/webapp/spec/fixtures/preservica/api/entity/content-objects/2e328d84-e429-4d46-a865-9ee11157b500/generations/1/bitstreams/1"
     expect(co_first.sha512_checksum).to eq "1932c08c4670d5010fac6fa363ad5d9be7a4e7d743757ba5eefbbe8e3f9b2fb89b1604c1e527cfae6f47a91a60845268e91d2723aa63a90dd4735f75017569f7"
-    # allows time for the files to be created
-    sleep 2
     expect(File.exist?("spec/fixtures/images/access_masters/00/01/20/00/00/00/200000001.tif")).to be true
     expect(co_first.ptiff_conversion_at.present?).to be_truthy
     File.delete("spec/fixtures/images/access_masters/00/01/20/00/00/00/200000001.tif") if File.exist?("spec/fixtures/images/access_masters/00/01/20/00/00/00/200000001.tif")
@@ -143,8 +141,6 @@ RSpec.describe Preservica::PreservicaObject, type: :model, prep_metadata_sources
     expect(co_first.preservica_generation_uri).to eq "https://preservica-dev-v6.library.yale.edu/api/entity/content-objects/2e328d84-e429-4d46-a865-9ee11157b501/generations/1"
     expect(co_first.preservica_bitstream_uri).to eq "/home/app/webapp/spec/fixtures/preservica/api/entity/content-objects/2e328d84-e429-4d46-a865-9ee11157b501/generations/1/bitstreams/1"
     expect(co_first.sha512_checksum).to eq "1932c08c4670d5010fac6fa363ad5d9be7a4e7d743757ba5eefbbe8e3f9b2fb89b1604c1e527cfae6f47a91a60845268e91d2723aa63a90dd4735f75017569f7"
-    # allows time for the files to be created
-    # sleep 2
     expect(File.exist?("spec/fixtures/images/access_masters/00/01/20/00/00/00/200000001.tif")).to be true
     expect(File.exist?("spec/fixtures/images/access_masters/00/02/20/00/00/00/200000002.tif")).to be true
     expect(co_first.ptiff_conversion_at.present?).to be_truthy
@@ -176,8 +172,6 @@ RSpec.describe Preservica::PreservicaObject, type: :model, prep_metadata_sources
     expect(co_first.preservica_generation_uri).to eq "https://preservica-dev-v6.library.yale.edu/api/entity/content-objects/2e328d84-e429-4d46-a865-9ee11157b503/generations/1"
     expect(co_first.preservica_bitstream_uri).to eq "/home/app/webapp/spec/fixtures/preservica/api/entity/content-objects/2e328d84-e429-4d46-a865-9ee11157b503/generations/1/bitstreams/1"
     expect(co_first.sha512_checksum).to eq "1932c08c4670d5010fac6fa363ad5d9be7a4e7d743757ba5eefbbe8e3f9b2fb89b1604c1e527cfae6f47a91a60845268e91d2723aa63a90dd4735f75017569f7"
-    # allows time for the files to be created
-    # sleep 2
     expect(File.exist?("spec/fixtures/images/access_masters/00/01/20/00/00/00/200000001.tif")).to be true
     expect(File.exist?("spec/fixtures/images/access_masters/00/02/20/00/00/00/200000002.tif")).to be true
     expect(File.exist?("spec/fixtures/images/access_masters/00/03/20/00/00/00/200000003.tif")).to be true
