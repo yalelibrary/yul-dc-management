40ccf5c4fda0aec0ac0e3f67f4cc64a869275a59
Maggie Zhao <LixiaZhao@yale.edu>
2022-04-04 12:05:52 -0400

    Add checksum check for the master mets and valid
    Co-authored-by: Martin Lovell <martin.lovell@yale.edu>

--- app/models/child_object.rb
+++ app/models/child_object.rb
@@ -62,6 +62,7 @@ class ChildObject < ApplicationRecord
     @access_master_path = File.join(image_mount, directory, pairtree_path, "#{oid}.tif")
   end
 
+  # rubocop:disable  Metrics/MethodLength
   def copy_to_access_master_pairtree
     # Don't copy over existing access masters if they already exist
     # TODO: Determine what happens if it's an intentional re-shoot of a child image
@@ -73,14 +74,16 @@ class ChildObject < ApplicationRecord
       processing_event("Not copied from Goobi package to access master pair-tree, already exists", 'access-master-exists')
       return true
     end
+    unless mets_access_master_checksum_matches?
+      processing_event("Original Copy of checksum does not match", 'failed')
+      false
+    end
     image_mount = ENV['ACCESS_MASTER_MOUNT'] || "data"
     pairtree_path = Partridge::Pairtree.oid_to_pairtree(oid)
     directory = format("%02d", pairtree_path.first)
     # Create path to access master if it doesn't exist
-    if checksum =~ /b([a-f0-9]{40})b/
-      FileUtils.mkdir_p(File.join(image_mount, directory, pairtree_path))
-      FileUtils.cp(mets_access_master_path, access_master_path)
-    end
+    FileUtils.mkdir_p(File.join(image_mount, directory, pairtree_path))
+    FileUtils.cp(mets_access_master_path, access_master_path)
     if access_master_checksum_matches?
       processing_event("Copied from Goobi package to access master pair-tree", 'goobi-copied')
       true
@@ -89,10 +92,16 @@ class ChildObject < ApplicationRecord
       false
     end
   end
+  # rubocop:enable  Metrics/MethodLength
 
   def access_master_checksum_matches?
     access_master_checksum = Digest::SHA1.file(access_master_path).to_s
     checksum == access_master_checksum
+  end
+
+  def mets_access_master_checksum_matches?
+    mets_master_checksum = Digest::SHA1.file(mets_access_master_path).to_s
+    checksum == mets_master_checksum
   end
 
   def access_master_exists?
