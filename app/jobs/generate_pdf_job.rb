44df52d58a76c8cda0fb64ffa119e8b4a79ba2c4
Martin Lovell <martin.lovell@yale.edu>
2022-04-07 10:54:20 -0400

    Prevent queueing PDF Job and Generating Manifest for Redirects - Prevent queueing the PDF job for ParentObjects which are redirects - Update properties for redirects before update rather than after and remove solr_index in the Active Record callback - Prevent manifest generation for redirects, but still solr index in that job. - Exit PDF job for redirects defensively and for PDF Jobs that were previously queued - After reassociate, skip Manifest job and go to Solr Index for redirects

--- app/jobs/generate_pdf_job.rb
+++ app/jobs/generate_pdf_job.rb
@@ -8,6 +8,7 @@ class GeneratePdfJob < ApplicationJob
   end
 
   def perform(parent_object, current_batch_process = parent_object.current_batch_process, current_batch_connection = parent_object.current_batch_connection)
+    return unless parent_object.should_create_manifest_and_pdf?
     parent_object.current_batch_process = current_batch_process
     parent_object.current_batch_connection = current_batch_connection
     parent_object.generate_pdf
