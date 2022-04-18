be13ec326d01bffd604965922b4f59d7d96cc55f
K8Sewell <kait@notch8.com>
2022-04-18 10:22:57 -0700

    Fixes spec

--- spec/requests/parent_objects_spec.rb
+++ spec/requests/parent_objects_spec.rb
@@ -49,6 +49,17 @@ RSpec.describe "/parent_objects", type: :request, prep_metadata_sources: true, p
     }
   end
 
+  let(:valid_preservica_attributes) do
+    {
+      oid: "200000000",
+      authoritative_metadata_source_id: 1,
+      admin_set: AdminSet.find_by_key('brbl'),
+      bib: "123",
+      preservica_uri: '/preservica/api/entity/structural-objects/7fe35e8c-c21a-444a-a2e2-e3c926b519c5',
+      preservica_representation_name: 'Preservation-1'
+    }
+  end
+
   describe "GET /index" do
     it "renders a successful response" do
       ParentObject.create! valid_attributes
@@ -201,8 +212,64 @@ RSpec.describe "/parent_objects", type: :request, prep_metadata_sources: true, p
   end
 
   describe '#sync_from_preservica' do
+    around do |example|
+      preservica_host = ENV['PRESERVICA_HOST']
+      preservica_creds = ENV['PRESERVICA_CREDENTIALS']
+      ENV['PRESERVICA_HOST'] = "testpreservica"
+      ENV['PRESERVICA_CREDENTIALS'] = '{"brbl": {"username":"xxxxx", "password":"xxxxx"}}'
+      access_host = ENV['ACCESS_MASTER_MOUNT']
+      ENV['ACCESS_MASTER_MOUNT'] = File.join("spec", "fixtures", "images", "access_masters")
+      perform_enqueued_jobs do
+        example.run
+      end
+      ENV['PRESERVICA_HOST'] = preservica_host
+      ENV['PRESERVICA_CREDENTIALS'] = preservica_creds
+      ENV['ACCESS_MASTER_MOUNT'] = access_host
+    end
+
+    before do
+      stub_metadata_cloud("AS-200000000", "aspace")
+      stub_request(:post, "https://testpreservica/api/accesstoken/login").to_return(status: 200, body: '{"token":"test"}')
+      stub_request(:post, "https://testpreservica/api/accesstoken/refresh").to_return(status: 200, body: '{"token":"test"}')
+      fixtures = %w[preservica/api/entity/structural-objects/7fe35e8c-c21a-444a-a2e2-e3c926b519c5/children
+                    preservica/api/entity/information-objects/1e42a2bb-8953-41b6-bcc3-1a19c86a5e3r/representations
+                    preservica/api/entity/information-objects/1e42a2bb-8953-41b6-bcc3-1a19c86a5e3r/representations/Access-2
+                    preservica/api/entity/information-objects/1e42a2bb-8953-41b6-bcc3-1a19c86a5e3r/representations/Preservation-1
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b486/generations
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b486/generations/1
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b486/generations/1/bitstreams/1
+                    preservica/api/entity/information-objects/1e42a2bb-8953-41b6-bcc3-1a19c86a5e3d/representations
+                    preservica/api/entity/information-objects/1e42a2bb-8953-41b6-bcc3-1a19c86a5e3d/representations/Access-2
+                    preservica/api/entity/information-objects/1e42a2bb-8953-41b6-bcc3-1a19c86a5e3d/representations/Preservation-1
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b489/generations
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b489/generations/1
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b489/generations/1/bitstreams/1
+                    preservica/api/entity/information-objects/f44ba97e-af2b-498e-b118-ed1247822f44/representations
+                    preservica/api/entity/information-objects/f44ba97e-af2b-498e-b118-ed1247822f44/representations/Access-2
+                    preservica/api/entity/information-objects/f44ba97e-af2b-498e-b118-ed1247822f44/representations/Preservation-1
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b487/generations
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b487/generations/1
+                    preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b487/generations/1/bitstreams/1]
+
+      fixtures.each do |fixture|
+        stub_request(:get, "https://test#{fixture}").to_return(
+          status: 200, body: File.open(File.join(fixture_path, "#{fixture}.xml"))
+        )
+      end
+      stub_request(:get, "https://testpreservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b487/generations/1/bitstreams/1/content").to_return(
+        status: 200, body: File.open(File.join(fixture_path, "preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b487/generations/1/bitstreams/1/content.tif"), 'rb')
+      )
+      stub_request(:get, "https://testpreservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b486/generations/1/bitstreams/1/content").to_return(
+        status: 200, body: File.open(File.join(fixture_path, "preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b486/generations/1/bitstreams/1/content.tif"), 'rb')
+      )
+      stub_request(:get, "https://testpreservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b489/generations/1/bitstreams/1/content").to_return(
+        status: 200, body: File.open(File.join(fixture_path, "preservica/api/entity/content-objects/ae328d84-e429-4d46-a865-9ee11157b489/generations/1/bitstreams/1/content.tif"), 'rb')
+      )
+    end
+
     it 'redirects to the parent_objects list' do
-      parent_object = ParentObject.create! valid_attributes
+      parent_object = ParentObject.create! valid_preservica_attributes
+      expect(parent_object.child_objects.count).to eq 0
       post sync_from_preservica_parent_object_url(parent_object)
       expect(response).to redirect_to(parent_object_url(parent_object))
       expect(flash[:notice]).to eq('This object has been queued for synchronization of child objects from Preservica.')
