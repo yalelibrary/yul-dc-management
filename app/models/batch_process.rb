0576759ff2b5bc5b4a193104930eeccd783d10a2
Martin Lovell <martin.lovell@yale.edu>
2022-06-02 11:04:47 -0400

    Update child deletion during sync, updated found_in_preservica

--- app/models/batch_process.rb
+++ app/models/batch_process.rb
@@ -419,8 +419,8 @@ class BatchProcess < ApplicationRecord # rubocop:disable Metrics/ClassLength
   # SYNC IMAGES FROM PRESERVICA
   def sync_images_preservica(local_children_hash, preservica_children_hash, parent_object)
     if local_children_hash != preservica_children_hash
-      parent_object.sync_from_preservica(local_children_hash, preservica_children_hash)
       setup_for_background_jobs(parent_object, parent_object.source_name)
+      parent_object.sync_from_preservica(local_children_hash, preservica_children_hash)
     elsif fails_fixity_check(parent_object)
       batch_processing_event("Checksum mismatch found on parent object: #{parent_object.oid}", "Row Processed")
     else
