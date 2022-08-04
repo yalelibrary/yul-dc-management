c92fdb24ce998c5344a912f1ce0ad8b1f7ef3c2a
K8Sewell <kait@notch8.com>
2022-08-04 11:33:23 -0700

    Pull auth with token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   include JwtWebToken
-  before_action :authenticate_with_token, :authenticate_user!
+  before_action :authenticate_user!
   rescue_from CanCan::AccessDenied do |exception|
     @error_message = exception.message
     render plain: "Access denied", status: 401
@@ -13,16 +13,6 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-
-    def authenticate_with_token
-      header = request.headers['Authorization']
-      header = header.split(' ').last if header
-      return unless header
-      decoded_token = jwt_decode(header)
-      token_user = User.where(id: decoded_token[:user_id]).first
-      return unless token_user&.active_for_authentication?
-      sign_in token_user, event: :authentication # this will throw if @user is not activated
-    end
 
     def authenticate_user!
       if user_signed_in?
