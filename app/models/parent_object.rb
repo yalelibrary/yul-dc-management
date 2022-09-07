68fc1953ed7697c4e9720d82dc355dc9c8bdb94f
K8Sewell <kait@notch8.com>
2022-09-07 10:28:36 -0700

    Manually complete batch process status

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -140,6 +140,7 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
         self.last_preservica_update = Time.current
         self.metadata_update = true
         save!
+        batch_processes[0].batch_status = "Complete"
       end
     else
       return unless ladybird_json
