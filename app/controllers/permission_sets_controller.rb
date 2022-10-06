c8ba4777ab69047b5db0bef39548c4a5cf01d5fe
JP Engstrom <jp@scientist.com>
2022-10-06 11:04:14 -0700

    Permission Set Menu Item and Index (#1073)
    * users with certain roles can access permission sets and only view their sets
    
    * rubocop
    
    Co-authored-by: JP Engstrom <jpengstrom@vpn172251393.its.yale.internal>
    Co-authored-by: JP Engstrom <jpengstrom@macbook-pro.lan>

---
+++ app/controllers/permission_sets_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class PermissionSetsController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_permission_set, only: [:show, :edit, :update, :destroy]
+
+  # GET /permission_sets
+  # GET /permission_sets.json
+  def index
+    authorize!(:view_list, PermissionSet)
+
+    permission_sets = PermissionSet.all
+    @visible_permission_sets = permission_sets.order('label ASC').select do |sets|
+      User.with_role(:approver, sets).include?(current_user) ||
+        User.with_role(:administrator, sets).include?(current_user) ||
+        User.with_role(:sysadmin, sets).include?(current_user)
+    end
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_permission_set
+      @permission_set = PermissionSet.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def permission_set_params
+      params.require(:permission_set).permit(:key, :label)
+    end
+end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -18,6 +18,7 @@ class Ability
     can :reindex_admin_set, AdminSet, roles: { name: editor_roles, users: { id: user.id } }
     can [:crud], ChildObject, parent_object: { admin_set: { roles: { name: editor_roles, users: { id: user.id } } } }
     can [:crud], ParentObject, admin_set: { roles: { name: editor_roles, users: { id: user.id } } }
+    can [:view_list], PermissionSet if user.has_role?(:sysadmin) || user.has_role?(:approver, :any) || user.has_role?(:administrator, :any)
     can [:read, :approve], PermissionSet, roles: { name: approver_roles, users: { id: user.id } }
     can [:crud, :approve], PermissionSet, roles: { name: administrator_roles, users: { id: user.id } }
   end
