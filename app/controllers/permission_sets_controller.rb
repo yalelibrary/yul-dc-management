bf1185e5ef00420d5d9b8f128c8c4e186d196f8e
JP Engstrom <jpengstrom@macbook-pro.lan>
2022-10-06 09:11:54 -0700

    rubocop

--- app/controllers/permission_sets_controller.rb
+++ app/controllers/permission_sets_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PermissionSetsController < ApplicationController
   load_and_authorize_resource
   before_action :set_permission_set, only: [:show, :edit, :update, :destroy]
@@ -10,10 +12,9 @@ class PermissionSetsController < ApplicationController
     permission_sets = PermissionSet.all
     @visible_permission_sets = permission_sets.order('label ASC').select do |sets|
       User.with_role(:approver, sets).include?(current_user) ||
-      User.with_role(:administrator, sets).include?(current_user) || 
-      User.with_role(:sysadmin, sets).include?(current_user)
+        User.with_role(:administrator, sets).include?(current_user) ||
+        User.with_role(:sysadmin, sets).include?(current_user)
     end
-
   end
 
   private
