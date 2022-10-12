ad1f0e5ce719126961d7a77d6598dc45c43c2168
JP Engstrom <jpengstrom@vpn17225168240.its.yale.internal>
2022-10-12 13:36:13 -0700

    rubocop

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   include CanCan::Ability
 
   # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   def initialize(user)
     alias_action :create, :read, :update, :destroy, to: :crud
     return unless user
@@ -19,12 +21,13 @@ class Ability
     can [:crud], ChildObject, parent_object: { admin_set: { roles: { name: editor_roles, users: { id: user.id } } } }
     can [:crud], ParentObject, admin_set: { roles: { name: editor_roles, users: { id: user.id } } }
     can [:view_list], PermissionSet if user.has_role?(:sysadmin) || user.has_role?(:approver, :any) || user.has_role?(:administrator, :any)
-    can [:create_set], PermissionSet if user.has_role?(:sysadmin) || user.has_role?(:administrator, :any)
-    can [:crud], PermissionSet if user.has_role?(:sysadmin) || user.has_role?(:administrator, :any)
+    can [:create_set, :crud], PermissionSet if user.has_role?(:sysadmin) || user.has_role?(:administrator, :any)
     can [:read, :approve], PermissionSet, roles: { name: approver_roles, users: { id: user.id } }
     can [:crud, :approve], PermissionSet, roles: { name: administrator_roles, users: { id: user.id } }
   end
   # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/PerceivedComplexity
 
   def apply_sysadmin_abilities
     can :manage, User
