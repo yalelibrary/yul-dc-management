4721694ff40c3e8fb9deab0da0a79715f559166b
Martin Lovell <martin.lovell@yale.edu>
2022-10-18 16:22:47 -0400

    Fix tests

--- spec/jobs/problem_report_job_spec.rb
+++ spec/jobs/problem_report_job_spec.rb
@@ -22,4 +22,12 @@ RSpec.describe ProblemReportManualJob, type: :job do
     expect(problem_report).to receive(:generate_child_problem_csv).once
     problem_report_job.perform(problem_report)
   end
+
+  it "increments job queue once per day" do
+    now = Time.zone.today
+    ActiveJob::Scheduler.start
+    new_time = now + 1.day
+    Timecop.travel(new_time)
+    expect(Delayed::Job.where('handler LIKE ?', '%job_class: ProblemReportJob%').count).to eq 1
+  end
 end
