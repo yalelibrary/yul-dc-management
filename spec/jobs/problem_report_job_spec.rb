4cd8e85a9e62d102a950e54226d5ad2ab4148934
Martin Lovell <martin.lovell@yale.edu>
2022-10-18 16:36:47 -0400

    Fix timecop test

--- spec/jobs/problem_report_job_spec.rb
+++ spec/jobs/problem_report_job_spec.rb
@@ -23,11 +23,21 @@ RSpec.describe ProblemReportManualJob, type: :job do
     problem_report_job.perform(problem_report)
   end
 
-  it "increments job queue once per day" do
-    now = Time.zone.today
-    ActiveJob::Scheduler.start
-    new_time = now + 1.day
-    Timecop.travel(new_time)
-    expect(Delayed::Job.where('handler LIKE ?', '%job_class: ProblemReportJob%').count).to eq 1
+  describe "jumping ahead one day" do
+    before do
+      Timecop.freeze(Time.zone.today)
+    end
+
+    after do
+      Timecop.return
+    end
+
+    it "increments job queue once per day" do
+      now = Time.zone.today
+      ActiveJob::Scheduler.start
+      new_time = now + 1.day
+      Timecop.travel(new_time)
+      expect(Delayed::Job.where('handler LIKE ?', '%job_class: ProblemReportJob%').count).to eq 1
+    end
   end
 end
