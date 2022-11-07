87179ddad07d8470adcc701041bea5ffa7dd449f
K8Sewell <kait@notch8.com>
2022-11-07 16:47:21 -0600

    Adds specs and drag by extension

--- spec/system/structure_editor_spec.rb
+++ spec/system/structure_editor_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe "Structure Editor", type: :system, prep_metadata_sources: true, p
   let(:admin_set) { FactoryBot.create(:admin_set, key: 'brbl', label: 'brbl') }
   # parent object has two child objects
   let(:parent_object) { FactoryBot.create(:parent_object, oid: '16172421', admin_set_id: admin_set.id) }
-  let(:child_object) { FactoryBot.create(:child_object, oid: '100001', parent_object: parent_object, caption: 'bola') }
   let(:iiif_presentation) { IiifPresentationV3.new(parent_object) }
-  
+
   around do |example|
     original_manifests_base_url = ENV['IIIF_MANIFESTS_BASE_URL']
     original_image_base_url = ENV["IIIF_IMAGE_BASE_URL"]
@@ -38,30 +37,97 @@ RSpec.describe "Structure Editor", type: :system, prep_metadata_sources: true, p
     stub_request(:put, "https://#{ENV['SAMPLE_BUCKET']}.s3.amazonaws.com/manifests/21/16/17/24/21/16172421.json")
       .to_return(status: 200)
     stub_metadata_cloud("16172421")
-    stub_metadata_cloud("100001")
   end
 
-  # manifest loads in structure editor
   describe 'can access the homepage' do
     it 'can render a manifest' do
-      visit "structure-editor/?manifest=#{parent_object_url(parent_object.oid)}/manifest&token=#{user.token}"
-      expect(page).to have_content("#{child_object.oid}")
+      visit '/structure-editor/'
+      click_on 'Get Manifest'
+      fill_in 'API Key', with: user.token
+      fill_in 'Manifest', with: "#{root_path}parent_objects/16172421/manifest.json"
+      click_on 'OK'
+      # loads manifest
+      expect(page).to have_content('Manifest Downloaded')
+      # loads child objects
+      expect(page).to have_content('16188699')
     end
   end
 
+  describe 'can perform actions' do
+    before do
+      visit '/structure-editor/'
+      click_on 'Get Manifest'
+      fill_in 'API Key', with: user.token
+      fill_in 'Manifest', with: "#{root_path}parent_objects/16172421/manifest.json"
+      # submit dialogue
+      click_on 'OK'
+      # acknowledge manifest loaded
+      click_on 'OK'
+    end
+
+    it 'can add a range' do
+      click_on 'Range +'
+      expect(page).to have_content('New Range')
+    end
+
+    it 'can add a canvas' do
+      click_on 'Range +'
+      find('.ant-tree-title').click
+      find('.item-label', match: :first).click
+      click_on 'Canvas +'
+      expect(page).to have_content('16188699').twice
+    end
+
+    it 'can delete a range' do
+      click_on 'Range +'
+      find('.ant-tree-title').click
+      find('.fa-xmark').click
+      expect(page).not_to have_content('New Range')
+    end
+
+    it 'can delete a canvas' do
+      click_on 'Range +'
+      find('.ant-tree-title').click
+      find('.item-label', match: :first).click
+      click_on 'Canvas +'
+      # finds the canvas label
+      find(:xpath, '(//span[@class="ant-tree-title"])[2]').click
+      find('.fa-xmark').click
+      expect(page).to have_content('16188699').once
+    end
+
+    it 'can change range title' do
+      click_on 'Range +'
+      # finds range label
+      find(:xpath, '(//html/body/div/section/section/aside/div/div/div[3]/div/div/div/div[1]/span[4]/span/span/span[2]/span)').double_click
+      find(:xpath, '//html/body/div/section/section/aside/div/div/div[3]/div/div/div/div[1]/span[4]/span/span/span[2]').fill_in with: 'Different Range'
+      find('.item-label', match: :first).click
+      expect(page).to have_content('Different Range')
+    end
+
+    it 'can submit structure back to management' do
+      click_on 'Range +'
+      click_on 'Submit'
+      visit '/parent_objects/16172421/manifest.json'
+      expect(page).to have_content('New Range')
+    end
+
+    it 'can drag a canvas' do
+      click_on 'Range +'
+      # finds first range label
+      find('.ant-tree-title').click
+      find('.item-label', match: :first).click
+      click_on 'Canvas +'
+      click_on 'Range +'
+      # finds second range label
+      find(:xpath, '(//html/body/div/section/section/aside/div/div/div[3]/div/div/div/div[3]/span[4]/span/span/span[2]/span)').click
+      find('.item-label', match: :first).click
+      click_on 'Canvas +'
+      second_canvas = find(:xpath, '(//html/body/div/section/section/aside/div/div/div[3]/div/div/div/div[4]/span[4]/span/span/span[1]/img)')
+      second_canvas.drag_by(0, 5)
+      within('.ant-tree-list') do
+        expect(page).to have_content('16188699').twice
+      end
+    end
+  end
 end
-
-
-
-
-# can add range
-
-# can add canvas
-
-# can delete range
-
-# can delete canvas
-
-# can drag canvas
-
-# can submit structure back to management
