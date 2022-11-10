396c8f7e0e9f9e15b7c030eb5cd03da9cc77c733
K8Sewell <kait@notch8.com>
2022-11-10 10:56:22 -0600

    Remove chrome options add cookie patch

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -13,15 +13,9 @@ capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
   }
 )
 
-options = Selenium::WebDriver::Chrome::Options.new(args: ["headless"])
-options.add_argument(
-  "--enable-features=NetworkService,NetworkServiceInProcess"
-)
-
 Capybara.register_driver :chrome do |app|
   d = Capybara::Selenium::Driver.new(app,
                                      browser: :remote,
-                                     options: options,
                                      desired_capabilities: capabilities,
                                      url: "http://chrome:4444/wd/hub")
   # Fix for capybara vs remote files. Selenium handles this for us
@@ -47,5 +41,28 @@ RSpec.configure do |config|
     # rails system specs reset app_host each time so needs to be forced on each test
     Capybara.app_host = "http://#{ENV['WEB_HOST']}:#{Capybara.server_port}"
     driven_by :chrome
+  end
+end
+
+# @TODO Remove this Monkey Patch after issue is resolved with running Chrome 74 in
+#       headless mode: https://github.com/teamcapybara/capybara/issues/2181
+module Selenium
+  module WebDriver
+    class Options
+      # capybara/rspec installs a RSpec callback that runs after each test and resets
+      # the session - part of which is deleting all cookies. However the call to Chrome
+      # Webdriver to delete all cookies in Chrome 74 hangs when run in headless mode
+      # (the reasons for which are still unknown).
+      #
+      # Fortunately, the call to set a cookie is still functioning and we can rely
+      # on expired cookies being cleared by Chrome, so we iterate over all current
+      # cookies and set their expiry date to some time in the past - effectively
+      # deleting them.
+      def delete_all_cookies
+        all_cookies.each do |cookie|
+          add_cookie(name: cookie[:name], value: '', expires: Time.zone.now - 1.second)
+        end
+      end
+    end
   end
 end
