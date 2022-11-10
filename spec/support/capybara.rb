c21e8cf778e3b38000737ba782dd4495f9c87089
K8Sewell <kait@notch8.com>
2022-11-10 11:14:43 -0600

    Remove patch and extend wait time

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # TODO  Webdrivers.cache_time = 3
-Capybara.default_max_wait_time = 8
+Capybara.default_max_wait_time = 10
 Capybara.default_driver = :rack_test
 
 # Setup chrome headless driver
@@ -41,28 +41,5 @@ RSpec.configure do |config|
     # rails system specs reset app_host each time so needs to be forced on each test
     Capybara.app_host = "http://#{ENV['WEB_HOST']}:#{Capybara.server_port}"
     driven_by :chrome
-  end
-end
-
-# @TODO Remove this Monkey Patch after issue is resolved with running Chrome 74 in
-#       headless mode: https://github.com/teamcapybara/capybara/issues/2181
-module Selenium
-  module WebDriver
-    class Options
-      # capybara/rspec installs a RSpec callback that runs after each test and resets
-      # the session - part of which is deleting all cookies. However the call to Chrome
-      # Webdriver to delete all cookies in Chrome 74 hangs when run in headless mode
-      # (the reasons for which are still unknown).
-      #
-      # Fortunately, the call to set a cookie is still functioning and we can rely
-      # on expired cookies being cleared by Chrome, so we iterate over all current
-      # cookies and set their expiry date to some time in the past - effectively
-      # deleting them.
-      def delete_all_cookies
-        all_cookies.each do |cookie|
-          add_cookie(name: cookie[:name], value: '', expires: Time.zone.now - 1.second)
-        end
-      end
-    end
   end
 end
