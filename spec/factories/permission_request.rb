b3232aab01d2cfc7de94d2dfcbab2293c2e14a8f
JP Engstrom <jpengstrom@macbook-pro.lan>
2022-12-01 11:25:38 -0800

    fix broken specs

--- spec/factories/permission_request.rb
+++ spec/factories/permission_request.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :permission_request do
     parent_object { FactoryBot.create(:parent_object) }
-    user { FactoryBot.create(:user) }
     permission_request_user { FactoryBot.create(:permission_request_user) }
     permission_set { FactoryBot.create(:permission_set) }
   end
--- spec/models/permission_request_spec.rb
+++ spec/models/permission_request_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe PermissionRequest, type: :model do
   describe PermissionRequest do
     it { is_expected.to belong_to(:permission_set) }
     it { is_expected.to belong_to(:permission_request_user) }
-    it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:parent_object) }
   end
 end
