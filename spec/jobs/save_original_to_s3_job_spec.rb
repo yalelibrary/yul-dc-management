987c4ae3301de7784310f8e54575c0574f257726
K8Sewell <kait@notch8.com>
2022-12-21 11:32:42 -0600

    Expand specs and add to method

--- spec/jobs/save_original_to_s3_job_spec.rb
+++ spec/jobs/save_original_to_s3_job_spec.rb
@@ -9,25 +9,40 @@ RSpec.describe SaveOriginalToS3Job, type: :job do
   let(:user) { FactoryBot.create(:user) }
   let(:batch_process) { FactoryBot.create(:batch_process, user: user) }
   let(:metadata_source) { FactoryBot.create(:metadata_source) }
-  let(:parent_object_private) { FactoryBot.create(:parent_object, oid: 2_004_628, authoritative_metadata_source: metadata_source, visibility: "Private") }
-  let(:child_object) { FactoryBot.create(:child_object, oid: "456789", parent_object: parent_object_private) }
+  let(:parent_object_private) { FactoryBot.create(:parent_object, oid: 2_004_628, authoritative_metadata_source: metadata_source, visibility: 'Private') }
+  let(:child_object) { FactoryBot.create(:child_object, oid: '456789', parent_object: parent_object_private) }
   let(:save_to_s3_job) { SaveOriginalToS3Job.new }
-  let(:parent_object_with_authoritative_json) { FactoryBot.build(:parent_object, oid: '16712419', ladybird_json: JSON.parse(File.read(File.join(fixture_path, "ladybird", "16712419.json")))) }
+  let(:parent_object_with_authoritative_json) do
+    FactoryBot.build(:parent_object,
+                     oid: '16712419',
+                     visibility: 'Public',
+                     ladybird_json: JSON.parse(File.read(File.join(fixture_path, 'ladybird', '16712419.json'))))
+  end
+  let(:child_object_with_authoritative_json) { FactoryBot.create(:child_object, oid: '345678', parent_object: parent_object_with_authoritative_json) }
 
   around do |example|
-    original_image_bucket = ENV["S3_SOURCE_BUCKET_NAME"]
-    ENV["S3_SOURCE_BUCKET_NAME"] = "not-a-real-bucket"
+    original_image_bucket = ENV['S3_SOURCE_BUCKET_NAME']
+    original_access_master_mount = ENV["ACCESS_MASTER_MOUNT"]
+    ENV['S3_SOURCE_BUCKET_NAME'] = 'not-a-real-bucket'
+    ENV["ACCESS_MASTER_MOUNT"] = File.join(fixture_path, "images/ptiff_images")
     example.run
-    ENV["S3_SOURCE_BUCKET_NAME"] = original_image_bucket
+    ENV['S3_SOURCE_BUCKET_NAME'] = original_image_bucket
+    ENV["ACCESS_MASTER_MOUNT"] = original_access_master_mount
   end
 
   before do
-    stub_request(:put, "https://not-a-real-bucket.s3.amazonaws.com/download/tiff/89/45/67/89/456789.tif")
-        .to_return(status: 200)
-    stub_request(:head, "https://not-a-real-bucket.s3.amazonaws.com/originals/89/45/67/89/456789.tif")
-        .to_return(status: 200, body: "", headers: {})
-    stub_request(:head, "https://not-a-real-bucket.s3.amazonaws.com/ptiffs/89/45/67/89/456789.tif")
-        .to_return(status: 200, body: "", headers: {})
+    stub_request(:head, 'https://not-a-real-bucket.s3.amazonaws.com/download/tiff/78/34/56/78/345678.tif')
+        .to_return(status: 200, body: '', headers: {})
+    stub_request(:head, 'https://not-a-real-bucket.s3.amazonaws.com/originals/78/34/56/78/345678.tif')
+        .to_return(status: 200, body: '', headers: {})
+    stub_request(:head, 'https://not-a-real-bucket.s3.amazonaws.com/ptiffs/78/34/56/78/345678.tif')
+        .to_return(status: 200, body: '', headers: {})
+    stub_request(:head, 'https://not-a-real-bucket.s3.amazonaws.com/download/tiff/89/45/67/89/456789.tif')
+        .to_return(status: 404, body: '', headers: {}).times(1).then.to_return(status: 200, body: '', headers: {})
+    stub_request(:head, 'https://not-a-real-bucket.s3.amazonaws.com/originals/89/45/67/89/456789.tif')
+        .to_return(status: 200, body: '', headers: {})
+    stub_request(:head, 'https://not-a-real-bucket.s3.amazonaws.com/ptiffs/89/45/67/89/456789.tif')
+        .to_return(status: 200, body: '', headers: {})
     child_object
   end
 
@@ -35,21 +50,21 @@ RSpec.describe SaveOriginalToS3Job, type: :job do
     it 'throws exception with Private or Redirect visibility' do
       expect do
         save_to_s3_job.perform(child_object.oid)
-      end.to raise_error("Not copying image from #{parent_object_private.oid}. Parent object must have Public or Yale Community Only visibility.")
+      end.to raise_error(an_instance_of(RuntimeError).and(having_attributes(message: 'Not copying image from 2004628. Parent object must have Public or Yale Community Only visibility.')))
     end
     it 'throws exception when file is already in S3' do
-      parent_object_private.visibility = "Public"
-      parent_object_private.save
       expect do
-        save_to_s3_job.perform(child_object.oid)
-      end.to raise_error("Not copying image. Child object #{child_object.oid} already exists on S3.")
+        save_to_s3_job.perform(child_object_with_authoritative_json.oid)
+      end.to raise_error(an_instance_of(RuntimeError).and(having_attributes(message: "Not copying image. Child object #{child_object_with_authoritative_json.oid} already exists on S3.")))
     end
-    it "has correct priority" do
+    it 'has correct priority' do
       expect(save_to_s3_job.default_priority).to eq(100)
     end
-    it "can save a file to S3" do
-      # allow(S3Service).to receive(:remote_metadata).and_return(parent_object_with_authoritative_json)
+    it 'can save a file to S3' do
+      parent_object_private.visibility = 'Public'
+      parent_object_private.save
       save_to_s3_job.perform(child_object.oid)
+      expect(S3Service.s3_exists?('https://not-a-real-bucket.s3.amazonaws.com/download/tiff/89/45/67/89/456789.tif'))
     end
   end
 end
