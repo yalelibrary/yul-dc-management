b0eaa99449b4473c0afab29d36e268ee85db3fd4
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-01-04 15:57:24 -0800

    rubocop

--- spec/requests/permission_set_spec.rb
+++ spec/requests/permission_set_spec.rb
@@ -32,12 +32,12 @@ RSpec.describe 'Permission Sets', type: :request, prep_metadata_sources: true, p
   end
 
   describe 'update /permission_sets' do
-
     context 'with valid attributes' do
       it 'updates permission set' do
         permission_set = PermissionSet.create(valid_attributes)
         patch permission_set_url(permission_set), params: { permission_set: updated_attributes }
         permission_set.reload
+        expect(permission_set.key).to eq "New Key"
         expect(response).to have_http_status(:found)
         expect(response).to have_http_status(302)
       end
@@ -47,10 +47,8 @@ RSpec.describe 'Permission Sets', type: :request, prep_metadata_sources: true, p
     #   it 'updates permission set' do
     #     permission_set = PermissionSet.create(valid_attributes)
     #     patch permission_set_url(permission_set), params: { permission_set: invalid_attributes }
-    #     byebug
     #     expect(response).to have_http_status(:unprocessable_entity)
     #   end
     # end
-
   end
 end
