ee0687cf481fe19005580e9abe8fe211fc5415d1
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-01-04 15:46:12 -0800

    permission set request spec

---
+++ spec/requests/permission_set_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Permission Sets', type: :request, prep_metadata_sources: true, prep_admin_sets: true do
+  let(:user) { FactoryBot.create(:sysadmin_user) }
+
+  before do
+    login_as user
+    # permission_set
+  end
+
+  let(:valid_attributes) do
+    {
+      key: "New Key",
+      label: "New Label"
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      key: "",
+      label: "New Label"
+    }
+  end
+
+  let(:updated_attributes) do
+    {
+      key: "New Key",
+      label: "New Label"
+    }
+  end
+
+  describe 'update /permission_sets' do
+
+    context 'with valid attributes' do
+      it 'updates permission set' do
+        permission_set = PermissionSet.create(valid_attributes)
+        patch permission_set_url(permission_set), params: { permission_set: updated_attributes }
+        permission_set.reload
+        expect(response).to have_http_status(:found)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    # context 'with invalid attributes' do
+    #   it 'updates permission set' do
+    #     permission_set = PermissionSet.create(valid_attributes)
+    #     patch permission_set_url(permission_set), params: { permission_set: invalid_attributes }
+    #     byebug
+    #     expect(response).to have_http_status(:unprocessable_entity)
+    #   end
+    # end
+
+  end
+end
