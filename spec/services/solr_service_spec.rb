14b7389824e057fdb9b5dc4c32c3e0aec7391f65
K8Sewell <kait@notch8.com>
2023-01-05 15:28:43 -0600

    Updates expectations

--- spec/services/solr_service_spec.rb
+++ spec/services/solr_service_spec.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe SolrService, solr: true do
+RSpec.describe SolrService, solr: true, prep_metadata_sources: true do
   index_analyzer = {
-    "tokenizer": {
-      class: "solr.WhitespaceTokenizerFactory"
+    'tokenizer': {
+      class: 'solr.WhitespaceTokenizerFactory'
     },
-    "filters": [
-      { class: "solr.WordDelimiterGraphFilterFactory" },
-      { class: "solr.FlattenGraphFilterFactory" }
+    'filters': [
+      { class: 'solr.WordDelimiterGraphFilterFactory' },
+      { class: 'solr.FlattenGraphFilterFactory' }
     ]
   }
   query_analyzer = {
-    "tokenizer": {
-      class: "solr.WhitespaceTokenizerFactory"
+    'tokenizer': {
+      class: 'solr.WhitespaceTokenizerFactory'
     },
-    "filters": [
-      { class: "solr.WordDelimiterGraphFilterFactory" }
+    'filters': [
+      { class: 'solr.WordDelimiterGraphFilterFactory' }
     ]
   }
 
-  it 'can add a field type' do
-    expect(SolrService).to receive(:add_field_type).once
-    SolrService.add_field_type("text_ocr", "solr.TextField", index_analyzer, query_analyzer)
+  it 'can trigger an error when field is already in schema' do
+    expect do
+      SolrService.add_field_type('text_ocr', 'solr.TextField', index_analyzer, query_analyzer)
+    end.to raise_error(Faraday::BadRequestError)
   end
   it 'can replace a field type' do
-    expect(SolrService).to receive(:replace_field_type).once
-    SolrService.replace_field_type("text_ocr", "solr.TextField", index_analyzer, query_analyzer)
+    expect(SolrService.replace_field_type('text_ocr', 'solr.TextField', index_analyzer, query_analyzer).success?).to eq true
   end
-  it 'can add a dynamic field type' do
-    expect(SolrService).to receive(:add_dynamic_field).once
-    SolrService.add_dynamic_field("*_wstsim", "text_ocr", true, true, true)
+  it 'can trigger an error when a dynamic field type is already in schema' do
+    expect do
+      SolrService.add_dynamic_field('*_wstsim', 'text_ocr', true, true, true)
+    end.to raise_error(Faraday::BadRequestError)
   end
   it 'can replace a dynamic field type' do
-    expect(SolrService).to receive(:replace_dynamic_field).once
-    SolrService.replace_dynamic_field("*_wstsim", "text_ocr", true, true, true)
+    expect(SolrService.replace_dynamic_field('*_wstsim', 'text_ocr', true, true, true).success?).to eq true
   end
 end
