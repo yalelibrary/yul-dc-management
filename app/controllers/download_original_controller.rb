dd4a59aa934c5bb79c292e7ab910394ede4a1463
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2023-01-16 13:22:54 -0600

    Creates API call to save original to S3 job (#1119)
    Co-authored-by: JP Engstrom <jpengstrom@macbook-pro.lan>

---
+++ app/controllers/download_original_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class DownloadOriginalController < ApplicationController
+  def stage
+    request = params
+    begin
+      child_object = ChildObject.find(request['oid'].to_i)
+    rescue ActiveRecord::RecordNotFound
+      render(json: { "title": "Invalid Child OID" }, status: 400) && (return false)
+    end
+    return unless check_child_visibility(child_object)
+    SaveOriginalToS3Job.perform_later(child_object.oid)
+    render(json: { "title": "Child object staged for download." }, status: 200)
+  end
+
+  def check_child_visibility(child_object)
+    render(json: { "title": "Child Object is restricted." }, status: 403) && (return false) unless
+    child_object.parent_object.visibility == "Public" || child_object.parent_object.visibility == "Yale Community Only"
+    true
+  end
+
+  private
+
+    def download_original_params
+      params.require(:download_original).permit(:oid)
+    end
+end
---
+++ spec/requests/download_original_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Download Original API', type: :request, prep_admin_sets: true do
+  let(:source) { FactoryBot.create(:metadata_source) }
+  let(:user) { FactoryBot.create(:sysadmin_user) }
+  let(:admin_set) { AdminSet.first }
+  let(:oid) { 2_034_600 }
+  let(:oid_2) { 137_105_661 }
+  let(:parent) { FactoryBot.create(:parent_object, oid: oid, admin_set: admin_set, visibility: 'Yale Community Only', authoritative_metadata_source_id: source.id) }
+  let(:parent_2) { FactoryBot.create(:parent_object, oid: oid_2, admin_set: admin_set, visibility: 'Private', authoritative_metadata_source_id: source.id) }
+  let(:child_object) { FactoryBot.create(:child_object, oid: '123456', parent_object: parent) }
+  let(:child_object_2) { FactoryBot.create(:child_object, oid: '2345678', parent_object: parent_2) }
+  let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+
+  around do |example|
+    original_image_bucket = ENV['S3_SOURCE_BUCKET_NAME']
+    original_download_bucket = ENV['S3_DOWNLOAD_BUCKET_NAME']
+    original_access_master_mount = ENV["ACCESS_MASTER_MOUNT"]
+    ENV['S3_SOURCE_BUCKET_NAME'] = 'not-a-real-bucket'
+    ENV['S3_DOWNLOAD_BUCKET_NAME'] = 'fake-download-bucket'
+    ENV["ACCESS_MASTER_MOUNT"] = File.join(fixture_path, "images/ptiff_images")
+    example.run
+    ENV['S3_SOURCE_BUCKET_NAME'] = original_image_bucket
+    ENV['S3_DOWNLOAD_BUCKET_NAME'] = original_download_bucket
+    ENV["ACCESS_MASTER_MOUNT"] = original_access_master_mount
+    perform_enqueued_jobs do
+      example.run
+    end
+  end
+
+  before do
+    stub_request(:head, 'https://fake-download-bucket.s3.amazonaws.com/download/tiff/56/12/34/56/123456.tif')
+        .to_return(status: 200, body: '', headers: {})
+    parent
+    parent_2
+    child_object
+    child_object_2
+    login_as user
+    user.add_role(:editor, admin_set)
+  end
+
+  describe 'POST /api/download/stage/child/:oid' do
+    it 'creates a new job to copy to s3' do
+      expect(SaveOriginalToS3Job).to receive(:perform_later).once
+      get "/api/download/stage/child/#{child_object.oid}", params: { oid: child_object.oid }, headers: headers
+      expect(response).to have_http_status(:ok) # 200
+    end
+
+    it 'errors if object is not YCO or Public' do
+      expect(SaveOriginalToS3Job).not_to receive(:perform_later)
+      get "/api/download/stage/child/#{child_object_2.oid}", params: { oid: child_object_2.oid }, headers: headers
+      expect(response).to have_http_status(:forbidden) # 403
+    end
+  end
+end
