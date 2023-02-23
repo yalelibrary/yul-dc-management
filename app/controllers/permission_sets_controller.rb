6acb7edaaffa657062a142b7c378602912382635
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-02-23 13:47:07 -0800

    rubocop

--- app/controllers/permission_sets_controller.rb
+++ app/controllers/permission_sets_controller.rb
@@ -5,7 +5,7 @@ class PermissionSetsController < ApplicationController
   before_action :set_permission_set, only: [:show, :edit, :update, :destroy, :permission_set_terms, :post_permission_set_terms, :new_term, :deactivate_permission_set_terms]
 
   skip_before_action :authenticate_user!, only: :terms_api
-  
+
   # GET /permission_sets
   # GET /permission_sets.json
   def index
