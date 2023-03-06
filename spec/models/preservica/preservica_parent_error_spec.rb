1d48433c00e4623f7f7c427433a39c9ae7a683c6
K8Sewell <kait@notch8.com>
2023-03-06 15:52:17 -0600

    Adds start of no source and no admin set specs

--- spec/models/preservica/preservica_parent_error_spec.rb
+++ spec/models/preservica/preservica_parent_error_spec.rb
@@ -5,8 +5,10 @@ require 'rails_helper'
 RSpec.describe Preservica::PreservicaObject, type: :model, prep_metadata_sources: true, prep_admin_sets: true do
   subject(:batch_process) { BatchProcess.new }
   let(:admin_set) { FactoryBot.create(:admin_set, key: 'brbl') }
+  let(:sml_admin_set) { FactoryBot.create(:admin_set, key: 'sml') }
   let(:user) { FactoryBot.create(:user, uid: "mk2525") }
   let(:preservica_parent) { Rack::Test::UploadedFile.new(Rails.root.join(fixture_path, "csv", "preservica", "preservica_parent.csv")) }
+  let(:preservica_parent_no_source) { Rack::Test::UploadedFile.new(Rails.root.join(fixture_path, "csv", "preservica", "preservica_parent_no_source.csv")) }
 
   around do |example|
     preservica_host = ENV['PRESERVICA_HOST']
@@ -25,6 +27,7 @@ RSpec.describe Preservica::PreservicaObject, type: :model, prep_metadata_sources
 
   before do
     user.add_role(:editor, admin_set)
+    user.add_role(:editor, sml_admin_set)
     login_as(:user)
     batch_process.user_id = user.id
     stub_pdfs
@@ -42,5 +45,20 @@ RSpec.describe Preservica::PreservicaObject, type: :model, prep_metadata_sources
       expect(batch_process.batch_ingest_events.count).to eq(1)
       expect(batch_process.batch_ingest_events[0].reason).to eq("Skipping row [2] with admin set [brbl] for parent: 200000000. Preservica credentials not set for brbl.")
     end.not_to change { ParentObject.count }
+  end
+
+  it 'can send an error when no metadata source is set' do
+    # rubocop:disable RSpec/AnyInstance
+    allow_any_instance_of(SetupMetadataJob).to receive(:perform).and_return(true)
+    # rubocop:enable RSpec/AnyInstance
+    expect do
+      batch_process.file = preservica_parent_no_source
+      batch_process.save
+      expect(batch_process.batch_ingest_events.count).to eq(1)
+      expect(batch_process.batch_ingest_events[0].reason).to eq("Skipping row [2] with no metadata source.")
+    end.not_to change { ParentObject.count }
+  end
+
+  it 'can send an error when no admin set is set' do
   end
 end
