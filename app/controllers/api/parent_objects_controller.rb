7d4c0f988dda4f3a2b250b22bbc60173dc7a04e6
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-03-20 16:08:52 -0700

    api specs and json data

--- app/controllers/api/parent_objects_controller.rb
+++ app/controllers/api/parent_objects_controller.rb
@@ -4,46 +4,39 @@ class Api::ParentObjectsController < ApplicationController
   skip_before_action :authenticate_user!
   
   def retrieve_metadata
-    # look for parent
     return unless parent_found
-    # check visibility
-    return unless parent_visibility_valid(parent_object)
-    # return metadata
+    return unless parent_visibility_valid(@parent_object)
     render json: {
       "dcs": {
-          "oid": "123",
-          "visibility": "Public",
-          "metadata_source": "ils",
-          "bib":  ".....",
-          "holding":  ".....",
-          "item":  ".....",
-          "barcode":  ".....",
-          "aspace_uri":  ".....",
-          "admin_set": "Beinecke",   
-          "child_object_count": 43,
-          "representative_child_oid": 123,
-          "rights_statement": "....",
-          "extent_of_digitization": "...",
-          "digitization_note": "...",
-          "call_number": "...",
-          "container_grouping": "...",
-          "redirect_to": "....",
-          # use blacklight base url env var
-          "iiif_manifest": "https://collections-*.library.yale.edu/manifests/123"  
+          "oid": "#{@parent_object.oid}",
+          "visibility": "#{@parent_object.visibility}",
+          "metadata_source": "#{@parent_object.authoritative_metadata_source_id}",
+          "bib":  "#{@parent_object.bib}",
+          "holding":  "#{@parent_object.holding}",
+          "item":  "#{@parent_object.item}",
+          "barcode":  "#{@parent_object.barcode}",
+          "aspace_uri":  "#{@parent_object.aspace_uri}",
+          "admin_set": "#{@parent_object.admin_set.label}",   
+          "child_object_count": "#{@parent_object.child_object_count}",
+          "representative_child_oid": "#{@parent_object.representative_child_oid}",
+          "rights_statement": "#{@parent_object.rights_statement}",
+          "extent_of_digitization": "#{@parent_object.extent_of_digitization}",
+          "digitization_note": "#{@parent_object.digitization_note}",
+          "call_number": "#{@parent_object.call_number}",
+          "container_grouping": "#{@parent_object.container_grouping}",
+          "redirect_to": "#{@parent_object.redirect_to}",
+          "iiif_manifest": "#{ENV['BLACKLIGHT_BASE_URL']}/manifests/#{@parent_object.oid}"  
       }
     }
   end
 
-
-
   def parent_found
     begin
-      parent_object = ParentObject.find(params['oid'].to_i)
+      @parent_object = ParentObject.find(params['oid'].to_i)
     rescue ActiveRecord::RecordNotFound
       render(json: { "title": "Invalid Parent OID" }, status: 404) && (return false)
     end
-    # maybe this should be @parent_object?
-    parent_object
+    @parent_object
   end
 
   def parent_visibility_valid(parent_object)
--- spec/requests/api/parent_objects_spec.rb
+++ spec/requests/api/parent_objects_spec.rb
@@ -10,12 +10,39 @@ RSpec.describe '/api/parent/oid', type: :request, prep_metadata_sources: true, p
       bib: "123"
     }
   end
+  let(:invalid_visibility) do
+    {
+      oid: "2000000",
+      authoritative_metadata_source_id: 1,
+      admin_set: AdminSet.find_by_key('brbl'),
+      bib: "123",
+      visibility: "Private"
+    }
+  end
 
+  # This one spec isnt passing for some reason...
   describe "GET with valid oid" do
     it "renders a successful response" do
       ParentObject.create! valid_attributes
       get "/api/parent/#{valid_attributes[:oid]}"
       expect(response).to be_successful
+    end
+  end
+
+  describe "GET with invalid oid" do
+    it "renders a 404 response" do
+      get "/api/parent/12345"
+      expect(response).to have_http_status(404)
+      expect(response.body).to eq("{\"title\":\"Invalid Parent OID\"}")
+    end
+  end
+
+  describe "GET with invalid visibility" do
+    it "renders a 403 response" do
+      ParentObject.create! invalid_visibility
+      get "/api/parent/#{invalid_visibility[:oid]}"
+      expect(response).to have_http_status(403)
+      expect(response.body).to eq("{\"title\":\"Parent Object is restricted.\"}")
     end
   end
 
