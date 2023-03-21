4e119cce7f2d69d067c8d52bfb4388ddd83895ba
K8Sewell <kait@notch8.com>
2023-03-21 16:20:03 -0500

    Finishes last spec

--- app/controllers/api/parent_objects_controller.rb
+++ app/controllers/api/parent_objects_controller.rb
@@ -2,33 +2,37 @@
 
 class Api::ParentObjectsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def retrieve_metadata
     return unless parent_found
     return unless parent_visibility_valid(@parent_object)
     render json: {
       "dcs": {
-          "oid": "#{@parent_object.oid}",
-          "visibility": "#{@parent_object.visibility}",
-          "metadata_source": metadata_source(@parent_object),
-          "bib":  "#{@parent_object.bib}",
-          "holding":  "#{@parent_object.holding}",
-          "item":  "#{@parent_object.item}",
-          "barcode":  "#{@parent_object.barcode}",
-          "aspace_uri":  "#{@parent_object.aspace_uri}",
-          "admin_set": "#{@parent_object.admin_set.label}",   
-          "child_object_count": "#{@parent_object.child_object_count}",
-          "representative_child_oid": "#{@parent_object.representative_child_oid}",
-          "rights_statement": "#{@parent_object.rights_statement}",
-          "extent_of_digitization": "#{@parent_object.extent_of_digitization}",
-          "digitization_note": "#{@parent_object.digitization_note}",
-          "call_number": "#{@parent_object.call_number}",
-          "container_grouping": "#{@parent_object.container_grouping}",
-          "redirect_to": "#{@parent_object.redirect_to}",
-          "iiif_manifest": "#{ENV['BLACKLIGHT_BASE_URL']}/manifests/#{@parent_object.oid}"  
+        "oid": @parent_object.oid.to_s,
+        "visibility": @parent_object.visibility.to_s,
+        "metadata_source": metadata_source(@parent_object),
+        "bib": @parent_object.bib.to_s,
+        "holding": @parent_object.holding.to_s,
+        "item": @parent_object.item.to_s,
+        "barcode": @parent_object.barcode.to_s,
+        "aspace_uri": @parent_object.aspace_uri.to_s,
+        "admin_set": @parent_object.admin_set.label.to_s,
+        "child_object_count": @parent_object.child_object_count.to_s,
+        "representative_child_oid": @parent_object.representative_child_oid.to_s,
+        "rights_statement": @parent_object.rights_statement.to_s,
+        "extent_of_digitization": @parent_object.extent_of_digitization.to_s,
+        "digitization_note": @parent_object.digitization_note.to_s,
+        "call_number": @parent_object.call_number.to_s,
+        "container_grouping": @parent_object.container_grouping.to_s,
+        "redirect_to": @parent_object.redirect_to.to_s,
+        "iiif_manifest": "#{ENV['BLACKLIGHT_BASE_URL']}/manifests/#{@parent_object.oid}"
       }
-    }
+    }, status: 200
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
   def parent_found
     begin
@@ -46,14 +50,10 @@ class Api::ParentObjectsController < ApplicationController
   end
 
   def metadata_source(parent_object)
-    if parent_object.authoritative_metadata_source_id == 1
-      return "ils"
-    elsif parent_object.authoritative_metadata_source_id == 2
-      return "Voyager"
-    elsif parent_object.authoritative_metadata_source_id == 3
-      return "ArchiveSpace"
-    end
-    return "Metadata Source not found"
+    return "ils" if parent_object.authoritative_metadata_source_id == 1
+    return "Voyager" if parent_object.authoritative_metadata_source_id == 2
+    return "ArchiveSpace" if parent_object.authoritative_metadata_source_id == 3
+    "Metadata Source not found"
   end
 
   private
@@ -61,5 +61,4 @@ class Api::ParentObjectsController < ApplicationController
     def parent_object_params
       params.require(:parent_object).permit(:oid)
     end
-
 end
