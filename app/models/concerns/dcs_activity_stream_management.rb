5e0011a5e2ab7669628d281b0b70fab5d2e2c7a8
Maggie Zhao <LixiaZhao@yale.edu>
2023-03-28 15:34:48 -0400

    add first to get oid

--- app/models/concerns/dcs_activity_stream_management.rb
+++ app/models/concerns/dcs_activity_stream_management.rb
@@ -36,13 +36,14 @@ module DcsActivityStreamManagement
 
   def mc_activity_stream_check
     md5_metadata_hash = Digest::MD5.hexdigest(dcs_metadata.to_json)
-    dcs_activity_stream_update = DcsActivityStreamUpdate.find(oid: oid)
+    dcs_activity_stream_update = DcsActivityStreamUpdate.find(oid: oid).first
     action_type = dcs_activity_stream_update.nil? ? "Create" : "Update"
     return if !dcs_activity_stream_update.nil? && dcs_activity_stream_update.md5_metadata_hash == md5_metadata_hash
 
     dcs_activity_stream_update = dcs_activity_stream_update.nil? ? dcs_activity_stream_update : DcsActivityStreamUpdate.new
     dcs_activity_stream_update.md5_metadata_hash = md5_metadata_hash
     dcs_activity_stream_update.oid = oid unless dcs_activity_stream_update.nil?
+    byebug
     dcs_activity_stream_update.save! if send_dcs_activity_stream_update(action_type)
   end
 
