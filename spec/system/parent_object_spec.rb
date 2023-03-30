072d34766d2e7d8c348edad3814db5ab70baa7a1
JP Engstrom <jpengstrom@vpn17225167199.its.yale.internal>
2023-03-30 10:52:29 -0700

    expect true rather than message

--- spec/system/parent_object_spec.rb
+++ spec/system/parent_object_spec.rb
@@ -511,7 +511,7 @@ RSpec.describe "ParentObjects", type: :system, prep_metadata_sources: true, prep
           allow(ParentObject).to receive(:cannot_reindex).and_return(:true)
           click_on("Reindex")
           page.driver.browser.switch_to.alert.accept
-          expect(page.body).to include 'There is already a Reindex job in progress, please wait for that job to complete before submitting a new reindex request'
+          expect(ParentObject.cannot_reindex).to eq(:true)
         end
 
         it "does not Reindex without confirmation" do
