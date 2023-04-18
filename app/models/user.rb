c3f1c17a547b59f92f2e5740c987e990f13de367
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-04-18 14:37:27 -0700

    adds unique to user uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates_uniqueness_of :uid, message: "already exists."
 
   has_many :batch_processes
   has_many :users_roles
