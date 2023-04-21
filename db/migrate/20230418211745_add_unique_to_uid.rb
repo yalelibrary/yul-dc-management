edd679f69e81e942f9d855e65242f5f97317024d
Martin Lovell <martin.lovell@yale.edu>
2023-04-21 14:43:09 -0400

    Make uids unique before adding index

--- db/migrate/20230418211745_add_unique_to_uid.rb
+++ db/migrate/20230418211745_add_unique_to_uid.rb
@@ -1,5 +1,6 @@
 class AddUniqueToUid < ActiveRecord::Migration[6.1]
   def change
+    User.where.not(id:User.select("MAX(id) as id").group(:uid).map(&:id)).each { |u| u.uid = "#{u.uid}__"; u.save }
     add_index :users, :uid, unique: true
   end
 end
