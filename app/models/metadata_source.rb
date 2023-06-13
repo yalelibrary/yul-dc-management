f8d595f533f0d16dd74bc2535f6ffd0c6ff8dc2b
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-06-13 14:00:40 -0700

    rubocop

--- app/models/metadata_source.rb
+++ app/models/metadata_source.rb
@@ -71,8 +71,8 @@ class MetadataSource < ApplicationRecord
     "#{file_prefix}#{parent_object.oid}.json"
   end
 
-  def self.all_metadata_cloud_names 
-    MetadataSource.distinct.pluck(:metadata_cloud_name) 
+  def self.all_metadata_cloud_names
+    MetadataSource.distinct.pluck(:metadata_cloud_name)
   end
 
   def url_type
