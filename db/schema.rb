63e7a77835614c1b2b788f37aa1ebb5f76cbc6f6
JP Engstrom <jpengstrom@vpn172022148121.its.yale.internal>
2023-06-23 13:25:34 -0700

    adds default value of None to extent_of_full_text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_31_180611) do
+ActiveRecord::Schema.define(version: 2023_06_23_201400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,7 +196,7 @@ ActiveRecord::Schema.define(version: 2023_05_31_180611) do
     t.datetime "last_preservica_update"
     t.string "digitization_funding_source"
     t.bigint "permission_set_id"
-    t.text "extent_of_full_text"
+    t.text "extent_of_full_text", default: "None"
     t.index ["admin_set_id"], name: "index_parent_objects_on_admin_set_id"
     t.index ["aspace_uri"], name: "index_parent_objects_on_aspace_uri"
     t.index ["authoritative_metadata_source_id"], name: "index_parent_objects_on_authoritative_metadata_source_id"
