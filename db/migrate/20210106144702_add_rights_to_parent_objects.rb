5c4c5f857cb3d7c27b0265cca0be093d047de6cd
Martin Lovell <martin.lovell@yale.edu>
2023-07-06 08:39:27 -0400

    CI Experiment

--- db/migrate/20210106144702_add_rights_to_parent_objects.rb
+++ db/migrate/20210106144702_add_rights_to_parent_objects.rb
@@ -1,5 +1,5 @@
 class AddRightsToParentObjects < ActiveRecord::Migration[6.0]
   def change
-    AddRightsToParentObjectsJob.perform_later
+    #AddRightsToParentObjectsJob.perform_later
   end
 end
