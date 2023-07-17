54bf51b96d361936b37c73e0c8472de68e249099
K8Sewell <kait@notch8.com>
2023-07-17 13:45:17 -0500

    Future upgrade notes

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,6 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 const config = environment.toWebpackConfig()
+
+// remove config.devtool when upgrading to webpack v5
 config.devtool = 'none'
 
 module.exports = config
