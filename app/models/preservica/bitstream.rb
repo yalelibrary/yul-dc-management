5df199bc9da8d12afeb076ed3bd5bfcd8489620f
K8Sewell <kait@notch8.com>
2023-08-28 10:49:34 -0500

    Add more information to error

--- app/models/preservica/bitstream.rb
+++ app/models/preservica/bitstream.rb
@@ -40,9 +40,9 @@ class Preservica::Bitstream
     end
     data_sha512 = sha512.hexdigest
     file_size = File.size?(file_name)
-    raise StandardError, "Checksum mismatch (#{data_sha512} != #{sha512_checksum})" unless data_sha512 == sha512_checksum
-    raise StandardError, "Data size did not match (#{data_length} != #{size})" unless data_length == size
-    raise StandardError, "File sizes do not match (#{file_size} != #{size})" unless file_size == size
+    raise StandardError, "Checksum mismatch (#{data_sha512} != #{sha512_checksum}) for child: #{oid}" unless data_sha512 == sha512_checksum
+    raise StandardError, "Data size did not match (#{data_length} != #{size}) for child: #{oid}" unless data_length == size
+    raise StandardError, "File sizes do not match (#{file_size} != #{size}) for child: #{oid}" unless file_size == size
     # could also check: Digest::SHA512.file(file_name).hexdigest == sha512_checksum, but probably not necessary
   end
 
