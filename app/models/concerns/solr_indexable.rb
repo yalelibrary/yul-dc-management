bef4be13f7a7b298bc02a923329d3e36b50f9a46
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-08-29 14:47:39 -0700

    removes No option for extent of digitization

--- app/models/concerns/solr_indexable.rb
+++ app/models/concerns/solr_indexable.rb
@@ -145,7 +145,7 @@ module SolrIndexable
         digitization_funding_source_tesi: generate_digitization_funding_source(json_to_index["digitization_funding_source"]),
         edition_ssim: json_to_index["edition"],
         extent_ssim: json_to_index["extent"],
-        extentOfDigitization_ssim: extent_of_digitization || "No",
+        extentOfDigitization_ssim: extent_of_digitization,
         findingAid_ssim: json_to_index["findingAid"],
         folder_ssim: json_to_index["folder"],
         format: json_to_index["format"],
