9c94db896bbacb79ae4e896edc6c38ab62575c86
K8Sewell <kait@notch8.com>
2023-09-11 14:14:19 -0500

    Resolve duplicate id issue

--- spec/support/admin_sets_helper.rb
+++ spec/support/admin_sets_helper.rb
@@ -4,8 +4,8 @@ module AdminSetsHelper
   # Setup rspec
   RSpec.configure do |config|
     config.before(prep_admin_sets: true) do
-      FactoryBot.create(:admin_set, key: "brbl", label: "Beinecke Library", homepage: "http://test.com")
-      FactoryBot.create(:admin_set, key: "sml", label: "Sterling Memorial Library", homepage: "http://test2.com")
+      FactoryBot.create(:admin_set, key: "brbl", label: "Beinecke Library", homepage: "http://test.com") if AdminSet.all.count == 0
+      FactoryBot.create(:admin_set, key: "sml", label: "Sterling Memorial Library", homepage: "http://test2.com") if AdminSet.all.count == 1
     end
   end
 end
--- spec/support/metadata_sources_helper.rb
+++ spec/support/metadata_sources_helper.rb
@@ -4,9 +4,9 @@ module MetdataSourcesHelper
   # Setup rspec
   RSpec.configure do |config|
     config.before(prep_metadata_sources: true) do
-      FactoryBot.create(:metadata_source)
-      FactoryBot.create(:metadata_source_voyager)
-      FactoryBot.create(:metadata_source_aspace)
+      FactoryBot.create(:metadata_source) if MetadataSource.all.count == 0
+      FactoryBot.create(:metadata_source_voyager) if MetadataSource.all.count == 1
+      FactoryBot.create(:metadata_source_aspace) if MetadataSource.all.count == 2
     end
   end
 end
