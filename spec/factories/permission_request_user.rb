078178814686a8c19ca25e05a3737e8363d32ca4
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-09-18 16:16:35 -0700

    specs and adds netid to requests api

--- spec/factories/permission_request_user.rb
+++ spec/factories/permission_request_user.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :permission_request_user, class: OpenWithPermission::PermissionRequestUser do
     sub { "id123" }
     name { "User Name" }
-    netid { "User Net ID" }
+    netid { "net_id" }
     email { "user@example.com" }
     email_verified { true }
     oidc_updated_at { "2022-11-08 15:33:30" }
