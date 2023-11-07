ecb23f8a0ddf9c361e2e42d0fa7653035bc61867
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-11-07 09:52:12 -0800

    index owp and skip user auth for api call

--- app/controllers/api/permission_sets_controller.rb
+++ app/controllers/api/permission_sets_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::PermissionSetsController < ApplicationController
+  skip_before_action :authenticate_user!
+  
   def terms_api
     # check for valid parent object
     begin
