3c1c513c53f97adcb6688762db62b931cd582459
JP Engstrom <jpengstrom@macbook-pro.lan>
2023-12-04 14:33:06 -0800

    update permission request specs for management post

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.hosts << "yul-dc-management-1"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
