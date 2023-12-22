a5275f6a4e97ad2fbd2fe03cd281fe71423ec6e1
K8Sewell <kait@notch8.com>
2023-12-22 08:16:00 -0600

    Consolidate test and skip cleaner

--- spec/system/parent_object_list_spec.rb
+++ spec/system/parent_object_list_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "ParentObjects", type: :system, prep_metadata_sources: true, prep_admin_sets: true do
+RSpec.describe "ParentObjects", type: :system, prep_metadata_sources: true, prep_admin_sets: true, skip_db_cleaner: true do
   context "parent objects datatable page", js: true do
     let(:user) { FactoryBot.create(:user) }
     let(:admin_set) { FactoryBot.create(:admin_set, key: "adminset") }
@@ -18,41 +18,27 @@ RSpec.describe "ParentObjects", type: :system, prep_metadata_sources: true, prep
     end
 
     it "displays parent objects without filter" do
-      visit parent_objects_path # , params: {key: admin_set.key, current_ability: Ability.new(user) }
-      page.driver.browser.switch_to.alert.dismiss if Selenium::WebDriver::Error::UnexpectedAlertOpenError
+      visit parent_objects_path 
       expect(page).to have_content("2002826")
       expect(page).to have_content("2004548")
-    end
-
-    it "filters when filter box is filled" do
-      visit parent_objects_path
-      page.driver.browser.switch_to.alert.dismiss if Selenium::WebDriver::Error::UnexpectedAlertOpenError
+      # filters when filter box is filled
       find("input[placeholder='OID']").set("2002")
       expect(page).to have_content("2002826")
       expect(page).not_to have_content("2004548")
-    end
-
-    it "retains filters and fills filter boxes on refresh" do
-      visit parent_objects_path
-      page.driver.browser.switch_to.alert.dismiss if Selenium::WebDriver::Error::UnexpectedAlertOpenError
+      # retains filters and fills filter boxes on refresh
       find("input[placeholder='OID']").set("2002")
       expect(page).to have_content("2002826")
       expect(page).not_to have_content("2004548")
       visit current_path
-      page.driver.browser.switch_to.alert.dismiss if Selenium::WebDriver::Error::UnexpectedAlertOpenError
       expect(page).to have_content("2002826")
       expect(page).not_to have_content("2004548")
       expect(find("input[placeholder='OID']").value).to eq("2002")
-    end
-
-    it "clears filter when Clear Filters is clicked" do
+      # clears filter when Clear Filters is clicked" do
       visit parent_objects_path
-      page.driver.browser.switch_to.alert.dismiss if Selenium::WebDriver::Error::UnexpectedAlertOpenError
       find("input[placeholder='OID']").set("2002")
       expect(page).to have_content("2002826")
       expect(page).not_to have_content("2004548")
       click_on("Clear Filters")
-      page.driver.browser.switch_to.alert.dismiss if Selenium::WebDriver::Error::UnexpectedAlertOpenError
       expect(page).to have_content("2002826")
       expect(page).to have_content("2004548")
     end
