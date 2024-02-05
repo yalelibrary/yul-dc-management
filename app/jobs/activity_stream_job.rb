e1678856e20860c0de8ede84c65b703ad2b8a7b0
K8Sewell <kait@notch8.com>
2024-02-05 14:02:13 -0600

    Undo rubocop -A correction and add ignore

--- app/jobs/activity_stream_job.rb
+++ app/jobs/activity_stream_job.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class ActivityStreamJob < ApplicationJob
+  # rubocop:disable Rails/SaveBang
   def perform
-    ActivityStreamReader.update! unless ActivityStreamLog.where(status: "Running").exists?
+    ActivityStreamReader.update unless ActivityStreamLog.where(status: "Running").exists?
   end
+  # rubocop:enable Rails/SaveBang
 end
--- app/jobs/activity_stream_manual_job.rb
+++ app/jobs/activity_stream_manual_job.rb
@@ -2,7 +2,9 @@
 
 # A separate job is needed for manual run, or it will enqueue itself again after it has finished
 class ActivityStreamManualJob < ApplicationJob
+  # rubocop:disable Rails/SaveBang
   def perform
-    ActivityStreamReader.update! unless ActivityStreamLog.where(status: "Running").exists?
+    ActivityStreamReader.update unless ActivityStreamLog.where(status: "Running").exists?
   end
+  # rubocop:enable Rails/SaveBang
 end
