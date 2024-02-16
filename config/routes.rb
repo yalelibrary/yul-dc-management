582e2337f45db4e2f98d26fc5c560dc4a725ad98
JP Engstrom <jp@scientist.com>
2024-02-16 10:50:05 -0800

    Add Export All Parent Objects by Source Job and Button (#1323)
    Co-authored-by: JP Engstrom <jpengstrom@vpn1722517378.its.yale.internal>
    Co-authored-by: JP Engstrom <jpengstrom@macbook-pro.lan>

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   resources :batch_processes do
     collection do
       post :export_parent_objects
+      post :export_parent_sources
       post :import
       post :trigger_mets_scan
       get :download_template
