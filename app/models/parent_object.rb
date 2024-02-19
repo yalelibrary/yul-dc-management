ff8dc152b5ac2165069990459aca968b774e4d87
K8Sewell <kait@notch8.com>
2024-02-19 14:43:00 -0600

    Make OwP testing easier and update attribute

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -445,7 +445,7 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   def assign_values
     self.call_number = authoritative_json["callNumber"].is_a?(Array) ? authoritative_json["callNumber"].first : authoritative_json["callNumber"]
     self.container_grouping = authoritative_json["containerGrouping"].is_a?(Array) ? authoritative_json["containerGrouping"].first : authoritative_json["containerGrouping"]
-    self.visibility = visibility_was && self.permission_set = permission_set_was if visibility_was == 'Open with Permission'
+    self.visibility = visibility_was && self.permission_set_id = permission_set_id_was if visibility_was == 'Open with Permission'
   end
 
   def add_media_type(url)
