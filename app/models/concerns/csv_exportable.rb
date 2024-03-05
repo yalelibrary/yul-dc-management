2326355783ad3a34f3ca6f94ec7e8bfa2a3a3913
JP Engstrom <jp@scientist.com>
2024-03-05 10:53:30 -0800

    Adds admin set list logic to export parent batch process (#1336)
    Co-authored-by: JP Engstrom <jpengstrom@macbook-pro.lan>

--- app/models/concerns/csv_exportable.rb
+++ app/models/concerns/csv_exportable.rb
@@ -17,13 +17,21 @@ module CsvExportable
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   def parent_output_csv(*admin_set_id)
     return nil unless batch_action == 'export all parent objects by admin set'
+    self.admin_set = ''
+    sets = admin_set
     output_csv = CSV.generate do |csv|
       csv << parent_headers
       with_each_parent_object(*admin_set_id) do |po|
         case po
         when ParentObject
+          sets << ', ' + po.admin_set.key
+          split_sets = sets.split(',').uniq.reject(&:blank?)
+          self.admin_set = split_sets.join(', ')
+          save!
           csv << [po.oid, po.admin_set.key, po.source_name,
                   po.child_object_count, po.authoritative_json&.[]('title')&.first, po.call_number, po.container_grouping, po.bib, po.holding, po.item,
                   po.barcode, po.aspace_uri, po.digital_object_source, po.preservica_uri,
@@ -65,7 +73,6 @@ module CsvExportable
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Lint/UselessAssignment
   def with_each_parent_object(*admin_set_id)
@@ -148,11 +155,17 @@ module CsvExportable
 
   def export_all_parents_source_csv(sources)
     return nil unless batch_action == 'export all parents by source'
+    self.admin_set = ''
+    sets = admin_set
     output_csv = CSV.generate do |csv|
       csv << parent_headers
       find_parent_by_source(sources) do |po|
         case po
         when ParentObject
+          sets << ', ' + po.admin_set.key
+          split_sets = sets.split(',').uniq.reject(&:blank?)
+          self.admin_set = split_sets.join(', ')
+          save!
           csv << [po.oid, po.admin_set.key, po.source_name,
                   po.child_object_count, po.authoritative_json&.[]('title')&.first, po.call_number, po.container_grouping, po.bib, po.holding, po.item,
                   po.barcode, po.aspace_uri, po.digital_object_source, po.preservica_uri,
