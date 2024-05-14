e49024a04be97463e207fc8a77b207c34613942f
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-05-14 13:31:46 -0700

    changes email recipient

--- app/mailers/access_change_request_mailer.rb
+++ app/mailers/access_change_request_mailer.rb
@@ -5,6 +5,6 @@ class AccessChangeRequestMailer < ApplicationMailer
 
   def access_change_request_email
     @access_change_request = params[:access_change_request]
-    mail(to: 'digitalspecialcollections@yale.edu', subject: 'DCS Object Visibility Update Request')
+    mail(to: 'summer.shetenhelm@yale.edu ', subject: 'DCS Object Visibility Update Request')
   end
 end
