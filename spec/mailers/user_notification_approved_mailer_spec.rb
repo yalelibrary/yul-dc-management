124d398316ee5500649a7205f746ddc8b2962b84
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-05-20 09:40:07 -0700

    mailer specs

---
+++ spec/mailers/user_notification_approved_mailer_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserNotificationApprovedMailer, type: :mailer, prep_admin_sets: true, prep_metadata_sources: true do
+  describe 'user_notification_approved_email' do
+    let(:admin_set) { AdminSet.first }
+    let(:metadata_source) { MetadataSource.first }
+    let(:parent_object) { FactoryBot.create(:parent_object, authoritative_metadata_source_id: metadata_source.id, admin_set_id: admin_set.id) }
+    let(:permission_set) { FactoryBot.create(:permission_set) }
+    let(:request_user) { FactoryBot.create(:permission_request_user, sub: "sub 1", name: "name 1", netid: "netid", email: "email@example.com") }
+    let(:permission_request) { FactoryBot.create(:permission_request, permission_set_id: permission_set.id, parent_object_id: parent_object.oid, user_note: 'message', permission_request_user: request_user, access_until: "2030-06-10 00:00:00") }
+    let(:user_approved_notification) do
+      {
+        request_user_name: permission_request.permission_request_user.name,
+        permission_set_label: permission_set.label,
+        request_user_email: permission_request.permission_request_user.email,
+        expiration_date: permission_request.access_until,
+        parent_object_title: "Title",
+        parent_object_oid: parent_object.oid
+      }
+    end
+    let(:mail) { described_class.with(user_notification: user_approved_notification).user_notification_approved_email.deliver_now }
+
+    it 'renders the expected fields' do
+      expect(mail.subject).to eq 'Your request to view Title has been approved'
+      expect(mail.to).to eq ["email@example.com"]
+      expect(mail.from).to eq ['do_not_reply@library.yale.edu']
+      expect(mail.body.encoded).to include(permission_request.permission_request_user.name)
+      expect(mail.body.encoded).to include("2030-06-10 00:00:00")
+    end
+  end
+end
