5c0806ae94c69bd245f5d2ad4c50277bb305c37a
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-05-20 10:28:54 -0700

    removes default mailer from and adds kissinger specs

--- spec/mailers/user_notification_approved_mailer_spec.rb
+++ spec/mailers/user_notification_approved_mailer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UserNotificationApprovedMailer, type: :mailer, prep_admin_sets: t
     let(:admin_set) { AdminSet.first }
     let(:metadata_source) { MetadataSource.first }
     let(:parent_object) { FactoryBot.create(:parent_object, authoritative_metadata_source_id: metadata_source.id, admin_set_id: admin_set.id) }
-    let(:permission_set) { FactoryBot.create(:permission_set) }
+    let(:permission_set) { FactoryBot.create(:permission_set, label: "PS1") }
     let(:request_user) { FactoryBot.create(:permission_request_user, sub: "sub 1", name: "name 1", netid: "netid", email: "email@example.com") }
     let(:permission_request) { FactoryBot.create(:permission_request, permission_set_id: permission_set.id, parent_object_id: parent_object.oid, user_note: 'message', permission_request_user: request_user, access_until: "2030-06-10 00:00:00") }
     let(:user_approved_notification) do
@@ -20,7 +20,18 @@ RSpec.describe UserNotificationApprovedMailer, type: :mailer, prep_admin_sets: t
         parent_object_oid: parent_object.oid
       }
     end
+    let(:user_approved_notification_kissinger) do
+      {
+        request_user_name: permission_request.permission_request_user.name,
+        permission_set_label: "kiss",
+        request_user_email: permission_request.permission_request_user.email,
+        expiration_date: permission_request.access_until,
+        parent_object_title: "Title",
+        parent_object_oid: parent_object.oid
+      }
+    end
     let(:mail) { described_class.with(user_notification: user_approved_notification).user_notification_approved_email.deliver_now }
+    let(:kissinger_mail) { described_class.with(user_notification: user_approved_notification_kissinger).user_notification_approved_email.deliver_now }
 
     it 'renders the expected fields' do
       expect(mail.subject).to eq 'Your request to view Title has been approved'
@@ -28,6 +39,14 @@ RSpec.describe UserNotificationApprovedMailer, type: :mailer, prep_admin_sets: t
       expect(mail.from).to eq ['do_not_reply@library.yale.edu']
       expect(mail.body.encoded).to include(permission_request.permission_request_user.name)
       expect(mail.body.encoded).to include("2030-06-10 00:00:00")
+    end
+
+    it 'sends email from correct address for a kissinger permission set' do
+      expect(kissinger_mail.subject).to eq 'Your request to view Title has been approved'
+      expect(kissinger_mail.to).to eq ["email@example.com"]
+      expect(kissinger_mail.from).to eq ['kissingerpapers@gmail.com']
+      expect(kissinger_mail.body.encoded).to include(permission_request.permission_request_user.name)
+      expect(kissinger_mail.body.encoded).to include("2030-06-10 00:00:00")
     end
   end
 end
--- spec/mailers/user_notification_denied_mailer_spec.rb
+++ spec/mailers/user_notification_denied_mailer_spec.rb
@@ -19,13 +19,30 @@ RSpec.describe UserNotificationDeniedMailer, type: :mailer, prep_admin_sets: tru
         parent_object_oid: parent_object.oid
       }
     end
+    let(:user_denied_notification_kissinger) do
+      {
+        request_user_name: permission_request.permission_request_user.name,
+        permission_set_label: "kiss",
+        request_user_email: permission_request.permission_request_user.email,
+        parent_object_title: "Title",
+        parent_object_oid: parent_object.oid
+      }
+    end
     let(:mail) { described_class.with(user_notification: user_denied_notification).user_notification_denied_email.deliver_now }
+    let(:kissinger_mail) { described_class.with(user_notification: user_denied_notification_kissinger).user_notification_denied_email.deliver_now }
 
     it 'renders the expected fields' do
       expect(mail.subject).to eq 'Your request to view Title has been denied'
       expect(mail.to).to eq ["email@example.com"]
       expect(mail.from).to eq ['do_not_reply@library.yale.edu']
       expect(mail.body.encoded).to include(permission_request.permission_request_user.name)
+    end
+
+    it 'sends email from correct address for a kissinger permission set' do
+      expect(kissinger_mail.subject).to eq 'Your request to view Title has been denied'
+      expect(kissinger_mail.to).to eq ["email@example.com"]
+      expect(kissinger_mail.from).to eq ['kissingerpapers@gmail.com']
+      expect(kissinger_mail.body.encoded).to include(permission_request.permission_request_user.name)
     end
   end
 end
