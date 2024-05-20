1e4d813805b6616ce8aa62026f514b230b27af5d
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-05-20 10:37:42 -0700

    refactor kissinger conditionals

--- app/mailers/user_notification_approved_mailer.rb
+++ app/mailers/user_notification_approved_mailer.rb
@@ -5,10 +5,9 @@ class UserNotificationApprovedMailer < ApplicationMailer
     @user_notification = params[:user_notification]
     if @user_notification[:permission_set_label] == "kiss"
       mail(from: "kissingerpapers@gmail.com")
-      mail(to: @user_notification[:request_user_email], subject: "Your request to view #{@user_notification[:parent_object_title]} has been approved")
     else
       mail(from: "do_not_reply@library.yale.edu")
-      mail(to: @user_notification[:request_user_email], subject: "Your request to view #{@user_notification[:parent_object_title]} has been approved")
     end
+    mail(to: @user_notification[:request_user_email], subject: "Your request to view #{@user_notification[:parent_object_title]} has been approved")
   end
 end
--- app/mailers/user_notification_denied_mailer.rb
+++ app/mailers/user_notification_denied_mailer.rb
@@ -5,10 +5,9 @@ class UserNotificationDeniedMailer < ApplicationMailer
     @user_notification = params[:user_notification]
     if @user_notification[:permission_set_label] == "kiss"
       mail(from: "kissingerpapers@gmail.com")
-      mail(to: @user_notification[:request_user_email], subject: "Your request to view #{@user_notification[:parent_object_title]} has been denied")
     else
       mail(from: "do_not_reply@library.yale.edu")
-      mail(to: @user_notification[:request_user_email], subject: "Your request to view #{@user_notification[:parent_object_title]} has been denied")
     end
+    mail(to: @user_notification[:request_user_email], subject: "Your request to view #{@user_notification[:parent_object_title]} has been denied")
   end
 end
