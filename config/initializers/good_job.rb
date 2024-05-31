377256f48513ece0463e647a4cb288a10487500f
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2024-05-31 11:58:28 -0500

    Set worker queues (#1390)
    * Set worker queues
    
    * Fix typo

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.good_job.retry_on_unhandled_error = false
   config.good_job.on_thread_error = ->(exception) { Raven.capture_exception(exception) }
   config.good_job.execution_mode = :external
-  # config.good_job.queues = '*'
+  config.good_job.queues = ENV['WORKER_QUEUES']
   config.good_job.shutdown_timeout = 60 # seconds
   config.good_job.poll_interval = 5
   config.good_job.enable_cron = true
