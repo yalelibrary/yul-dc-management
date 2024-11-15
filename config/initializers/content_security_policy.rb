a63cc6917c1a8465e7f653750f240b23d51c0d5c
K8Sewell <kait@notch8.com>
2024-11-15 12:53:35 -0600

    rubocop

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -15,12 +15,16 @@ if ENV["RAILS_ENV"] == 'production' || ENV["RAILS_ENV"] == 'staging'
       policy.style_src   :self
       policy.connect_src :self
       # Specify URI for violation reports
-      policy.report_uri -> { "https://api.honeybadger.io/v1/browser/csp?api_key=#{ENV['HONEYBADGER_API_KEY_MANAGEMENT']}&report_only=true&env=#{ENV['CLUSTER_NAME']}" } unless ENV['CLUSTER_NAME'] == 'local'
+      unless ENV['CLUSTER_NAME'] == 'local'
+        policy.report_uri lambda {
+                            "https://api.honeybadger.io/v1/browser/csp?api_key=#{ENV['HONEYBADGER_API_KEY_MANAGEMENT']}&report_only=true&env=#{ENV['CLUSTER_NAME']}"
+                          }
+      end
     end
 
     config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
 
-    config.content_security_policy_nonce_directives = %w(script-src style-src)
+    config.content_security_policy_nonce_directives = %w[script-src style-src]
 
     # Report violations without enforcing the policy.
     # config.content_security_policy_report_only = true
