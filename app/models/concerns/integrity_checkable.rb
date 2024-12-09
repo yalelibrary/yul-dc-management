38f9cfbe05b703ce0662c766e39cdebb06830ec6
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2024-12-09 14:25:05 -0600

    Update integrity_checkable.rb

--- app/models/concerns/integrity_checkable.rb
+++ app/models/concerns/integrity_checkable.rb
@@ -5,13 +5,14 @@ module IntegrityCheckable
 
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Layout/LineLength
   def integrity_check
     self.admin_set = ''
     sets = admin_set
 
-    random_parents = ParentObject.where.not(child_object_count: 0).and(ParentObject.where.not(child_object_count: nil)).limit(2000).order("RANDOM()")
+    random_parents = ParentObject.where.not(digital_object_source: 'Preservica').and(ParentObject.where.not(child_object_count: 0).and(ParentObject.where.not(child_object_count: nil))).limit(2000).order("RANDOM()")
 
     child_object_sample = []
     random_parents.each do |po|
@@ -22,21 +23,21 @@ module IntegrityCheckable
       child_object_sample.each do |co|
         attach_item(co.parent_object)
         attach_item(co)
-        
+
         add_admin_set_to_bp(sets, co)
-        # look for file size of 0
-        # check for checksum mismatch
-        # checksum method: access_master_checksum_matches?
+
         if co.access_master_exists?
-          if co.file_size == 0
-            co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'failed')
-            co.processing_event("The child oid: #{co.oid} has a file size of 0. Please verify image for child oid: #{co.oid} at #{co.access_master_path} on #{ENV['ACCESS_MASTER_MOUNT']}.", 'failed')    
-          elsif co.file_size != 0 && !co.access_master_checksum_matches?
-            co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'failed')
-            co.processing_event("The child oid: #{co.oid} has a checksum mismatch. The checksum of the image file saved to this child oid does not match the checksum of the image file in the database at #{co.access_master_path} on #{ENV['ACCESS_MASTER_MOUNT']}. This may mean that the image has been corrupted. Please verify integrity of image for child oid: #{co.oid} by manually comparing the checksum values and update record as necessary.", 'failed')   
+          if co&.file_size&.positive?
+            if co&.access_master_checksum_matches?
+              co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'review-complete')
+              co.processing_event("Child Object: #{co.oid} - file exists and checksum matches.", 'review-complete')
+            else
+              co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'failed')
+              co.processing_event("The Child Object: #{co.oid} - has a checksum mismatch. The checksum of the image file saved to this child oid does not match the checksum of the image file in the database. This may mean that the image has been corrupted. Please verify integrity of image for Child Object: #{co.oid} - by manually comparing the checksum values and update record as necessary.", 'failed')
+            end
           else
-            co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'review-complete')
-            co.processing_event("Child Object: #{co.oid} - file exists.", 'review-complete')
+            co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'failed')
+            co.processing_event("Child Object: #{co.oid} - has a file size of 0. Please verify image for Child Object: #{co.oid}.", 'failed')
           end
         else
           co.parent_object.processing_event("Integrity check complete for Child Object: #{co.oid}", 'failed')
@@ -52,6 +53,7 @@ module IntegrityCheckable
   end
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/PerceivedComplexity
   # rubocop:enable Layout/LineLength
 end
