4de99dc9003509b527be998b254d615c4a04402a
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-12-16 11:12:46 -0800

    update specs and button patch

--- spec/system/batch_process_child_detail_spec.rb
+++ spec/system/batch_process_child_detail_spec.rb
@@ -55,6 +55,7 @@ RSpec.describe 'Batch Process Child detail page', type: :system, prep_metadata_s
   end
 
   context 'with expected failure with an integrity check job' do
+    let(:user) { FactoryBot.create(:user, uid: 'johnsmith2531') }
     let(:parent_object) { FactoryBot.create(:parent_object, oid: '444', child_object_count: 1, authoritative_metadata_source: MetadataSource.first, admin_set: AdminSet.first) }
     let(:child_object) { FactoryBot.create(:child_object, oid: '567890', parent_object: parent_object, file_size: 1234, checksum: 'f3755c5d9e086b4522a0d3916e9a0bfcbd47564ef') }
 
@@ -68,15 +69,22 @@ RSpec.describe 'Batch Process Child detail page', type: :system, prep_metadata_s
     end
 
     before do
+      login_as user
       allow(GoodJob).to receive(:preserve_job_records).and_return(true)
       ActiveJob::Base.queue_adapter = GoodJob::Adapter.new(execution_mode: :inline)
       stub_request(:get, File.join(child_object.access_master_path)).to_return(status: 200, body: File.open(File.join(child_object.access_master_path)).read)
     end
 
     it 'will allow user to update the child object checksum' do
+      expect(child_object.sha512_checksum).to be_nil
       expect { ChildObjectIntegrityCheckJob.new.perform }.to change { IngestEvent.count }.by(3)
-      # byebug
       expect(child_object.events_for_batch_process(BatchProcess.where(batch_action: 'integrity check'))[0].reason).to eq "The Child Object: #{child_object.oid} - has a checksum mismatch. The checksum of the image file saved to this child oid does not match the checksum of the image file in the database. This may mean that the image has been corrupted. Please verify integrity of image for Child Object: #{child_object.oid} - by manually comparing the checksum values and update record as necessary."
+      visit show_child_batch_process_path(child_oid: child_object.oid, id: BatchProcess.where(batch_action: 'integrity check')[0].id, oid: parent_object.oid)
+      expect(page).to have_button('Update Checksum')
+      # click_on 'Update Checksum'
+      # expect(child_object.sha512_checksum).to eq("d6e3926fbe14fedbf3a568b6a5dbdb3e8b2312f217daa460a743559d41a688af4a7c701e7bac908fc7e3fd51c505fa01dad9eee96fcfd2666e92c648249edf02")
     end
   end
 end
+
+# s3/05/54/16/42/43/54/16424354.tif
