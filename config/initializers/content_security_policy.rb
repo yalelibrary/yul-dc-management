ccbf314801f18114447969c292db27a7fcd2af9e
K8Sewell <kait@notch8.com>
2025-01-07 16:06:18 -0600

    reset to not use nonce directive

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -25,7 +25,7 @@ if ENV["RAILS_ENV"] == 'production' || ENV["RAILS_ENV"] == 'staging'
 
     config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
 
-    config.content_security_policy_nonce_directives = %w[script-src style-src]
+    # config.content_security_policy_nonce_directives = %w[script-src style-src]
 
     # Report violations without enforcing the policy.
     # config.content_security_policy_report_only = true
