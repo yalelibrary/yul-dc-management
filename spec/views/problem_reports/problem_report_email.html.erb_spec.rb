780bff43b5d11a7ddb0f3e4b754e07c70fb5caae
Martin Lovell <martin.lovell@yale.edu>
2025-01-13 11:59:01 -0500

    Update spec description

--- spec/views/problem_reports/problem_report_email.html.erb_spec.rb
+++ spec/views/problem_reports/problem_report_email.html.erb_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 RSpec.describe "problem_report_mailer/problem_report_email", type: :view do
   let(:problem_report) { FactoryBot.create(:problem_report) }
-  it "renders a warning about problem count" do
+  it "renders a warning about problem count when > 1000" do
     problem_report.problem_child_count = 1001
     @problem_report = problem_report
     render
     expect(rendered).to have_content "Warning: Only the first 1000 problem children will be in the spreadsheet"
   end
 
-  it "renders a warning about problem count" do
+  it "does not render a warning about problem count when <= 1000" do
     problem_report.problem_child_count = 999
     @problem_report = problem_report
     render
