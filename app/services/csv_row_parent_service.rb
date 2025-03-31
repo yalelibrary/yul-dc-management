83c92b6c3c24fe920e8a867299d47ad673329c2c
JP Engstrom <jpengstrom@macbook-pro.lan>
2025-03-31 15:22:30 -0700

    change conditional to allow Preservica and preservica

--- app/services/csv_row_parent_service.rb
+++ app/services/csv_row_parent_service.rb
@@ -80,7 +80,7 @@ class CsvRowParentService
   end
 
   def digital_object_source
-    if row['digital_object_source'] != "Preservica" || row['digital_object_source'] != "preservica"
+    if row['digital_object_source'] != "Preservica" && row['digital_object_source'] != "preservica"
       raise BatchProcessingError.new("Skipping row [#{index + 2}]. Digital Object Source must be 'Preservica'",
 'Skipped Row')
     end
