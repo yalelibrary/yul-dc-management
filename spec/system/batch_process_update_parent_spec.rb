b4d709e2904be2c6e84b872e6488bbe2aac851b7
JP Engstrom <jpengstrom@macbook-pro.lan>
2025-04-28 17:12:56 -0700

    remove line

--- spec/system/batch_process_update_parent_spec.rb
+++ spec/system/batch_process_update_parent_spec.rb
@@ -52,7 +52,6 @@ RSpec.describe BatchProcess, type: :system, prep_metadata_sources: true, prep_ad
         expect(p_o_a.barcode).to eq("39002102340669")
         expect(p_o_a.project_identifier).to eq("Beinecke")
         expect(p_o_a.digitization_funding_source).to eq("This is the Digitization Funding Source")
-        visit "/parent_objects/2005512/manifest"
         visit "/batch_processes/#{BatchProcess.last.id}/parent_objects/2005512"
         expect(page).to have_content "Status Complete"
       end
