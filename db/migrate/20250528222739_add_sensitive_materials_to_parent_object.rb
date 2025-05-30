7669b727117dd5413e4293d71b8a96c95abe386d
JP Engstrom <jpengstrom@macbook-pro.lan>
2025-05-30 13:09:35 -0700

    default nil not needed

--- db/migrate/20250528222739_add_sensitive_materials_to_parent_object.rb
+++ db/migrate/20250528222739_add_sensitive_materials_to_parent_object.rb
@@ -1,5 +1,5 @@
 class AddSensitiveMaterialsToParentObject < ActiveRecord::Migration[7.0]
   def change
-    add_column :parent_objects, :sensitive_materials, :string, default: nil
+    add_column :parent_objects, :sensitive_materials, :string
   end
 end
