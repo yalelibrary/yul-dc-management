104a1de9cbb2b948b5210e2c5dae7f03d4d0bb62
JP Engstrom <jpengstrom@MWPF3NS40T.yu.yale.edu>
2025-06-04 14:30:14 -0700

    adjust preservica ingest to verify preserica objects

--- app/models/parent_object.rb
+++ app/models/parent_object.rb
@@ -240,9 +240,14 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   # rubocop:disable Layout/LineLength
   def sync_from_preservica(_local_children_hash, preservica_children_hash)
     Rails.logger.info "************ parent_object.rb # sync_from_preservica +++ hits method with local and preservica children - (local_children_hash keys count): #{_local_children_hash.keys.count} && (preservica_children_hash key count): #{preservica_children_hash.keys.count} *************"
-    # iterate through local hashes and remove any children no longer found on preservica
+
     # iterate through preservica and update when local version found
-    sync_from_preservica_update_existing_children(preservica_children_hash)
+    sync_from_preservica_update_existing_children(_local_children_hash, preservica_children_hash)
+
+    # iterate through local hashes and remove any children no longer found on preservica
+    child_objects.each do |co|
+      co.destroy! unless found_in_preservica(co.preservica_content_object_uri, preservica_children_hash)
+    end
 
     # create child records for any new items in preservica
     create_child_records
@@ -251,9 +256,17 @@ class ParentObject < ApplicationRecord # rubocop:disable Metrics/ClassLength
   # rubocop:enable Lint/UnderscorePrefixedVariableName
   # rubocop:enable Layout/LineLength
 
-  def sync_from_preservica_update_existing_children(preservica_children_hash)
+  def sync_from_preservica_update_existing_children(_local_children_hash, preservica_children_hash)
+    local_children_hash.each_value do |value|
+      co = ChildObject.find_by(parent_object_oid: oid, order: value[:order])
+      if !value[:checksum].present? && co.checksum.present?
+        co.sha512_checksum = co.access_sha512_checksum
+        co.save!
+      end
+    end
+
     preservica_children_hash.each_value do |value|
-      co = ChildObject.find_by(parent_object_oid: oid, preservica_content_object_uri: value[:content_uri])
+      co = ChildObject.find_by(parent_object_oid: oid, sha512_checksum: value[:checksum])
       next if co.nil?
       co.pyramidal_tiff.force_update = true
       co.order = value[:order]
