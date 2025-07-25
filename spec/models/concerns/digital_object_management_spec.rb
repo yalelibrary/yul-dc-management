183f2dec8aa99d3bdc21702bb6af3863215ee3e7
JP Engstrom <jpengstrom@MWPF4GMV5Y.yu.yale.edu>
2025-07-25 11:28:54 -0700

    adds sensitive materials to digital object json

--- spec/models/concerns/digital_object_management_spec.rb
+++ spec/models/concerns/digital_object_management_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe DigitalObjectManagement, type: :model, prep_metadata_sources: tru
                                           item: '23456789',
                                           child_object_count: 1,
                                           visibility: "Private",
+                                          sensitive_materials: "Yes",
                                           aspace_json: { "title": ["test"], "volumeEnumeration": "v. 59", "callNumber": "MSS GQT" })
     full_parent_object.bib = '123456789'
     full_parent_object.barcode = '98765432'
@@ -26,6 +27,7 @@ RSpec.describe DigitalObjectManagement, type: :model, prep_metadata_sources: tru
     expect(JSON.parse(full_parent_object.generate_digital_object_json)["holdingId"]).to eq("987654321")
     expect(JSON.parse(full_parent_object.generate_digital_object_json)["itemId"]).to eq("23456789")
     expect(JSON.parse(full_parent_object.generate_digital_object_json)["barcode"]).to eq("98765432")
+    expect(JSON.parse(full_parent_object.generate_digital_object_json)["sensitiveMaterials"]).to eq("Yes")
     expect(JSON.parse(full_parent_object.generate_digital_object_json)["volumeEnumeration"]).to eq(nil)
     expect(JSON.parse(full_parent_object.generate_digital_object_json)["callNumber"]).to eq(nil)
   end
@@ -40,28 +42,6 @@ RSpec.describe DigitalObjectManagement, type: :model, prep_metadata_sources: tru
       example.run
       ENV['VPN'] = original_vpn
       ENV['FEATURE_FLAGS'] = original_flags
-    end
-
-    it "can send ils digital object updates" do
-      full_parent_object = FactoryBot.build(:parent_object,
-                                            oid: '45678',
-                                            authoritative_metadata_source_id: voyager,
-                                            child_object_count: 1,
-                                            visibility: "Private",
-                                            voyager_json: { "title": ["test"], "volumeEnumeration": "v. 59", "callNumber": "MSS GQT" })
-      full_parent_object.bib = '123456789'
-      full_parent_object.barcode = '98765432'
-      full_parent_object.holding = '987654321'
-      full_parent_object.item = '23456789'
-      full_parent_object.save!
-      expect(full_parent_object.digital_object_json_available?).to be_truthy
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["source"]).to eq("ils")
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["bibId"]).to eq("123456789")
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["holdingId"]).to eq("987654321")
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["itemId"]).to eq("23456789")
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["barcode"]).to eq("98765432")
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["volumeEnumeration"]).to eq("v. 59")
-      expect(JSON.parse(full_parent_object.generate_digital_object_json)["callNumber"]).to eq("MSS GQT")
     end
 
     it "can send alma digital object updates" do
