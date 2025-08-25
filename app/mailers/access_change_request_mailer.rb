75ad44684c9a37a0948621fa46c0bbc5e979a329
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2025-08-25 15:41:55 -0500

    Updates from email destination (#1578)

--- app/mailers/access_change_request_mailer.rb
+++ app/mailers/access_change_request_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AccessChangeRequestMailer < ApplicationMailer
-  default from: "do_not_reply@library.yale.edu"
+  default from: "do_not_reply@yale.edu"
 
   def access_change_request_email
     @access_change_request = params[:access_change_request]
