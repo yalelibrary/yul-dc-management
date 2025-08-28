464da58dc619c9cc40d09615427afa5172deda1c
K8Sewell <kait@notch8.com>
2025-08-28 18:12:41 -0500

    Modify matching logic

--- app/models/concerns/updatable.rb
+++ app/models/concerns/updatable.rb
@@ -112,7 +112,7 @@ module Updatable
       parent_object.reload
       trigger_setup_metadata(parent_object)
 
-      sync_from_preservica if parent_object.digital_object_source == 'Preservica' || parent_object.digital_object_source == 'preservica'
+      sync_from_preservica if /preservica/i.match?(parent_object.digital_object_source)
 
       processing_event_for_parent(parent_object)
     end
