085b98825687a1fff16118d5a24bab6b86c1d5fb
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2025-11-12 12:43:53 -0600

    Adds error handling to SolrReindexAllJob (#1604)
    * Adds error handling to SolrReindexAllJob
    
    * Adds more specific error message
    
    * Ensure new jobs start if one fails

--- app/jobs/solr_reindex_all_job.rb
+++ app/jobs/solr_reindex_all_job.rb
@@ -11,8 +11,14 @@ class SolrReindexAllJob < ApplicationJob
     500
   end
 
-  def perform(start_position = 0)
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/PerceivedComplexity
+  def perform(start_position = 0, current_batch_process = BatchProcess.new)
     solr = SolrService.connection
+    current_batch_process.user = User.system_user
+    current_batch_process.save!
     # Groups of limit
     parent_objects = ParentObject.order(:oid).offset(start_position).limit(SolrReindexAllJob.job_limit)
     last_job = parent_objects.count < SolrReindexAllJob.job_limit
@@ -23,6 +29,12 @@ class SolrReindexAllJob < ApplicationJob
           results, child_results = parent_object.to_solr_full_text
           child_documents += child_results unless child_results.nil?
           results
+                 rescue => e
+                   # rubocop:disable Lint/UselessAssignment
+                   current_batch_connection = parent_object.current_batch_connection
+                   # rubocop:enable Lint/UselessAssignment
+                   parent_object.processing_event("SolrReindexAllJob failed due to #{e.message} for parent object OID: #{parent_object.oid}.")
+                   return nil # if errors will convert parent to nil and compact later removes them
         end.compact)
         solr.commit
         reindex_child_documents(solr, child_documents)
@@ -30,7 +42,14 @@ class SolrReindexAllJob < ApplicationJob
       SolrReindexAllJob.perform_later(start_position + parent_objects.count) unless last_job
     end
     SolrService.clean_index_orphans if last_job
+  rescue => e
+    SolrReindexAllJob.perform_later(start_position + parent_objects.count) unless last_job
+    current_batch_process.batch_processing_event("SolrReindexAllJob failed due to #{e.message}", "failed")
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/PerceivedComplexity
 
   def reindex_child_documents(solr, child_documents)
     child_documents.each_slice(SolrReindexAllJob.solr_batch_limit) do |child_documents_group|
