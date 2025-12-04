601499c8c6b69e2f799c4ba877b272d89b798790
JP Engstrom <jpengstrom@macbook-pro.lan>
2025-12-04 10:19:53 -0800

    adds rescue for frozen attribute

--- app/jobs/sync_from_preservica_job.rb
+++ app/jobs/sync_from_preservica_job.rb
@@ -10,6 +10,8 @@ class SyncFromPreservicaJob < ApplicationJob
 
   def perform(batch_process)
     batch_process.sync_from_preservica
+  rescue FrozenError => e
+    batch_process.batch_processing_event("Frozen error: #{e.message} at #{e.backtrace.first}", "failed")
   rescue => e
     batch_process.batch_processing_event("Setup job failed to save: #{e.message}", "failed")
   end
