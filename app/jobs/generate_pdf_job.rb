426565d3f3234c0e8216bddb1b07724a2bf36419
JP Engstrom <jpengst@gmail.com>
2026-01-13 09:27:57 -0800

    Allow PDF Generation for Tiled Images (#1629)
    ---------
    
    Co-authored-by: JP Engstrom <jpengstrom@MWPF53Q8XW.yu.yale.edu>
    Co-authored-by: JP Engstrom <jpengstrom@YUAG282.yu.yale.edu>
    Co-authored-by: JP Engstrom <jpengstrom@MWPF4K7Z8R.yu.yale.edu>

--- app/jobs/generate_pdf_job.rb
+++ app/jobs/generate_pdf_job.rb
@@ -7,6 +7,7 @@ class GeneratePdfJob < ApplicationJob
     10
   end
 
+  # rubocop:disable Metrics/MethodLength
   def perform(parent_object, current_batch_process = parent_object.current_batch_process, current_batch_connection = parent_object.current_batch_connection)
     return unless parent_object.should_create_manifest_and_pdf?
     parent_object.current_batch_process = current_batch_process
@@ -16,12 +17,17 @@ class GeneratePdfJob < ApplicationJob
   rescue RuntimeError => e
     if e.message.include?("Can not read scanlines from a tiled image")
       parent_object.processing_event("Unable to generate PDF: source image is in tiled format and cannot be processed.", "pdf-generation-failed")
+      current_batch_process&.batch_processing_event("Unable to generate PDF: source image is in tiled format and cannot be processed.", "pdf-generation-failed")
+    elsif e.message.include?("Invalid tile byte count")
+      parent_object.processing_event("Unable to generate PDF: source image has invalid tile byte count and cannot be processed. Please regenerate the PTIFF.", "pdf-generation-failed")
+      current_batch_process&.batch_processing_event("Unable to generate PDF for parent #{parent_object.oid}: source image has invalid tile byte count. Please regenerate the PTIFF.", "failed")
     else
       parent_object.processing_event("Unable to generate PDF, #{e.message}", "failed")
-      raise
     end
+    raise
   rescue => e
     parent_object.processing_event("Unable to generate PDF, #{e.message}", "failed")
     raise
   end
 end
+# rubocop:enable Metrics/MethodLength
