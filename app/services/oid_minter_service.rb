187afbf28eb83856e8d4fecbf76b16cb9b66e60a
K8Sewell <kait@notch8.com>
2026-02-26 14:49:56 -0600

    Mint OIDs with different start points for different clusters

--- app/services/oid_minter_service.rb
+++ app/services/oid_minter_service.rb
@@ -12,7 +12,17 @@ class OidMinterService
   end
 
   def self.initialize_sequence!
-    initial_value = Rails.application.config.oid_sequence_initial_value
+    initial_value = if ENV['CLUSTER_NAME'] == 'yul-dc-demo'
+                      20_000_000
+                    elsif ENV['CLUSTER_NAME'] == 'yul-dc-test'
+                      40_000_000
+                    elsif ENV['CLUSTER_NAME'] == 'yul-dc-uat'
+                      50_000_000
+                    elsif ENV['CLUSTER_NAME'] == 'yul-dc-prod' || ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
+                      Rails.application.config.oid_sequence_initial_value
+                    else
+                      60_000_000
+                    end
     # This does nothing if the sequence already exists
     ActiveRecord::Base.connection.execute("CREATE SEQUENCE IF NOT EXISTS OID_SEQUENCE START WITH #{initial_value};")
     initial_value
