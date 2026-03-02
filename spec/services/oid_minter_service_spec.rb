642be0e05f6faa86d794c66cf764dbd26505f13d
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2026-03-02 11:42:19 -0600

    Mint OIDs with different start points for different clusters (#1662)
    * Mint OIDs with different start points for different clusters
    
    * Adds more conditions to conditional
    
    * rubocop

--- spec/services/oid_minter_service_spec.rb
+++ spec/services/oid_minter_service_spec.rb
@@ -25,4 +25,76 @@ describe OidMinterService do
       expect(oids.length).to equal oids.uniq.length
     end
   end
+  context 'when in yul-dc-demo cluster environment' do
+    around do |example|
+      original_cluster_name = ENV['CLUSTER_NAME']
+      ENV['CLUSTER_NAME'] = 'yul-dc-demo'
+      example.run
+      ENV['CLUSTER_NAME'] = original_cluster_name
+    end
+    it 'initializes the sequence with the expected value' do
+      oids = described_class.generate_oids(1)
+      expect(oids[0]).to be >= 20_000_000
+    end
+  end
+  context 'when in yul-dc-test cluster environment' do
+    around do |example|
+      original_cluster_name = ENV['CLUSTER_NAME']
+      ENV['CLUSTER_NAME'] = 'yul-dc-test'
+      example.run
+      ENV['CLUSTER_NAME'] = original_cluster_name
+    end
+    it 'initializes the sequence with the expected value' do
+      oids = described_class.generate_oids(1)
+      expect(oids[0]).to be >= 40_000_000
+    end
+  end
+  context 'when in yul-dc-uat cluster environment' do
+    around do |example|
+      original_cluster_name = ENV['CLUSTER_NAME']
+      ENV['CLUSTER_NAME'] = 'yul-dc-uat'
+      example.run
+      ENV['CLUSTER_NAME'] = original_cluster_name
+    end
+    it 'initializes the sequence with the expected value' do
+      oids = described_class.generate_oids(1)
+      expect(oids[0]).to be >= 50_000_000
+    end
+  end
+  context 'when in yul-dc-prod cluster environment' do
+    around do |example|
+      original_cluster_name = ENV['CLUSTER_NAME']
+      ENV['CLUSTER_NAME'] = 'yul-dc-prod'
+      example.run
+      ENV['CLUSTER_NAME'] = original_cluster_name
+    end
+    it 'initializes the sequence with the expected value' do
+      oids = described_class.generate_oids(1)
+      expect(oids[0]).to be >= 30_000_000
+    end
+  end
+  context 'when in test environment' do
+    around do |example|
+      original_environment_name = ENV['RAILS_ENV']
+      ENV['RAILS_ENV'] = 'test'
+      example.run
+      ENV['RAILS_ENV'] = original_environment_name
+    end
+    it 'initializes the sequence with the expected value' do
+      oids = described_class.generate_oids(1)
+      expect(oids[0]).to be >= 200_000_000
+    end
+  end
+  context 'when in development environment' do
+    around do |example|
+      original_environment_name = ENV['RAILS_ENV']
+      ENV['RAILS_ENV'] = 'development'
+      example.run
+      ENV['RAILS_ENV'] = original_environment_name
+    end
+    it 'initializes the sequence with the expected value' do
+      oids = described_class.generate_oids(1)
+      expect(oids[0]).to be >= 100_000_000
+    end
+  end
 end
