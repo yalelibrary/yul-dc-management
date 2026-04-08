aaaca3ba6021986124ee0266152bda65c9588bd4
JP Engstrom <jpengstrom@MWPF3VNXT8.yu.yale.edu>
2026-04-08 13:21:32 -0700

    adds raises to rescues

--- spec/jobs/sync_from_preservica_job_spec.rb
+++ spec/jobs/sync_from_preservica_job_spec.rb
@@ -45,13 +45,13 @@ RSpec.describe SyncFromPreservicaJob, type: :job, prep_metadata_sources: true, p
       allow(batch_process).to receive(:batch_processing_event)
     end
 
-    it 'logs the error with batch_processing_event' do
+    it 'logs the error with batch_processing_event and re-raises' do
       expect(batch_process).to receive(:batch_processing_event)
         .with("Setup job failed to save: #{error_message}", "failed")
 
       expect do
         described_class.new.perform(batch_process)
-      end.not_to raise_error
+      end.to raise_error(StandardError, error_message)
     end
   end
 end
