0380d60c015a20aa2f4ba84191af76490ba30ade
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2026-04-30 11:02:29 -0500

    Refactors CreateChildOidCsvJob (#1695)
    * Make error message specific to job
    
    * Process export children in batches
    
    * Remove error message
    
    * Capture errors
    
    * Update order of expectations

--- app/jobs/create_child_oid_csv_job.rb
+++ app/jobs/create_child_oid_csv_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CreateChildOidCsvJob < ApplicationJob
+  retry_on GoodJob::InterruptError, attempts: 3
   queue_as :default
 
   def default_priority
@@ -10,6 +11,6 @@ class CreateChildOidCsvJob < ApplicationJob
   def perform(batch_process)
     batch_process.child_output_csv
   rescue => e
-    batch_process.batch_processing_event("Setup job failed to save: #{e.message}", "failed")
+    batch_process.batch_processing_event("CreateChildOidCsvJob failed to save: #{e.message}", "failed")
   end
 end
