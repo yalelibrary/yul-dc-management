bcadde66f9dcbf0b5af97e577a3c5a0c70e16937
JP Engstrom <jpengstrom@MacBook-Pro.local>
2026-05-21 11:00:17 -0600

    remove metadata_fields for other ticket

--- config/initializers/metadata_fields.rb
+++ config/initializers/metadata_fields.rb
@@ -140,24 +140,6 @@ METADATA_FIELDS = {
       'callNumber_tesim'
     ]
   },
-  acquisitionSource: {
-    label: 'Acquisition Source',
-    solr_fields: [
-      'acquisitionSource_tesim'
-    ]
-  },
-  copyDescription: {
-    label: 'Copy Description',
-    solr_fields: [
-      'copyDescription_tesim'
-    ]
-  },
-  bindingInfo: {
-    label: 'Binding Information',
-    solr_fields: [
-      'bindingInfo_tesim'
-    ]
-  },
   sourceTitle: {
     label: 'Collection Title',
     solr_fields: [
