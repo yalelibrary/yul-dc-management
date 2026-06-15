5f5ce3e486992ba84e3396ad68cb7ce6e7a0a289
K8Sewell <kait@notch8.com>
2026-06-15 13:20:13 -0500

    Add more robust file comparison

--- app/models/preservica/bitstream.rb
+++ app/models/preservica/bitstream.rb
@@ -31,36 +31,53 @@ class Preservica::Bitstream
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Layout/LineLength
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   def download_to_file(file_name)
     Rails.logger.info "************ bitstream.rb # download_to_file +++ hits download to file method with file: #{file_name} *************"
     co_oid = file_name.scan(/\d+/).last
+    expected_size = size
+    expected_sha512 = sha512_checksum
+    temp_file_name = "#{file_name}.part"
+
+    File.delete(temp_file_name) if File.exist?(temp_file_name)
+
     data_length = 0
-    sha512 = Digest::SHA512. new
-    File.open(file_name, 'wb') do |file|
+    sha512 = Digest::SHA512.new
+    File.open(temp_file_name, 'wb') do |file|
       Rails.logger.info "************ bitstream.rb # download_to_file +++ File.open succeeds in opening file *************"
       preservica_client.get(content_uri) do |chunk|
-        data_length += chunk.length
+        data_length += chunk.bytesize
         no_of_bites = file.write(chunk)
         raise StandardError, "Failed to write to file" if no_of_bites < 1
         Rails.logger.info "************ bitstream.rb # download_to_file +++ File.write wrote #{no_of_bites} bites to file *************"
         sha512 << chunk
       end
+      file.flush
+      file.fsync
     end
     data_sha512 = sha512.hexdigest
-    file_size = File.size?(file_name)
+    file_size = File.size?(temp_file_name)
     Rails.logger.info "************ bitstream.rb # download_to_file +++ counts file size (data_length): #{data_length} *************"
     Rails.logger.info "************ bitstream.rb # download_to_file +++ grabs sha checksum (data_sha512): #{data_sha512} *************"
-    unless data_sha512.casecmp?(sha512_checksum)
+    unless data_sha512.casecmp?(expected_sha512)
       raise StandardError,
-"The checksum for this object is different than the checksum that DCS expected. Please ensure your image folder in Preservica has SHA-512 fixity checksums. ------------ Message from System: Checksum mismatch for Child Object: #{co_oid} - (#{data_sha512} != #{sha512_checksum})"
+"The checksum for this object is different than the checksum that DCS expected. Please ensure your image folder in Preservica has SHA-512 fixity checksums. ------------ Message from System: Checksum mismatch for Child Object: #{co_oid} - (#{data_sha512} != #{expected_sha512})"
     end
-    raise StandardError, "Data size did not match for Child Object: #{co_oid} - (#{data_length} != #{size})" unless data_length == size
-    raise StandardError, "File sizes do not match for Child Object: #{co_oid} - (#{file_size} != #{size})" unless file_size == size
-    # could also check: Digest::SHA512.file(file_name).hexdigest == sha512_checksum, but probably not necessary
+    raise StandardError, "Data size did not match for Child Object: #{co_oid} - (#{data_length} != #{expected_size})" unless data_length == expected_size
+    raise StandardError, "File sizes do not match for Child Object: #{co_oid} - (#{file_size} != #{expected_size})" unless file_size == expected_size
+
+    File.delete(file_name) if File.exist?(file_name)
+    File.rename(temp_file_name, file_name)
+  rescue StandardError
+    File.delete(temp_file_name) if File.exist?(temp_file_name)
+    raise
   end
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
   # rubocop:enable Layout/LineLength
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/PerceivedComplexity
 
   def xml
     @xml ||= Nokogiri::XML(preservica_client.content_object_generation_bitstream(@content_id, @generation_id, @id)).remove_namespaces!
