d28bb826407d18f0a87a9c01654bd8424cfb6a40
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2026-07-31 15:47:47 -0500

    Add variable wait times for retry (#1770)

--- app/jobs/sync_from_preservica_job.rb
+++ app/jobs/sync_from_preservica_job.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class SyncFromPreservicaJob < ApplicationJob
-  retry_on RuntimeError, Net::HTTPFatalError, Net::ReadTimeout, FrozenError, PreservicaImageService::PreservicaImageServiceNetworkError, attempts: 3 do |job, exception|
+  retry_on RuntimeError, Net::HTTPFatalError, Net::ReadTimeout, FrozenError, PreservicaImageService::PreservicaImageServiceNetworkError, wait: lambda { |executions|
+                                                                                                                                                 [30, 180, 10_800][executions - 1 || 10_800]
+                                                                                                                                               }, attempts: 3 do |job, exception|
     batch_arg = job.arguments.first
     batch_process_id = batch_arg&.respond_to?(:id) ? batch_arg.id : batch_arg&._aj_globalid&.split('/')&.last&.to_i
     batch_process = BatchProcess.find_by(id: batch_process_id)
