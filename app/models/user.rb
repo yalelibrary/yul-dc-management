db90eed37660dd0c2752764342e65884a24d336d
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2026-09-23 15:54:04 -0500

    Updates system user name (#1824)

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   def self.system_user
     system_user = User.find_by_uid('System')
     unless system_user
-      system_user = User.new(uid: 'System', email: 'test@example.com', first_name: 'test', last_name: 'user')
+      system_user = User.new(uid: 'System', email: 'test@example.com', first_name: 'System', last_name: 'User')
       Rails.logger.error("Unable to save system user") unless system_user.save!
     end
     system_user
